9dca973525190a47bd1306ab139de611f95c3c1d
JacquesBonet <jacques.bonet@gmail.com>
2017-12-21 11:32:58 +0100

    Fichier manquant

---
+++ app/utils/Store.ts
@@ -0,0 +1,25 @@
+import { AsyncStorage } from 'react-native'
+import { AUTH_LOGIN_STORE } from '../constants/Auth'
+
+export async function setLogin(value) {
+  await AsyncStorage.setItem(AUTH_LOGIN_STORE, JSON.stringify(value))
+}
+
+export async function getLogin() {
+  const authString = await AsyncStorage.getItem(AUTH_LOGIN_STORE)
+
+  if (authString === null)
+    return {
+      username: '',
+      password: '',
+    }
+  return JSON.parse(authString)
+}
+
+let seqNumber = 0;
+
+/** calculate seq number ***/
+export function getSeqNumber() {
+  return ++seqNumber;
+}
+
