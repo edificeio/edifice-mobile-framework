a2cd02c84a8842a7c7d15f82346c93cb6f269352
Camille Wintz <camille.wintz@lweb-education.net>
2018-02-27 18:35:29 +0100

    added scroll animation and close icon to carousel

--- app/ui/Images.tsx
+++ app/ui/Images.tsx
@@ -1,31 +1,50 @@
 import style from "glamorous-native"
 import * as React from "react"
-import { View, Text, Image, ScrollView, Modal, Dimensions } from "react-native"
+import { View, Text, Image, ScrollView, Modal, Dimensions, Animated } from "react-native"
 import { layoutSize } from "../constants/layoutSize"
-import { Row } from ".";
+import { Row, Icon } from ".";
 import { StackNavigator } from "react-navigation";
 import FitImage from 'react-native-fit-image';
 
-export interface IAvatarsProps {
-	images: object[];
-}
-
-export class Images extends React.Component<IAvatarsProps, any> {
+export class Images extends React.Component<{ images: object[] }, any> {
 	state = {
 		fullscreen: false
 	};
 
+	carouselRef: any;
+	currentScroll = 0;
+
+	slideToImage(e){
+		const { width, height } = Dimensions.get('window');
+		const direction = this.currentScroll % width > width / 2;
+		let newPosition = Math.floor((this.currentScroll / width)) * width;
+		if(direction){
+			newPosition += width;
+		}
+		this.carouselRef.scrollTo({ x: newPosition, animated: true });
+	}
+
 	carousel(){
+		const { width, height } = Dimensions.get('window');
 		return (
 			<Modal visible={ this.state.fullscreen } onRequestClose={ () => this.setState({ fullscreen: false })} transparent={ true }>
-				<View style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.5)'}}>
-					<ScrollView horizontal={ true } style={{ flex: 1, flexDirection: 'row', height: '100%' }} contentContainerStyle={{ justifyContent: 'center' }}>
+				<View style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.90)'}}>
+					<ScrollView 
+						ref={ e => this.carouselRef = e }
+						horizontal={ true } 
+						style={{ flex: 1, flexDirection: 'row', height: '100%' }} 
+						contentContainerStyle={{ justifyContent: 'center' }}
+						onScroll={ (e) => this.currentScroll = e.nativeEvent.contentOffset.x  }
+						scrollEventThrottle={16}
+						onTouchEnd={ (e) => this.slideToImage(e) }
+					>
 						{ this.props.images.map((image, index) => (
 						<View style={{ flex: 1, justifyContent: 'center'}} key={ index }>
-							<FitImage resizeMode="contain" style={{ width: Dimensions.get('window').width, height: 250 }} source={ image } />
+							<FitImage resizeMode="contain" style={{ width: width, height: height }} source={ image } />
 						</View>
 					)) }
 					</ScrollView>
+					<Icon size={ 16 } color="#ffffff" name="close" onPress={ () => this.setState({ fullscreen: false })} style={{ position: 'absolute', top: 10, right: 10 }} />
 				</View>
 			</Modal>
 		)
