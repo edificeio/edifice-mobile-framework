0fa9b3425fea67a7601af474dffec72cd5bdf163
Camille Wintz <camille.wintz@lweb-education.net>
2018-04-19 14:58:41 +0200

    changed shadow for underline

--- app/auth/components/NotifPrefLine.tsx
+++ app/auth/components/NotifPrefLine.tsx
@@ -7,13 +7,23 @@ import { TouchCard } from "../../ui/Card";
 import I18n from "react-native-i18n";
 import { Toggle } from "../../ui/forms/Toggle";
 
+const Container = style.touchableOpacity({
+	alignItems: 'center',
+	flexDirection: 'row',
+	backgroundColor: 'white',
+	borderBottomWidth: 1,
+	borderColor: '#ddd',
+	height: 46,
+	justifyContent: 'flex-start'
+});
+
 export const NotifPrefLine = ({ i18nKey, value, onCheck, onUncheck }) => {
 	return (
-        <TouchCard style={{ marginBottom: 10, marginTop: 10, paddingHorizontal: 20 }} onPress={ () => value ? onUncheck() : onCheck() }>
+        <Container style={{ marginBottom: 10, marginTop: 10, paddingHorizontal: 20 }} onPress={ () => value ? onUncheck() : onCheck() }>
             <Line style={{ justifyContent: 'space-between', alignItems: 'center' }}>
                 <Label style={{ flex: 1 }}>{ I18n.t(i18nKey.replace('.', '-')) }</Label>
                 <Toggle checked={ value } onCheck={ () => onCheck() } onUncheck={ () => onUncheck() } />
             </Line>
-        </TouchCard>
+        </Container>
     );
 }
