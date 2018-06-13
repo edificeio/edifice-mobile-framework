42f0ca069e292e1dd692d75d71f5b9a91930d009
Lancel Thaledric <thaledric@gmail.com>
2018-06-13 17:56:22 +0200

    Display some homework in the right way (static version)

--- app/homework/components/Homework.tsx
+++ app/homework/components/Homework.tsx
@@ -1,24 +1,87 @@
 import * as React from "react";
 import { PageContainer, Content } from "../../ui/ContainerContent";
-import { ScrollView, Text, View } from "react-native";
+import { View } from "react-native";
 import style from "glamorous-native";
 import { CommonStyles } from "../../styles/common/styles";
-import { Card } from "../../ui/Card";
-import { CircleNumber } from "../../ui/CircleNumber";
+
+const { Text, FlatList } = style;
+
+interface HomeworkTask {
+	title: string;
+	description: string;
+};
+interface HomeworkDayTaskArray {
+	daynb: number;
+	dayname: string;
+	tasks: HomeworkTask[];
+};
+
+const TESTDATA = [
+	{
+		daynb: 22,
+		dayname: "jeudi",
+		tasks: [
+			{
+				title: "Intelligence Artificielle",
+				description: "Réviser le Traitement Automatique du Langage Naturel",
+			},
+			{
+				title: "Tristitude",
+				description: "Conjuguer Bouillir au subjonctif pluriel",
+			},
+			{
+				title: "Philosophie",
+				description: "Dissertation : Quel avenir pour les pingouins d'Afrique ?",
+			},
+		],
+	},
+	{
+		daynb: 23,
+		dayname: "vendredi",
+		tasks: [
+			{
+				title: "Scuplture sur marbre",
+				description: "Sculpter la Venus de Milo avec des bras",
+			},
+			{
+				title: "Kamoulox",
+				description: "Photocopier du sable et éventer la famille pirate",
+			},
+		],
+	},
+	{
+		daynb: 26,
+		dayname: "lundi",
+		tasks: [
+			{
+				title: "Russe",
+				description: "Réciter l'alphabet cyrillique à l'envers",
+			},
+			{
+				title: "Physique - Chimie",
+				description: "Exo 15 → 88 p. 249 et plus ou moins",
+			},
+			{
+				title: "Philosophie",
+				description: "Dissertation : Quel avenir pour les dissertations sur les pingouins d'Afrique ?",
+			},
+		],
+	},
+];
 
 /**
- * Page container that show every UI component.
+ * Homework
+ *
+ * Display page for all homework in a calendar-like way.
+ *
+ * FIXME: On FlatList, paddingBottom and marginBottom doesn't seem to work, even the bottom margins and padding of the children.
  */
 export class Homework extends React.Component {
-
 	public render() {
 		return (
 			<PageContainer>
-				<HomeworkTimeLine/>
-				<ScrollView>
-					<HomeworkDayTasks/>
-					<HomeworkDayTasks/>
-				</ScrollView>
+				<HomeworkTimeLine />
+				<FlatList paddingVertical={15} data={TESTDATA} renderItem={({ item }) => <HomeworkDayTasks data={item} />} />
 			</PageContainer>
 		);
 	}
@@ -36,7 +99,17 @@ const HomeworkTimeLine = style.view({
 });
 
 /**
- * Just a wrapper for the heading of a day tasks.
+ * HomeworkDayCheckpoint
+ *
+ * Just a wrapper for the heading of a day tasks. Displays a day number in a circle and a day name
+ * TODO?: May took a Date object as a parameter instead of a number and a string ?
+ * Props:
+ *     `style`: `any` - Glamorous style to add.
+ * 	   `nb`: `number`- Day number to be displayed in a `HomeworkDayCircleNumber`.
+ *     `text`: `string` - Day name to be displayed.
+ *     `active`: `boolean` - An active `HomeworkDayCheckpoint` will be highlighted. Default `false`.
+ *
+ * An unstyled version on this component is available as `HomeworkDayCheckpoint_Unstyled`.
  */
 
 const HomeworkDayCheckpoint_Unstyled = ({
@@ -51,59 +124,129 @@ const HomeworkDayCheckpoint_Unstyled = ({
 	active?: boolean
 }) => (
 	<View style={[style]}>
-		{active ? <HomeworkDayCircleNumberActive nb={nb} /> : <HomeworkDayCircleNumber nb={nb} />}
-		<Text>{text.toUpperCase()}</Text>
+		<HomeworkDayCircleNumber nb={nb} active={active} />
+		<Text color={CommonStyles.lightTextColor} fontSize={12}>
+			{text.toUpperCase()}
+		</Text>
 	</View>
 );
+
 const HomeworkDayCheckpoint = style(HomeworkDayCheckpoint_Unstyled)({
 	flexDirection: "row",
 	alignItems: "center",
 });
 
-// TODO: Use just one class with an `active` prop, and manage text color
-const HomeworkDayCircleNumber_Unstyled = ({ style, nb }: { style?: any; nb?: number }) => (
+/**
+ * HomeworkDayCircleNumber
+ *
+ * Display a number in a circle elegantly. Mostly used to show a day number.
+ * Props:
+ *     `style`: `any` - Glamorous style to add.
+ * 	   `nb`: `number` - Just as simple as the number to be displayed.
+ *     `active`: `boolean` - An active `HomeworkDayCircleNumber` will be highlighted.
+ * TODO: style.Text component gives Invariant Violation, must use `const {Text} = style`. Why ?
+ * TODO: When active, the blue background should be a gradient, according to the mockup.
+ *
+ * An unstyled version on this component is available as `HomeworkDayCircleNumber_Unstyled`.
+ */
+const HomeworkDayCircleNumber_Unstyled = ({
+	style,
+	nb,
+	active = false,
+}: {
+	style?: any;
+	nb?: number;
+	active?: boolean
+}) => (
 	<View style={[style]}>
-		<Text>{nb}</Text>
+		<Text color={active ? CommonStyles.tabBottomColor : CommonStyles.lightTextColor} fontSize={12}>
+			{nb}
+		</Text>
 	</View>
 );
-const HomeworkDayCircleNumber = style(HomeworkDayCircleNumber_Unstyled)({
-	alignItems: "center",
-	justifyContent: "center",
-	width: 30,
-	height: 30,
-	borderStyle: "solid",
-	borderWidth: 1,
-	borderColor: CommonStyles.tabBottomColor,
-	backgroundColor: CommonStyles.tabBottomColor,
+
+const HomeworkDayCircleNumber = style(HomeworkDayCircleNumber_Unstyled)(
+	{
+		alignItems: "center",
+		justifyContent: "center",
+		width: 30,
+		height: 30,
+		borderStyle: "solid",
+		borderWidth: 1,
+		borderColor: CommonStyles.tabBottomColor,
+		shadowColor: "#6B7C93",
+		shadowOpacity: 0.2,
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 4,
+		borderRadius: 15,
+		marginHorizontal: 14,
+	},
+	({ active }) => ({
+		backgroundColor: active ? CommonStyles.actionColor : CommonStyles.tabBottomColor
+	})
+);
+
+/**
+ * HomeworkCard
+ *
+ * Like `Card`, but some margin and padding, custom shadow and rounded.
+ *
+ * An unstyled version on this component is available as `HomeworkCard_Unstyled`.
+ */
+
+const HomeworkCard_Unstyled = ({
+	style,
+	title,
+	description,
+}: {
+	style?: any;
+	title?: string;
+	description?: string
+}) => (
+	<View style={[style]}>
+		<Text fontSize={14} color={CommonStyles.textColor} lineHeight={20}>
+			{description}
+		</Text>
+		<Text fontSize={12} color={CommonStyles.lightTextColor} marginTop={5}>
+			{title}
+		</Text>
+	</View>
+);
+
+const HomeworkCard = style(HomeworkCard_Unstyled)({
+	marginBottom: 15,
+	marginLeft: 60,
+	marginRight: 20,
+	borderRadius: 5,
 	shadowColor: "#6B7C93",
 	shadowOpacity: 0.2,
 	shadowOffset: { width: 0, height: 2 },
-	shadowRadius: 4,
-	borderRadius: 15,
-	marginHorizontal: 14,
+	paddingVertical: 20,
+	paddingHorizontal: 15,
+	backgroundColor: "#FFF",
 });
-const HomeworkDayCircleNumberActive = style(HomeworkDayCircleNumber)({
-	backgroundColor: CommonStyles.actionColor,
-	color: CommonStyles.tabBottomColor,
-});
-
 
 /**
- * All the tasks of one day, with day number displayed.
+ * HomeworkDayTasks
+ *
+ * Display the task list of a day (with day number and name).
+ * Props:
+ *     data: HomeworkDayTaskArray - information of the day (number and name) and list of the tasks.
+ * TODO: Detect if the day is today, and set the HomeworkDayCheckpoint active in this case.
  */
-class HomeworkDayTasks extends React.Component {
+interface HomeworkDayTasksProps {
+	data: HomeworkDayTaskArray;
+}
+class HomeworkDayTasks extends React.Component<HomeworkDayTasksProps, any> {
+	constructor(props: HomeworkDayTasksProps) {
+		super(props);
+	}
+
 	public render() {
 		return (
 			<View>
-				<HomeworkDayCheckpoint nb={19} text="jeudi" active/>
-				<Card>
-					<Text>Réviser le Traitement Automatique du Langage Naturel</Text>
-					<Text>Intelligence Artificielle</Text>
-				</Card>
-				<Card>
-					<Text>Conjuguer Bouillir au subjonctif pluriel</Text>
-					<Text>Tristitude</Text>
-				</Card>
+				<HomeworkDayCheckpoint nb={this.props.data.daynb} text={this.props.data.dayname} />
+				{this.props.data.tasks.map(item => <HomeworkCard title={item.title} description={item.description} />)}
 			</View>
 		);
 	}
