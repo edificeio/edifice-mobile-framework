ce7e1a24b363c72ddceb83a8815c9507b60440ac
Rafik Djedjig <rafik.djedjig@web-education.net>
2018-07-17 16:46:05 +0200

    WIP mixpanel init

--- app/tracking/TrackingManager.ts
+++ app/tracking/TrackingManager.ts
@@ -1,27 +1,26 @@
 import { Platform } from 'react-native';
 import firebase from "react-native-firebase";
-import MixpanelInstance from 'react-native-mixpanel';
+import { MixpanelInstance } from "react-native-mixpanel";
 
 
 // Firebase Init
 let analytics;
 try{
-	console.warn('mixpanel ok');
 	analytics = firebase.app().analytics();
 }
 catch(e){
 	console.warn(e);
 }
 
-
 export class Tracking {
 
 	// Mixpanel Init
 	private static mixpanel;
-	// Externalize mixpanel token configuration
+	// TODO : Externalize mixpanel token configuration
 	static async initMixpanel() {
 		Tracking.mixpanel = new MixpanelInstance('9cc560e73f2e7b38c0b247fcd8c84e5a')
 		await Tracking.mixpanel.initialize()
+		console.warn('Mixpanel init done ' + Tracking.mixpanel);
 	}
 
 	public static init() {
@@ -49,7 +48,7 @@ export class Tracking {
 		}
 		if(params){
 			analytics.logEvent(name, params);
-			Tracking.mixpanel.trackWithProperties(name, params);
+			Tracking.mixpanel.track(name, params);
 		}
 		else{
 			analytics.logEvent(name);
@@ -61,3 +60,4 @@ export class Tracking {
 		Tracking.logEvent(currentScreen, navParams)
 	}
 }
+Tracking.initMixpanel();
