9d3f4e0152381a1fd01ba7e39f4b84dc930d9060
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-08-10 22:23:35 +0200

    clean & document fetchWithCache

--- app/infra/fetchWithCache.ts
+++ app/infra/fetchWithCache.ts
@@ -10,6 +10,9 @@ import { Connection } from "./Connection";
  * @param init options of the standard fetch api (@see https://developer.mozilla.org/fr/docs/Web/API/WindowOrWorkerGlobalScope/fetch#Param%C3%A8tres)
  * @param forceSync If true, fetch on internet preferably. If false, fetch on the the only if there is no cache.
  * @param platform url to the plateform (ex: "https://recette.opendigitaleducation.com") Uses global plateform configuration as defaut value.
+ * @param getBody function that return a promise to get the body of the response. The default behaviour treat data as raw text.
+ * @param getCacheResult funcation that return the cache result from the cache stored data. The default behaviour treat cache data as a Response.
+ * @returns a Response object. Use it as you wish.
  */
 export async function fetchWithCache(
   path: string,
@@ -17,7 +20,7 @@ export async function fetchWithCache(
   forceSync: boolean = true,
   platform: string = Conf.platform,
   getBody = r => r.text(),
-  getResult = cr => new Response(...cr)
+  getCacheResult = cr => new Response(...cr)
 ) {
   const dataFromCache = await AsyncStorage.getItem(path);
 
@@ -40,12 +43,21 @@ export async function fetchWithCache(
   if (dataFromCache) {
     console.log("fetch from cache");
     const cacheResponse = JSON.parse(dataFromCache);
-    return getResult(cacheResponse);
+    return getCacheResult(cacheResponse);
   }
 
   return null;
 }
 
+/**
+ * Perform a fetch operation that will recieve JSON object.
+ * It will saves the result of the fetch in the cache storage, and get from it if internet connexion isn't available.
+ * @param path uri to fetch, without plateform. Must be absolute (start with "/" representing the root of the plateform domain)
+ * @param init options of the standard fetch api (@see https://developer.mozilla.org/fr/docs/Web/API/WindowOrWorkerGlobalScope/fetch#Param%C3%A8tres)
+ * @param forceSync If true, fetch on internet preferably. If false, fetch on the the only if there is no cache.
+ * @param platform url to the plateform (ex: "https://recette.opendigitaleducation.com") Uses global plateform configuration as defaut value.
+ * @returns the Js reprentation of the JSON data. Use it directly.
+ */
 export async function fetchJSONWithCache(
   path: string,
   init: any = {},
@@ -60,36 +72,4 @@ export async function fetchJSONWithCache(
     r => r.json(),
     cr => cr.body
   );
-}
-
-export async function fetchJSONWithCache2(
-  path: string,
-  init: any = {},
-  forceSync: boolean = true,
-  platform: string = Conf.platform
-) {
-  const dataFromCache = await AsyncStorage.getItem(path);
-
-  if (Connection.isOnline && (forceSync || !dataFromCache)) {
-    console.log("fetch from web");
-    const response = await fetch(`${platform}${path}`, init);
-    const cacheResponse = {
-      body: await response.clone().json(),
-      init: {
-        headers: response.headers,
-        status: response.status,
-        statusText: response.statusText
-      }
-    };
-    await AsyncStorage.setItem(path, JSON.stringify(cacheResponse));
-    return response.json();
-  }
-
-  if (dataFromCache) {
-    console.log("fetch from cache");
-    const cacheResponse = JSON.parse(dataFromCache);
-    return cacheResponse.body;
-  }
-
-  return null;
 }
