eb75464a040d4cc2dfb8e0763e3efb10649820bd
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-08-21 18:26:16 +0200

    fix: profile header

--- app/auth/AccountNavigation.tsx
+++ app/auth/AccountNavigation.tsx
@@ -1,22 +1,24 @@
-import * as React from "react"
+import * as React from "react";
 import { StackNavigator } from "react-navigation";
-import NotificationsSettings, { NotificationsSettingsHeader } from "./containers/NotificationsSettings";
-import UserProfile from "./containers/UserProfile";
-import I18n from 'react-native-i18n';
-import { navOptions } from "../utils/navHelper";
 
-export default StackNavigator(
-	{
-		userProfile: {
-			screen: UserProfile,
-			navigationOptions: ({ navigation }) => navOptions({ title: I18n.t('Profile') }, navigation)
-		},
-		notificationsSettings: {
-			screen: NotificationsSettings,
-			navigationOptions: ({ navigation }) => ({
-				header: <NotificationsSettingsHeader navigation={ navigation }/>,
-				tabBarVisible: false
-			})
-		}
-	}
-)
+import NotificationsSettings, {
+  NotificationsSettingsHeader
+} from "./containers/NotificationsSettings";
+import UserProfile, { UserProfileHeader } from "./containers/UserProfile";
+
+export default StackNavigator({
+  userProfile: {
+    navigationOptions: ({ navigation }) => ({
+      header: <UserProfileHeader navigation={navigation} />
+    }),
+    screen: UserProfile
+  },
+
+  notificationsSettings: {
+    navigationOptions: ({ navigation }) => ({
+      header: <NotificationsSettingsHeader navigation={navigation} />,
+      tabBarVisible: false
+    }),
+    screen: NotificationsSettings
+  }
+});
