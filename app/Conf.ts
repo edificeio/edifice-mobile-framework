a7532230a5740a8ed2296d3d449cd5bdd10c7da5
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-10-11 11:25:53 +0200

    [build] using recette-ent77

--- app/Conf.ts
+++ app/Conf.ts
@@ -1,6 +1,6 @@
 export const Conf = {
   authLoginStore: "AUTH_LOGIN_STORE1",
-  platform: "https://recette.opendigitaleducation.com",
+  platform: "https://recette-ent77.opendigitaleducation.com",
 
   mixpanelTokenAndroid: "c82f3785bad1015243c64ad254086189",
   mixpanelTokenIOS: "c82f3785bad1015243c64ad254086189"
