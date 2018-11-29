3f5b41f1999c912a8705ee8f7cc4864df26918a8
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-11-29 12:37:35 +0100

    [fix][ui] ButtonLine was crashing

--- app/ui/ButtonLine.tsx
+++ app/ui/ButtonLine.tsx
@@ -1,31 +1,52 @@
-import style from "glamorous-native"
+import style from "glamorous-native";
 import * as React from "react";
 import { RowProperties } from ".";
 import I18n from "i18n-js";
 import { Icon } from "./icons/Icon";
 
 const Container = style.touchableOpacity({
-	alignItems: 'center',
-	flexDirection: 'row',
-	backgroundColor: 'white',
-	borderBottomWidth: 1,
-	borderColor: '#ddd',
-	height: 46,
-	justifyContent: 'flex-start',
-	marginTop: 20,
-	paddingHorizontal: 15
+  alignItems: "center",
+  flexDirection: "row",
+  backgroundColor: "white",
+  borderBottomWidth: 1,
+  borderColor: "#ddd",
+  height: 46,
+  justifyContent: "flex-start",
+  marginTop: 20,
+  paddingHorizontal: 15
 });
 
-const LinkStyle = style.text({
-	fontSize: 14,
-	flex: 1
-}, ({ color }) => ({
-	color: color || '#414355'
-}));
+const LinkStyle = style.text(
+  {
+    fontSize: 14,
+    flex: 1
+  },
+  ({ color }) => ({
+    color: color || "#414355"
+  })
+);
 
-export const ButtonLine = ({ onPress, title, color, hideIcon }: { onPress: () => any, title: string, color?: string, hideIcon?: boolean }) => (
-	<Container onPress={() => onPress()}> {/* FIXME: a ButtonLine without onClick prop will raise an error. Fire an event up or use onPress={onPress}. */}
-		<LinkStyle color={ color }>{ I18n.t(title) }</LinkStyle> {/* FIXME: This UI button force to use translation. It shouldn't. */}
-		{ !hideIcon && <Icon name="arrow_down" color={ "#868CA0" } style={{ transform: [{ rotate: '270deg'}]}} />}
-	</Container>
-)
+export const ButtonLine = ({
+  onPress,
+  title,
+  color,
+  hideIcon
+}: {
+  onPress: () => any;
+  title: string;
+  color?: string;
+  hideIcon?: boolean;
+}) => (
+  <Container onPress={() => onPress()}>
+    {/* FIXME: a ButtonLine without onClick prop will raise an error. Fire an event up or use onPress={onPress}. */}
+    <LinkStyle color={color}>{I18n.t(title)}</LinkStyle>
+    {/* FIXME: This UI button force to use translation. It shouldn't. */}
+    {!hideIcon && (
+      <Icon
+        name="arrow_down"
+        color={"#868CA0"}
+        style={{ transform: [{ rotate: "270deg" }] }}
+      />
+    )}
+  </Container>
+);
