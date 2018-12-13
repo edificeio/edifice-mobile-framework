9ce93ebcd70d5b696dab07a116844b262a6a479b
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-12-13 11:37:38 +0100

    [build] test without prebuild

--- appcenter-pre-build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Set up React native JS bundle (PROD ONLY)
-./node_modules/.bin/react-native bundle --platform android --dev true --entry-file index.js --bundle-output android/app/build/intermediates/assets/debug/index.android.bundle --assets-dest android/app/build/intermediates/res/merged/debug
