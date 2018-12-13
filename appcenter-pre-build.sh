45025784b48f1f24e8634b287d8554f258d6df31
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-12-13 15:08:24 +0100

    [build] appcenter : prebuild can now use dev and prod environment

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-./node_modules/.bin/react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/release
+if [ "$APPCENTER_BRANCH" == "dev" ];
+then
+    echo "Generate JS bundle and assets for Debug environment"
+    ./node_modules/.bin/react-native bundle --platform android --dev true --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/debug
+else
+    echo "Generate JS bundle and assets for Release environment"
+    ./node_modules/.bin/react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/release
+fi
