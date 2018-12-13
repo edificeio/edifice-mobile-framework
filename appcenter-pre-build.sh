75cc6ae2f66479c235134b9b8de6636f102b9de0
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-12-13 15:14:33 +0100

    [build] appcenter : user dev env for appcenter branch

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$APPCENTER_BRANCH" == "dev" ];
+if [ "$APPCENTER_BRANCH" == "dev" || "$APPCENTER_BRANCH" == "appcenter" ];
 then
     echo "Generate JS bundle and assets for Debug environment"
     ./node_modules/.bin/react-native bundle --platform android --dev true --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/debug
