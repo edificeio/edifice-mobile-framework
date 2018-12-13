09346f2acb7baac0cdfd80903a008bb3003a3afa
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-12-13 10:49:11 +0100

    [build] new assets and bundle location for appcenter

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Set up React native JS bundle (PROD ONLY)
-./node_modules/.bin/react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/release
+./node_modules/.bin/react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/build/intermediates/assets/release/index.android.bundle --assets-dest android/app/build/intermediates/res/merged/release
