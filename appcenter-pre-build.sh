a6d18bb7d5a421dd77df1c2d828156222f9d6876
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-12-13 12:38:08 +0100

    [build] appcenter prebuild PROD ONLY

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-# Empty script
+./node_modules/.bin/react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/release
