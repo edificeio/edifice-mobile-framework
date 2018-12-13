5bd7ee1c6fe2ca5a81a4188c7a147fe30bcd5be0
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-12-13 15:19:05 +0100

    [build] appcenter : more convenient use of branch name in build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "$APPCENTER_BRANCH" == "dev" || "$APPCENTER_BRANCH" == "appcenter" ];
+if [ "$APPCENTER_BRANCH" == "master" || "$APPCENTER_BRANCH" == "test-prod" ];
 then
     echo "Generate JS bundle and assets for Debug environment"
-    ./node_modules/.bin/react-native bundle --platform android --dev true --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/debug
+    ./node_modules/.bin/react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/release
 else
     echo "Generate JS bundle and assets for Release environment"
-    ./node_modules/.bin/react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/release
+    ./node_modules/.bin/react-native bundle --platform android --dev true --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/debug
 fi
