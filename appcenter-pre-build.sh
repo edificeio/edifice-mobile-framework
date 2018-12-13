baf9fb11ef75f3f1101ea03c2f476477be76eff1
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-12-13 16:18:45 +0100

    [build] appcenter : typo in build script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,9 +2,9 @@
 
 if [ "$APPCENTER_BRANCH" == "master" || "$APPCENTER_BRANCH" == "test-prod" ];
 then
-    echo "Generate JS bundle and assets for Debug environment"
+    echo "Generate JS bundle and assets for Release environment"
     ./node_modules/.bin/react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/release
 else
-    echo "Generate JS bundle and assets for Release environment"
+    echo "Generate JS bundle and assets for Debug environment"
     ./node_modules/.bin/react-native bundle --platform android --dev true --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/debug
 fi
