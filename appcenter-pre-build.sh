3a60e12ce34d474c76f23aed46f5e54b7477ba51
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2018-12-13 11:32:37 +0100

    [build] generate bundle at default location

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Set up React native JS bundle (PROD ONLY)
-./node_modules/.bin/react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/build/intermediates/assets/release/index.android.bundle --assets-dest android/app/build/intermediates/res/merged/release
+./node_modules/.bin/react-native bundle --platform android --dev true --entry-file index.js --bundle-output android/app/build/intermediates/assets/debug/index.android.bundle --assets-dest android/app/build/intermediates/res/merged/debug
