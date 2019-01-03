c2019322d4c7157edd703f039bc931be042a5c2a
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2019-01-03 14:15:14 +0100

    [build] try to remove all yellow boxes in release mode

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 import { AppStore } from './app/AppStore';
 
+if (!__DEV__) { console.disableYellowBox = true; }
 
 AppRegistry.registerComponent('mobileapp', () => AppStore);
