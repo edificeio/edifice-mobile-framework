5b32d903726d486efaf9bb1298a043bf1bdea0a1
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2019-01-09 16:53:05 +0100

    [build] remove yellow boxes for all AppCenter builds

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,15 +3,9 @@
 if [ ! -z "${APPCENTER_ANDROID_MODULE}" ];
 then
     echo "=== Generate Android JS bundle ==="
-
-    if [ "$APPCENTER_BRANCH" == "master" ] || [ "$APPCENTER_BRANCH" == "prod" ] || [ "$APPCENTER_BRANCH" == "preprod" ];
-    then
-        echo "Generate JS bundle and assets for Release environment"
+    echo "Generate JS bundle and assets for Release environment"
         ./node_modules/.bin/react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/release
-    else
-        echo "Generate JS bundle and assets for Debug environment"
-        ./node_modules/.bin/react-native bundle --platform android --dev true --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/build/generated/res/react/debug
-    fi
+
 fi
 
 if [ ! -z "${APPCENTER_XCODE_SCHEME}" ];
