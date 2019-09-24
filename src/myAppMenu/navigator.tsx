cd8c2c0f601e3d8795102d77ea6bfedc25589c16
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2019-09-24 17:27:19 +0200

    fix: no more ghost white header on myApps screen
    UX is slightly different on iOS : header transition is in "screen" mode instead of "float" mode when accessing to a sub-app.
    This is technical reasons. A possible solution is not have two stack navigators (this require merge all stacks of the subapps).

--- src/myAppMenu/navigator.tsx
+++ src/myAppMenu/navigator.tsx
@@ -1,31 +1,32 @@
 import * as React from "react";
-import { createStackNavigator } from "react-navigation";
+import { createStackNavigator, NavigationScreenProp } from "react-navigation";
+import I18n from "i18n-js";
 
-import { AppTitle, Header } from "../ui/headers/Header";
 import { IAppModule } from "../infra/moduleTool";
 import { getRoutes, getModules } from "../navigation/helpers/navBuilder";
 import { standardNavScreenOptions } from "../navigation/helpers/navScreenOptions";
 import MyAppGrid from "./components/MyAppGrid";
 
+const MyAppGridContainer = (modules: IAppModule[]) => createStackNavigator({
+  myAppsGrid: {
+    screen: (props: any) => <MyAppGrid {...props} modules={modules} />,
+    navigationOptions: ({ navigation }: { navigation: NavigationScreenProp<{}> }) =>
+      standardNavScreenOptions(
+        {
+          title: I18n.t("MyApplications")
+        },
+        navigation
+      ),
+  }
+});
+
 export default (apps: string[]) => {
     const filter = (mod: IAppModule) => mod.config.hasRight(apps) && mod.config.group;
     const modules = getModules(filter);
     return createStackNavigator({
-      myApps: {
-        screen: props => <MyAppGrid {...props} modules={modules} />,
-  
-        navigationOptions: ({ navigation }) =>
-          standardNavScreenOptions(
-            {
-              header: (
-                <Header>
-                  <AppTitle>{"Mes Applis"}</AppTitle>
-                </Header>
-              ),
-            },
-            navigation
-          ),
-      },
+      myApps: MyAppGridContainer(modules),
       ...getRoutes(modules)
+    }, {
+      headerMode: "none"
     });
   };
