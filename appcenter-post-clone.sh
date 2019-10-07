d6131c0e8c1e6c8cfb8cd07e64ee38d4777589b3
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2019-10-07 14:11:33 +0200

    revert appcenter-post-clone.sh fix

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -5,15 +5,15 @@
 # (Remove this file if this issue is resolved)
 #!/usr/bin/env bash
 
-echo 'Doing some post-clone stuff...'
+# echo 'Doing some post-clone stuff...'
 
 #This should match what's in Podfile.lock for COCOAPODS:
-COCOAPODS_VER=1.7.5
+# COCOAPODS_VER=1.7.5
 
-echo "Installing CocoaPods version $COCOAPODS_VER"
+# echo "Installing CocoaPods version $COCOAPODS_VER"
 
-sudo gem install cocoapods -v $COCOAPODS_VER
+# sudo gem install cocoapods -v $COCOAPODS_VER
 
-echo "Finished installing CocoaPods version $COCOAPODS_VER"
+# echo "Finished installing CocoaPods version $COCOAPODS_VER"
 
-pod cache clean --all
+# pod cache clean --all
