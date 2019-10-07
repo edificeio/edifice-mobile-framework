f15af29de76a81a4dba1e0de99b9015b5c4188ee
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2019-10-07 11:35:12 +0200

    fix: add `appcenter-post-clone.sh` build script to solve https://github.com/microsoft/react-native-code-push/issues/1687 (Attempt 3)

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -3,4 +3,15 @@
 # Update pod cache cause of an issue with app center
 # See https://github.com/microsoft/react-native-code-push/issues/1687
 # (Remove this file if this issue is resolved)
-pod cache clean --all
+#!/usr/bin/env bash
+
+echo 'Doing some post-clone stuff...'
+
+#This should match what's in Podfile.lock for COCOAPODS:
+COCOAPODS_VER=1.7.5
+
+echo "Installing CocoaPods version $COCOAPODS_VER"
+
+sudo gem install cocoapods -v $COCOAPODS_VER
+
+echo "Finished installing CocoaPods version $COCOAPODS_VER"
