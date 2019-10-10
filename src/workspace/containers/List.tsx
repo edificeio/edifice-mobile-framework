36eef2031f87c05eb15c4140dd9f6d377ac65fb8
Jacques Bonet <jacques.bonet@gmail.com>
2019-10-10 15:51:11 +0200

    feat(workspace): back management feat(workspace): add loading spinner

--- src/workspace/containers/List.tsx
+++ src/workspace/containers/List.tsx
@@ -1,61 +1,87 @@
 import * as React from "react";
 import {connect} from "react-redux";
 import config from "../config";
-import { NavigationScreenProp } from "react-navigation";
-import { standardNavScreenOptions } from "../../navigation/helpers/navScreenOptions";
-import { bindActionCreators } from "redux";
-import { getFolderDocuments } from "../actions/documents";
-import { getSubFolders } from "../actions/folders";
-import List from "../components/List";
-import { View } from "react-native";
+import {bindActionCreators} from "redux";
+import {NavigationScreenProp} from "react-navigation";
+import {standardNavScreenOptions} from "../../navigation/helpers/navScreenOptions";
+import {HeaderAction} from "../../ui/headers/NewHeader";
+import {FlatList, View, ViewStyle} from "react-native";
+import {IEntity, IProps} from "../types/entity";
+import {Entity} from "../components";
+import {fetchWorkspaceList} from "../actions/list";
+import {Loading} from "../../ui";
 
-const mapStateToProps = state => {
-  const documents = config.getLocalState(state).documents.data;
-  const folders = config.getLocalState(state).folders.data;
-  return { documents, folders };
-};
 
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ getFolderDocuments, getSubFolders }, dispatch);
-};
+const HeaderBackAction = ({navigation, style}: {
+    navigation: NavigationScreenProp<{}>, style?: ViewStyle
+}) => {
+    const filter = navigation.getParam("filter")
+    const parentId = navigation.getParam("backId")
 
-class ContainerList extends React.PureComponent<any, {}> {
-  static navigationOptions = ({ navigation }: { navigation: NavigationScreenProp<{}> }) => {
-    return standardNavScreenOptions(
-      {
-        title: "Page",
-      },
-      navigation
-    );
-  };
-
-  private refreshScreen = () => {
-    this.props.getFolderDocuments(this.props.navigation.getParam("id"));
-    this.props.getSubFolders(this.props.navigation.getParam("id"));
-  }
-
-  private refreshOnGoBack = this.props.navigation.addListener("didFocus", this.refreshScreen)
-
-  public componentWillUnmount() {
-    this.refreshOnGoBack.remove()
-  }
-
-  public render() {
-    if (this.props.folders || this.props.documents) {
-      return (
-        <List
-          navigate={(id, name) => this.props.navigation.push("FolderView", { id, name })}
-          folders={this.props.folders}
-          documents={this.props.documents}
-        />
-      );
-    } else {
-      <View />;
-    }
-  }
+    return (
+        <HeaderAction
+            onPress={() => navigation.navigate({routeName: "Workspace", params: {filter, parentId}, key: Math.random().toString()})}
+            name={"back"} style={style}/>
+    )
 }
 
+
+
+export class List extends React.PureComponent<IProps, {}> {
+    static navigationOptions = ({navigation}: { navigation: NavigationScreenProp<{}> }) => {
+        return standardNavScreenOptions(
+            {
+                title: "Workspace",
+                headerLeft: <HeaderBackAction navigation={navigation}/>,
+            },
+            navigation
+        );
+    };
+
+    public componentDidMount() {
+        this.props.fetchWorkspaceList(
+            {
+                filter: this.props.navigation.getParam("filter"),
+                parentId: this.props.navigation.getParam("parentId")
+            });
+    }
+
+    public onPress(parentId: string) {
+        const filter = this.props.navigation.getParam("filter")
+        const backId = this.props.navigation.getParam("parentId")
+
+        this.props.navigation.navigate({routeName: "Workspace", params: {backId, filter, parentId}, key: parentId})
+    }
+
+    public render() {
+        const {filesFolders, isFetching} = this.props
+
+        if (isFetching)
+            return <Loading/>;
+
+        return (
+            <View>
+                <FlatList
+                    data={Object.values(filesFolders)}
+                    keyExtractor={(item: IEntity) => item.id}
+                    renderItem={({item}) => <Entity {...item} onPress={this.onPress.bind(this)}/>}
+                />
+            </View>
+        )
+    }
+}
+
+const mapStateToProps = (state: any) => {
+    const filesFolders = config.getLocalState(state).filesFolders.data;
+    const {isFetching} = config.getLocalState(state).filesFolders;
+    return {filesFolders, isFetching};
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+    return bindActionCreators({fetchWorkspaceList}, dispatch);
+};
+
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(ContainerList);
+    mapStateToProps,
+    mapDispatchToProps
+)(List);
