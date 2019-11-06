7d093d4a166dd7060d171ed0c55ee6d5556f17c7
ROLLIN Quentin <quentin.rollin@cgi.com>
2019-11-06 18:07:13 +0100

    fix(workspace): empty screen label

--- src/workspace/containers/Items.tsx
+++ src/workspace/containers/Items.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {FlatList, RefreshControl, StyleSheet, View, ViewStyle} from "react-native";
+import { FlatList, RefreshControl, StyleSheet, View, ViewStyle } from "react-native";
 import { connect } from "react-redux";
 import I18n from "i18n-js";
 import { bindActionCreators } from "redux";
@@ -7,40 +7,26 @@ import { NavigationScreenProp } from "react-navigation";
 import config from "../config";
 import { standardNavScreenOptions } from "../../navigation/helpers/navScreenOptions";
 import { HeaderAction } from "../../ui/headers/NewHeader";
-import {EVENT_TYPE, FilterId, IItem, IItemsProps, IState} from "../types";
+import { EVENT_TYPE, FilterId, IItem, IItemsProps, IState } from "../types";
 import { Item } from "../components";
 import { fetchWorkspaceList } from "../actions/list";
 import { CommonStyles } from "../../styles/common/styles";
 import { layoutSize } from "../../styles/common/layoutSize";
 import ConnectionTrackingBar from "../../ui/ConnectionTrackingBar";
-import {renderEmptyList} from "../utils/empty";
-
+import { getEmptyScreen } from "../utils/empty";
+import { PageContainer } from "../../ui/ContainerContent";
 
 const styles = StyleSheet.create({
-  mainPanel: {
-    backgroundColor: CommonStyles.lightGrey,
-    flex: 1
-  },
   separator: {
     borderBottomColor: CommonStyles.borderColorLighter,
     borderBottomWidth: StyleSheet.hairlineWidth,
-    marginLeft: layoutSize.LAYOUT_84
+    marginLeft: layoutSize.LAYOUT_84,
   },
-  endSeparator: {
-    borderBottomColor: CommonStyles.borderColorLighter,
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  }
 });
 
-
-const HeaderBackAction = ({ navigation, style }: {
-  navigation: NavigationScreenProp<{}>, style?: ViewStyle
-}) => {
-  return (
-    <HeaderAction onPress={() => navigation.pop()} name={"back"} style={style}/>
-  )
+const HeaderBackAction = ({ navigation, style }: { navigation: NavigationScreenProp<{}>; style?: ViewStyle }) => {
+  return <HeaderAction onPress={() => navigation.pop()} name={"back"} style={style} />;
 };
-
 
 export class Items extends React.PureComponent<IItemsProps> {
   redirected = false;
@@ -49,7 +35,7 @@ export class Items extends React.PureComponent<IItemsProps> {
     return standardNavScreenOptions(
       {
         title: navigation.getParam("title") || "Workspace",
-        headerLeft: <HeaderBackAction navigation={navigation}/>,
+        headerLeft: <HeaderBackAction navigation={navigation} />,
       },
       navigation
     );
@@ -75,11 +61,10 @@ export class Items extends React.PureComponent<IItemsProps> {
   }
 
   public makeRequest() {
-    this.props.fetchWorkspaceList(
-      {
-        filter: this.props.navigation.getParam("filter"),
-        parentId: this.props.navigation.getParam("parentId") || FilterId.root
-      });
+    this.props.fetchWorkspaceList({
+      filter: this.props.navigation.getParam("filter"),
+      parentId: this.props.navigation.getParam("parentId") || FilterId.root,
+    });
   }
 
   public onEvent(type: EVENT_TYPE, item: IItem) {
@@ -91,51 +76,42 @@ export class Items extends React.PureComponent<IItemsProps> {
 
         isFolder
           ? this.props.navigation.push("Workspace", { filter, parentId, title })
-          : this.props.navigation.push("WorkspaceDetails", { item, title })
-        return
+          : this.props.navigation.push("WorkspaceDetails", { item, title });
+        return;
     }
   }
 
-  private sortItems( a: IItem, b: IItem): number {
+  private sortItems(a: IItem, b: IItem): number {
     return a.isFolder
       ? b.isFolder
         ? a.name.toLowerCase().localeCompare(b.name.toLowerCase())
         : -1
       : b.isFolder
-        ? 1
-        : a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+      ? 1
+      : a.name.toLowerCase().localeCompare(b.name.toLowerCase());
   }
 
   public render(): React.ReactNode {
     const { items, isFetching } = this.props;
     const values = Object.values(items);
     const parentId = this.props.navigation.getParam("parentId") || null;
-    const itemsArray = parentId === FilterId.root || parentId === null
-      ? values
-      : values.sort((a, b) => this.sortItems(a, b));
-
-    if (!isFetching && itemsArray.length === 0)
-      return renderEmptyList(parentId, this.makeRequest.bind(this));
+    const itemsArray =
+      parentId === FilterId.root || parentId === null ? values : values.sort((a, b) => this.sortItems(a, b));
 
     return (
-      <View style={styles.mainPanel}>
+      <PageContainer>
         <ConnectionTrackingBar />
-        <View style={styles.endSeparator}>
-          <FlatList
-            data={itemsArray}
-            ItemSeparatorComponent={() => <View style={styles.separator}/>}
-            keyExtractor={(item: IItem) => item.id}
-            refreshControl={
-              <RefreshControl
-                refreshing={isFetching}
-                onRefresh={this.makeRequest.bind(this)}
-              />
-            }
-            renderItem={({ item }) => <Item {...item} onEvent={this.onEvent.bind(this)}/>}
-          />
-        </View>
-      </View>
-    )
+        <FlatList
+          contentContainerStyle={!isFetching && itemsArray.length === 0 ? { flex: 1 } : null}
+          data={itemsArray}
+          ListEmptyComponent={() => (isFetching ? null : getEmptyScreen(parentId))}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          keyExtractor={(item: IItem) => item.id}
+          refreshControl={<RefreshControl refreshing={isFetching} onRefresh={this.makeRequest.bind(this)} />}
+          renderItem={({ item }) => <Item {...item} onEvent={this.onEvent.bind(this)} />}
+        />
+      </PageContainer>
+    );
   }
 }
 
@@ -147,7 +123,6 @@ const mapStateToProps = (state: any, props: any) => {
   return { items: stateItems[parentId] || {}, isFetching };
 };
 
-
 const mapDispatchToProps = (dispatch: any) => {
   return bindActionCreators({ fetchWorkspaceList }, dispatch);
 };
@@ -156,4 +131,3 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Items);
-
--- src/workspace/utils/empty.tsx
+++ src/workspace/utils/empty.tsx
@@ -1,102 +1,59 @@
 import * as React from "react";
-import {EmptyScreen} from "../../ui/EmptyScreen";
+import { EmptyScreen } from "../../ui/EmptyScreen";
 import I18n from "i18n-js";
-import {FilterId} from "../types/filters";
-import {FlatList, RefreshControl, StyleSheet, View} from "react-native";
-import {CommonStyles} from "../../styles/common/styles";
-import {DEVICE_HEIGHT, DEVICE_WIDTH, layoutSize} from "../../styles/common/layoutSize";
+import { FilterId } from "../types/filters";
 
-const styles = StyleSheet.create({
-  mainPanel: {
-    backgroundColor: "#FFF6F8",
-    flex: 1
-  },
-});
-
-export const renderEmptyTrash = (): React.ReactNode => {
-  return (
-    <View style={{ flex: 1, flexGrow: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <EmptyScreen
-        imageSrc={require("../../../assets/images/empty-screen/empty-trash.png")}
-        imgWidth={265}
-        imgHeight={336}
-        text={I18n.t("trash-emptyScreenText")}
-        title={I18n.t("trash-emptyScreenTitle")}
-        scale={0.76}
-      />
-    </View>
-  );
-}
-
-// export const renderEmptyFolder = (): React.ReactNode => {
-//   return (
-//     <View style={{ width: DEVICE_WIDTH(), height: DEVICE_HEIGHT() - layoutSize.LAYOUT_110, justifyContent: 'center', alignItems: 'center'}}>
-//       <EmptyScreen
-//         imageSrc={require("../../../assets/images/empty-screen/empty-folder.png")}
-//         imgWidth={500}
-//         imgHeight={500}
-//         text=""
-//         title=""
-//         scale={0.7}
-//       />
-//     </View>
-//   );
-// }
-
-export const renderEmptyWorkspace = (parentId: string): React.ReactNode => {
-  const text = parentId === FilterId.owner
-    ? I18n.t("owner-emptyScreenText")
-    : parentId === FilterId.protected
-      ? I18n.t("protected-emptyScreenText")
-      : I18n.t("share-emptyScreenText")
-  const title = parentId === FilterId.owner
-    ? I18n.t("owner-emptyScreenTitle")
-    : parentId === FilterId.protected
-      ? I18n.t("protected-emptyScreenTitle")
-      : I18n.t("share-emptyScreenTitle")
-
-  return (
-    <View style={{ flex: 1, flexGrow: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <EmptyScreen
+export const getEmptyScreen = (parentId: string): React.ReactElement => {
+  switch (parentId) {
+    case FilterId.owner: {
+      return (<EmptyScreen
         imageSrc={require("../../../assets/images/empty-screen/empty-workspace.png")}
         imgWidth={400}
         imgHeight={316}
-        text={text}
-        title={title}
+        text={I18n.t("owner-emptyScreenText")}
+        title={I18n.t("owner-emptyScreenTitle")}
         scale={0.76}
-      />
-    </View>
-  );
-}
-
-export const renderEmptyList = (parentId: string, onRefresh: () => void): React.ReactNode => {
-
-/*  return parentId === FilterId.trash
-    ? renderEmptyTrash()
-    : parentId in FilterId
-      ? renderEmptyWorkspace(parentId)
-      : renderEmptyFolder()
-*/
-  return (
-    <View style={styles.mainPanel}>
-        <FlatList
-          data={renderEmptyContent(parentId) as any}
-          keyExtractor={(item: any) => item.id}
-          refreshControl={
-            <RefreshControl
-              refreshing={false}
-              onRefresh={onRefresh}
-            />
-          }
-          renderItem={ ({item}) => (item as any).render()}
-        />
-      </View>
-  )
-}
-
-export const renderEmptyContent = (parentId: string): React.ReactNode => {
-
-  return parentId === FilterId.trash
-    ? [{ id: "1", render: () => renderEmptyTrash()}]
-    : [{id: "1", render: () => renderEmptyWorkspace(parentId)}]
+      />)
+    }
+    case FilterId.protected: {
+      return (<EmptyScreen
+        imageSrc={require("../../../assets/images/empty-screen/empty-workspace.png")}
+        imgWidth={400}
+        imgHeight={316}
+        text={I18n.t("protected-emptyScreenText")}
+        title={I18n.t("protected-emptyScreenTitle")}
+        scale={0.76}
+      />)
+    }
+    case FilterId.shared: {
+      return (<EmptyScreen
+        imageSrc={require("../../../assets/images/empty-screen/empty-workspace.png")}
+        imgWidth={400}
+        imgHeight={316}
+        text={I18n.t("share-emptyScreenText")}
+        title={I18n.t("share-emptyScreenTitle")}
+        scale={0.76}
+      />)
+    }
+    case FilterId.trash: {
+      return (<EmptyScreen
+        imageSrc={require("../../../assets/images/empty-screen/empty-trash.png")}
+        imgWidth={400}
+        imgHeight={316}
+        text={I18n.t("trash-emptyScreenText")}
+        title={I18n.t("trash-emptyScreenTitle")}
+        scale={0.76}
+      />)
+    }
+    default: {
+      return (<EmptyScreen
+        imageSrc={require("../../../assets/images/empty-screen/empty-workspace.png")}
+        imgWidth={400}
+        imgHeight={316}
+        text={I18n.t("subFolder-emptyScreenText")}
+        title={I18n.t("subFolder-emptyScreenTitle")}
+        scale={0.76}
+      />)
+    }
+  }
 }
