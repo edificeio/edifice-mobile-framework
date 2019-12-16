815d0c438e63e62af182a20f5213add22c7ef8ae
Jacques Bonet <jacques.bonet@gmail.com>
2019-12-16 14:08:30 +0100

    fix(workspace/intent): call Linking.getIntent() before getFilePath(), contentUri comparison change, state optimisation change

--- src/infra/withLinkingAppWrapper.tsx
+++ src/infra/withLinkingAppWrapper.tsx
@@ -1,24 +1,28 @@
 import * as React from "react";
 import RNFileShareIntent from "react-native-file-share-intent";
-import { AppState, AppStateStatus, Platform } from "react-native";
+import {AppState, AppStateStatus, Linking, Platform} from "react-native";
 import { nainNavNavigate } from "../navigation/helpers/navHelper";
 import { FilterId } from "../workspace/types/filters";
 import I18n from "i18n-js";
+import {ContentUri} from "../types/contentUri";
 
 export interface IWrapperState {
-  contentUri: string | null;
-  handled: boolean;
+  contentUri:ContentUri[];
 }
+
+const initState = {
+  contentUri: [{
+    mime: "",
+    name: "",
+    uri: ""
+  }]
+}
+
 
 export default function withLinkingAppWrapper(WrappedComponent: React.Component): React.Component {
   class HOC extends React.Component<{}, IWrapperState> {
-    constructor(props) {
-      super(props);
-      this.state = {
-        contentUri: null,
-        handled: false,
-      };
-    }
+    state = initState;
+    handled: boolean = false;
 
     public componentDidMount() {
       AppState.addEventListener("change", this._handleAppStateChange);
@@ -30,9 +34,7 @@ export default function withLinkingAppWrapper(WrappedComponent: React.Component)
     }
 
     public componentDidUpdate() {
-      if (this.state.contentUri && !this.state.handled) {
-        this._handleContentUri();
-      }
+      this._handleContentUri();
     }
 
     _handleAppStateChange = (nextAppState: AppStateStatus) => {
@@ -44,38 +46,48 @@ export default function withLinkingAppWrapper(WrappedComponent: React.Component)
       }
     };
 
-    _checkContentUri = () => {
-      if (RNFileShareIntent && Platform.OS === "android" && !this.state.handled) {
-        RNFileShareIntent.getFilePath((contentUri: any) => {
-          if (contentUri && this.state.contentUri != contentUri) {
-            this.setState({ contentUri, handled: true }); // permit to have componentDidUpdate
+    _checkContentUri = async () => {
+      if (RNFileShareIntent && Platform.OS === "android" && !this.handled) {
+        const url = await this._getInitialUrl();
+        RNFileShareIntent.getFilePath((contentUri: ContentUri[]) => {
+          if (contentUri && contentUri.length > 0 && contentUri[0].uri != this.state.contentUri[0].uri) {
+            this.setState({ contentUri}); // permit to have componentDidUpdate
           }
         });
       }
     };
 
     _handleContentUri = () => {
-      nainNavNavigate("Workspace", {
-        contentUri: null,
-        filter: FilterId.root,
-        parentId: FilterId.root,
-        title: I18n.t("workspace"),
-        childRoute: "Workspace",
-        childParams: {
-          parentId: "owner",
-          filter: FilterId.owner,
-          title: I18n.t("owner"),
-          contentUri: this.state.contentUri,
-        },
-      });
+      if (this.state.contentUri[0].uri.length > 0 && !this.handled) {
+        this.handled = true;
+        nainNavNavigate("Workspace", {
+          contentUri: null,
+          filter: FilterId.root,
+          parentId: FilterId.root,
+          title: I18n.t("workspace"),
+          childRoute: "Workspace",
+          childParams: {
+            parentId: "owner",
+            filter: FilterId.owner,
+            title: I18n.t("owner"),
+            contentUri: this.state.contentUri,
+          },
+        });
+      }
     };
 
     _clearContentUri = () => {
       if (Platform.OS === 'android') {
         RNFileShareIntent.clearFilePath();
       }
-      this.setState({ contentUri: null, handled: false }); // permit to have componentDidUpdate
+      this.handled = false;
     };
+
+
+    _getInitialUrl = async () => {
+      const url = await Linking.getInitialURL()
+      return url
+    }
 
     render() {
       return <WrappedComponent {...this.props} />;
