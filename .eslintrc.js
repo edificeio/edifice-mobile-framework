661fc4eac25d6336e373a593e14eca22db238785
Jacques Bonet <jacques.bonet@gmail.com>
2020-01-18 22:20:00 +0100

    feature(app): correct syntax error eslingconfig.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,3 @@
 module.exports = {
-<<<<<<< c6ea32b943152b0b813acb9e26feb67ca2a34f1f
-<<<<<<< c6ea32b943152b0b813acb9e26feb67ca2a34f1f
-    extends: "@react-native-community"
-}
-=======
-  root: true,
-  extends: '@react-native-community',
-  rules: {
-    'react-native/no-inline-styles': 0,
-    "quotes": [1, "double", { "avoidEscape": true }]
-  }
-=======
-  extends: "universe/native",
->>>>>>> feature(app): new eslint/prettier config
+    extends: "universe/native",
 };
->>>>>>> feature(infra): new redux action component simplifying redux ODE action construction feature(workspace): folder creation
