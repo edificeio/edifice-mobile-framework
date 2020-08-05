414f9a2420650ac72e7fc94f28209e124f9dfea8
CAULONQUE <cyril.caulonque@cgi.com>
2020-08-05 17:31:05 +0200

    fix(zimbra): pressing button instead of swiping to open drawer nav

--- src/zimbra/containers/DrawerNavigatorWrapper.tsx
+++ src/zimbra/containers/DrawerNavigatorWrapper.tsx
@@ -1,9 +1,9 @@
 import I18n from "i18n-js";
 import React from "react";
-import { NavigationScreenProp } from "react-navigation";
-import { createDrawerNavigator } from "react-navigation-drawer";
+import { createDrawerNavigator, NavigationDrawerProp } from "react-navigation-drawer";
 
 import { standardNavScreenOptions } from "../../navigation/helpers/navScreenOptions";
+import { HeaderAction } from "../../ui/headers/NewHeader";
 import DrawerMenuContainer from "./DrawerMenu";
 import MailList from "./MailList";
 
@@ -17,15 +17,24 @@ const DrawerNavigatorComponent = createDrawerNavigator(
   },
   {
     contentComponent: DrawerMenuContainer,
+    edgeWidth: -1,
   }
 );
 
 export default class DrawerNavigatorWrapper extends React.Component<any, any> {
   static router = DrawerNavigatorComponent.router;
-  static navigationOptions = ({ navigation }: { navigation: NavigationScreenProp<any> }) => {
+  static navigationOptions = ({ navigation }: { navigation: NavigationDrawerProp<any> }) => {
     return standardNavScreenOptions(
       {
         title: I18n.t("zimbra-inbox"),
+        headerLeft: (
+          <HeaderAction
+            name="menu"
+            onPress={() => {
+              navigation.toggleDrawer();
+            }}
+          />
+        ),
       },
       navigation
     );
