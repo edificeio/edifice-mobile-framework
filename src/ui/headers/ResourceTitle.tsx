e8950abef4ea1607abb0396657772c83efa5ec2f
Camille Meshorer <camille.meshorer@opendigitaleducation.com>
2020-08-19 11:20:34 +0200

    fix(timeline): #38009, trimmed header title

--- src/ui/headers/ResourceTitle.tsx
+++ src/ui/headers/ResourceTitle.tsx
@@ -4,7 +4,7 @@ import { Title, SubTitle } from './Header';
 
 export const ResourceTitle = ({ title, subTitle }: { title: string, subTitle: string }) => (
 	<View style={{ justifyContent: "center", alignItems: "center", flex: 1, height: 60 }}>
-		<Title numberOfLines={2} style={{ lineHeight: 18 }}>{title}</Title>
+		<Title numberOfLines={1} style={{ lineHeight: 18 }}>{title}</Title>
 		<SubTitle numberOfLines={1} style={{ lineHeight: 14 }}>{subTitle}</SubTitle>
 	</View>
 )
