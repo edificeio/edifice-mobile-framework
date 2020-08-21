4383d786a8b5b8b9a8ed4cacfc36cec0e414104b
rollinq <quentin.rollin@cgi.com>
2020-08-21 17:32:26 +0200

    feat(zimbra): update zimbra module declaration

--- src/zimbra/index.ts
+++ src/zimbra/index.ts
@@ -1,6 +1,7 @@
 import zimbraConfig from "./config";
 import mainComp from "./navigator";
 import mainReducer from "./reducers";
+import { registerModule } from "../AppModules";
 
 // Main component
 export const root = mainComp;
@@ -11,8 +12,17 @@ export const reducer = mainReducer;
 // Route
 export const route = zimbraConfig.createRoute(root);
 
-export default {
+const module = {
   reducer,
   root,
   route,
 };
+export default module;
+
+registerModule(
+  {
+    order: 6,
+    config: require("./config").default,
+    module
+  }
+)
