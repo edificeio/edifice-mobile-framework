fd64fa4122c32ee71b00ca81cf80bbc81eba297d
rollinq <quentin.rollin@cgi.com>
2020-09-02 11:56:12 +0200

    fix: handle zimbra access right

--- src/zimbra/config.ts
+++ src/zimbra/config.ts
@@ -6,5 +6,5 @@ export default new FunctionalModuleConfig({
   name: "zimbra",
   displayName: "Conversation",
   iconName: "mail",
-  hasRight: app => app.name === "Messagerie" || app.name === "Zimbra",
+  apiName: "Zimbra"
 });
