610583c840f390e47fa1528a2a9bb40e973dbb7f
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-01-06 17:01:21 +0100

    chore: compatibility with AppCenter 4.0.2 BREAKING CHANGES: Update Cocoa Pods to latest Version (10.x)

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,18 +1,1 @@
 #!/usr/bin/env bash
-
-# Update pod cache cause of an issue with app center
-# See https://github.com/microsoft/react-native-code-push/issues/1687
-# (Remove this file if this issue is resolved)
-#!/usr/bin/env bash
-
-echo 'Installing CocoaPods 1.7.5'
-echo 'Remove this script when AppCenter module will be compatible with CocoaPods 1.8.x'
-
-# Remove installed cocoapods version
-sudo gem uninstall cocoapods -a -x
-
-# Install v1.7.5
-sudo gem install cocoapods -v 1.7.5
-
-# Pod cache clean
-# pod cache clean --all # NO WAY !!
