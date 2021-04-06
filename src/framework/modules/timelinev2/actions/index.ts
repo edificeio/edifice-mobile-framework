fcede98fc2f817f5fc41a706fb83311741384519
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-04-06 12:20:10 +0200

    wip

--- src/framework/modules/timelinev2/actions/index.ts
+++ src/framework/modules/timelinev2/actions/index.ts
@@ -2,22 +2,23 @@
  * Timeline v2 actions
  */
 
-import { Dispatch } from "redux";
 import { ThunkDispatch } from "redux-thunk";
 import { getUserSession } from "../../../session";
 import moduleConfig from "../moduleConfig";
-import { getAreNotificationTypesLoaded } from "../reducer/notifTypes";
+import * as notifTypesState from "../reducer/notifTypes";
 import { loadNotificationsTypesAction } from "./notifTypes";
 
 export const loadNotificationsAction = () => async (dispatch: ThunkDispatch<any, any, any>, getState: () => any) => {
   try {
-    console.log("begin loadNotificationsAction");
     const session = getUserSession(getState());
-    const state = moduleConfig.getState(getState());
-    console.log(state.notifTypes, getAreNotificationTypesLoaded(state.notifTypes));
-    if (!getAreNotificationTypesLoaded(state.notifTypes)) {
+    let state = moduleConfig.getState(getState());
+    // 1 - Load notification types if necessary
+    if (!notifTypesState.getAreNotificationTypesLoaded(state.notifTypes)) {
       await dispatch(loadNotificationsTypesAction());
     }
+    state = moduleConfig.getState(getState());
+    // 2 - Load notification from filtered types
+    console.log(notifTypesState.getNotificationFilterList(state.notifTypes));
   } catch (e) {
     console.warn(`[${moduleConfig.name}] loadNotificationsAction failed`, e);
   }
--- src/framework/modules/timelinev2/reducer/notifTypes.ts
+++ src/framework/modules/timelinev2/reducer/notifTypes.ts
@@ -1,6 +1,7 @@
-import { IEntcoreApp } from "../../../moduleTool";
 import { AsyncState, createAsyncActionTypes, createSessionAsyncReducer } from "../../../redux/async";
 import moduleConfig from "../moduleConfig";
+
+// State definition
 
 export interface IEntcoreNotificationType {
     type: string;
@@ -12,12 +13,38 @@ export interface IEntcoreNotificationType {
     "push-notif": boolean;
     key: string;
 }
-export type INotificationType = IEntcoreNotificationType & IEntcoreApp;
+
+export interface INotificationFilter {
+    type: string;
+    "app-name": string | null;
+    "app-address": string | null;
+    "push-notif": boolean;
+    i18n: string;
+}
+
+export type INotifTypesState = IEntcoreNotificationType[];
 
 export const actionTypes = createAsyncActionTypes(moduleConfig.namespaceActionType("NOTIFICATION_TYPES"));
 
-const initialState: INotificationType[] = [];
+const initialState: INotifTypesState = [];
 
 export default createSessionAsyncReducer(initialState, actionTypes);
 
-export const getAreNotificationTypesLoaded = (state: AsyncState<INotificationType[]>) => state.lastSuccess;
+// State getters
+
+export const getAreNotificationTypesLoaded = (state: AsyncState<INotifTypesState>) => state.lastSuccess;
+
+export const getNotificationFilterList = (state: AsyncState<INotifTypesState>) => state.data.reduce(
+    (acc: { [type: string]: INotificationFilter }, v) => {
+        acc = {
+            ...acc,
+            [v.type]: ({
+                type: v.type,
+                "app-name": v["app-name"],
+                "app-address": v["app-address"],
+                "push-notif": acc[v.type] && acc[v.type]["push-notif"] || v["push-notif"],
+                i18n: `timeline.notifType.${v.type}`
+            } as INotificationFilter)
+        }
+        return acc;
+    }, {});
