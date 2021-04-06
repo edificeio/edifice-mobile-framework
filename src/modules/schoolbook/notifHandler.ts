53919b3eff3517f18f1c533aea8ee94e06c0c61a
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-04-06 12:20:11 +0200

    chore: add empty schoolbook module

---
+++ src/modules/schoolbook/notifHandler.ts
@@ -0,0 +1,38 @@
+/**
+ * Schoolbook notif handler
+ */
+
+import type { IResourceUriNotification, ITimelineNotification } from "../../framework/notifications";
+
+import { NotifHandlerThunkAction, registerNotifHandlers } from "../../framework/notifications/routing";
+import { mainNavNavigate } from "../../navigation/helpers/navHelper";
+
+export interface ISchoolbookNotification extends ITimelineNotification, IResourceUriNotification {}
+
+const handleSchoolbookNotificationAction: NotifHandlerThunkAction = notification => async (dispatch, getState) => {
+	mainNavNavigate('schoolbook/details', {
+		notification
+	});
+	return {
+		managed: true,
+		trackInfo: { action: "Schoolbook", name: `${notification.type}.${notification["event-type"]}` }
+	}
+}
+
+export default () => registerNotifHandlers([
+	{
+		type: 'SCHOOLBOOK',
+		"event-type": 'PUBLISH',
+		notifHandlerAction: handleSchoolbookNotificationAction
+	},
+	{
+		type: 'SCHOOLBOOK',
+		"event-type": 'WORD-SHARED',
+		notifHandlerAction: handleSchoolbookNotificationAction
+	},
+	{
+		type: 'SCHOOLBOOK',
+		"event-type": 'ACKNOWLEDGE',
+		notifHandlerAction: handleSchoolbookNotificationAction
+	}
+]);
