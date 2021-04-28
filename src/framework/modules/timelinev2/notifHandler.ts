e5509482962bbda2c3e38ebdd4f758ea11aa0214
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-04-28 15:05:25 +0200

    fix(timeline): #44583 flash msgs are now redirected to the timeline

---
+++ src/framework/modules/timelinev2/notifHandler.ts
@@ -0,0 +1,27 @@
+/**
+ * Timeline notif handler
+ */
+
+import { mainNavNavigate } from "../../../navigation/helpers/navHelper";
+import { NotifHandlerThunkAction, registerNotifHandlers } from "../../notifications/routing";
+
+const handleFlashMsgNotificationAction: NotifHandlerThunkAction = notification => async (dispatch, getState) => {
+	mainNavNavigate('timeline', {
+		notification
+	});
+	return {
+		managed: 1,
+		trackInfo: { action: "Timeline", name: `${notification.type}.${notification["event-type"]}` }
+	}
+}
+
+// Note : by default, unmanaged notifications leads to TimelineScreen.
+// But, if this default behaviour is changed in the future, this notifHandler will prevent side-effect.
+
+export default () => registerNotifHandlers([
+	{
+		type: 'TIMELINE',
+		"event-type": 'SEND-FLASH-MESSAGE-PUSH',
+		notifHandlerAction: handleFlashMsgNotificationAction
+	}
+]);
