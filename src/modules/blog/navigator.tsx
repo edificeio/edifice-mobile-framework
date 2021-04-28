d178ac5d2d05f47f3e6fd1fc6473f7fecc282855
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-04-28 15:04:44 +0200

    wip

--- src/modules/blog/navigator.tsx
+++ src/modules/blog/navigator.tsx
@@ -1,11 +1,15 @@
 import { createStackNavigator } from "react-navigation-stack";
 
 import BlogPostDetailsScreen from "./screens/BlogPostDetailsScreen";
+import BlogSelectScreen from "./screens/BlogSelectScreen";
 
 export default () => createStackNavigator(
     {
         "blog/details": {
             screen: BlogPostDetailsScreen
+        },
+        "blog/select": {
+            screen: BlogSelectScreen
         },
     },
     {
