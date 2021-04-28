bc8ba9c539486d159ea13006a91a673a9dc8fea1
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-04-28 15:04:43 +0200

    chore: update getItemJson api

--- src/framework/storage.ts
+++ src/framework/storage.ts
@@ -7,7 +7,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export const getItemJson = async <T>(k: string) => {
     try {
         const ret = await AsyncStorage.getItem(k);
-        if (!ret) return {};
+        if (!ret) return undefined;
         else return JSON.parse(ret) as T;
     } catch (e) {
         if (e instanceof Error) throw new Error(`[Storage] getItemJson: failed to load key "${k}": ${(e as Error).message}`);
