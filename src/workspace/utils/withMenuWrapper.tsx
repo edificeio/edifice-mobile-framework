9acde90f6023c7c756c890fdf091f55dc18c27fc
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-05-19 18:00:40 +0200

    fix(workspace): #45626 use new filepicker

--- src/workspace/utils/withMenuWrapper.tsx
+++ src/workspace/utils/withMenuWrapper.tsx
@@ -166,6 +166,13 @@ function withMenuWrapper<T extends IProps>(WrappedComponent: React.ComponentType
             menuItems={popupMenuItems}
             onEvent={this.handleEvent.bind(this)}
             selected={selectedArrayItems}
+            eventHandleData={{
+              dispatch,
+              filterId,
+              navigation,
+              parentId: navigation.getParam("parentId"),
+              selected: selectedItems,
+            }}
           />
         </View>
       );
