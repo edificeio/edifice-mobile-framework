013e2f007fc129aeb809ff1e5969574c9cf3b0bc
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-05-26 11:19:37 +0200

    fix: #45234 use timelineSubModules

--- src/modules/schoolbook/index.ts
+++ src/modules/schoolbook/index.ts
@@ -1,13 +1,14 @@
 import { NavigableModule } from "../../framework/util/moduleTool";
 
 import config from './moduleConfig';
-import getRoot from './navigator';
+import getRoot, { timelineRoutes } from './navigator';
 import reducer from './reducer';
 import setUpNotifHandlers from './notifHandler';
-import { timelineModules } from "../../framework/modules/timelinev2/timelineModules";
+import { timelineSubModules } from "../../framework/modules/timelinev2/timelineModules";
 
-export default timelineModules.register(
-    new NavigableModule({ config, getRoot, reducer })
-);
+const module = new NavigableModule({ config, getRoot, reducer });
+export default module;
+
+timelineSubModules.register(timelineRoutes);
 
 setUpNotifHandlers();
