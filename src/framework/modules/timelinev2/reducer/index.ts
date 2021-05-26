34db2e11d491d2a352ddaf2922f5823f198e8e6a
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-05-26 10:52:18 +0200

    wip

--- src/framework/modules/timelinev2/reducer/index.ts
+++ src/framework/modules/timelinev2/reducer/index.ts
@@ -1,15 +1,18 @@
 import { combineReducers } from "redux";
 
 import notifDefinitions, { INotifDefinitions_State } from "./notifDefinitions";
+import notifSettings, { INotifSettings_State } from "./notifSettings";
 
 // State
 
 export interface ITimeline_State {
   notifDefinitions: INotifDefinitions_State;
+  notifSettings: INotifSettings_State;
 }
 
 // Reducer
 
 export default combineReducers({
-  notifDefinitions
+  notifDefinitions,
+  notifSettings
 });
