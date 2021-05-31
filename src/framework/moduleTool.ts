4cdf764d230bf6e11da65267c22e88a9390aced5
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-05-31 15:30:25 +0200

    wip: observer pattern for grouping modules

--- src/framework/moduleTool.ts
+++ src/framework/moduleTool.ts
@@ -9,7 +9,7 @@ import { Reducer, AnyAction } from "redux";
 import { ThunkAction } from "redux-thunk";
 import { createMainTabNavOption } from "../navigation/helpers/mainTabNavigator";
 import I18n from "i18n-js";
-import { IAppConf, IPlatformConf } from "../infra/appConf";
+import { NavigationRouteConfig } from "react-navigation";
 
 /**
  * Describes an app asit lives in Entcore, on the platform.
@@ -24,23 +24,12 @@ export interface IEntcoreApp {
   prefix?: string; // Note : Connectors haven't a prefix, Applications do.
 }
 
-export enum ModuleGroup {
-  NO_GROUP,
-  TAB_MODULE,
-  MYAPPS_MODULE,
-  TIMELINE_MODULE
-}
-
 interface IModuleConfigBase<Name extends string> {
   // Module configured information
   name: Name;                // Technical (and unique) name of the module. This module will be referenced by this name everywhere in the code.
   iconName: string;          // Name of the icon in Icomoon font. Equals to `name` if not specified.
-  iconColor?: string;         // Color of the icon. Default color if not specified.
+  iconColor?: string;        // Color of the icon. Default color if not specified.
   displayName: string;       // I18n key of the module title displayed. Equals to `name` if not specified.
-  group: ModuleGroup;        // Name the navigation group where the module will be accessible. If no group specified, the module will be included but not accessible.
-  order: number;
-  // Grouping is based on Array.filter. Mismatches leads to a non-visible module in any navigator.
-  // Note : Modules can have their own custom navigation groups.
 
   // matchEntcoreApp: ((entcoreApp: IEntcoreApp) => boolean); // Function that tell if the module can be accessible by the user. If stirng is provided, compare to prefix.
   entcoreScope: string[]     // oAuth scope required for this module
@@ -68,8 +57,6 @@ export const createModuleConfig: <Name extends string>(opts: IModuleConfigDeclar
     iconName: opts.iconName || opts.name,
     iconColor: opts.iconColor,
     displayName: opts.displayName || opts.name,
-    group: opts.group || ModuleGroup.NO_GROUP,
-    order: opts.order || 0,
     matchEntcoreApp: (typeof opts.matchEntcoreApp === 'string'
       ? ((entcoreApp: IEntcoreApp) => entcoreApp.address === opts.matchEntcoreApp)
       : opts.matchEntcoreApp) || (() => true),
@@ -116,18 +103,12 @@ export class Module<Name extends string, State, ActionMap extends IActionMap>
   createModuleRoute() {
     return {
       screen: this.mainComp,
-      navigationOptions: (() => {
-        switch (this.config.group) {
-          case ModuleGroup.NO_GROUP: return {};
-          case ModuleGroup.TAB_MODULE: return createMainTabNavOption(I18n.t(this.config.displayName), this.config.iconName);
-          case ModuleGroup.MYAPPS_MODULE: return {};
-          case ModuleGroup.TIMELINE_MODULE: return {};
-        }
-      })()
+      navigationOptions: createMainTabNavOption(I18n.t(this.config.displayName), this.config.iconName)
     }
   }
 }
 
+export type AnyModule = IModule<string, any, IActionMap>;
 export interface ModuleMap { [key: string]: IModule<typeof key, unknown, IActionMap> }
 export interface ModuleConfigMap { [key: string]: IModuleConfig<typeof key> }
 
@@ -159,26 +140,57 @@ export const loadModuleConfigMap: (modules: Array<IModuleConfigDeclaration<strin
     // ToDo extract here custom module config from appConf
   }
 
+/** Get reducers map from given ModuleMap */
 export const getModuleReducers: (modules: ModuleMap) => { [key: string]: Reducer } = modules => {
   return Object.fromEntries(Object.entries(modules).map(([k, v]) => [k, v.reducer]));
 }
 
-export const getModuleRoutes: (modules: ModuleMap) => { [key: string]: Reducer } = modules => {
+/** Get routes map from given ModuleMap */
+export const getModuleRoutes: (modules: ModuleMap) => { [key: string]: NavigationRouteConfig<any, any, unknown> } = modules => {
   return Object.fromEntries(Object.entries(modules).map(([k, v]) => [k, v.route]));
 }
+/** Get routes map from a given array of modules */
+export const getModuleRoutesByArray: (modules: AnyModule[]) => { [key: string]: NavigationRouteConfig<any, any, unknown> } = (modules) => {
+  return Object.fromEntries(modules.map(m => [m.config.name, m.route]));
+}
 
+/** Get ModuleMap by filtering a given ModuleMap */
 export const getModulesByFilter: (modules: ModuleMap, filterFunc: ((m: IModule<string, unknown, IActionMap>) => boolean)) => ModuleMap = (modules, filterFunc) => {
   return Object.fromEntries(Object.entries(modules).filter(([k, v]) => filterFunc(v)));
 }
 
+/** Get ModuleMap by filtering a given ModuleMap corresponding to user's available apps */
 export const getAvailableModules: (modules: ModuleMap, availableApps: IEntcoreApp[]) => ModuleMap = (modules, availableApps) => {
   return Object.fromEntries(Object.entries(modules).filter(([k, v]) => availableApps.find(app => v.config.matchEntcoreApp(app))));
 }
 
+/** Get ModuleConfigMap by filtering a given ModuleConfigMap corresponding to user's available apps */
 export const getAvailableModulesConfigs: (moduleConfigs: ModuleConfigMap, availableApps: IEntcoreApp[]) => ModuleConfigMap = (moduleConfigs, availableApps) => {
   return Object.fromEntries(Object.entries(moduleConfigs).filter(([k, v]) => availableApps.find(app => v.matchEntcoreApp(app))));
 }
 
+/** Get scope array from a given ModuleConfigMap */
 export const getModulesScope: (moduleConfigs: ModuleConfigMap) => string[] = moduleConfigs => {
   return [...new Set(Object.values(moduleConfigs).map(c  => c.entcoreScope).flat())];
 }
+
+export type IRegisteredModule = AnyModule & { order: number }
+
+/**
+ * Create a registeredModules functions to get and register.
+ * Use this in your module to create submodules.
+ * @param registeredModules
+ */
+export const createGetRegisteredModules = (registeredModules: IRegisteredModule[]) => () => registeredModules.sort((a, b) => a.order - b.order);
+export const createRegisterModule = (registeredModules: IRegisteredModule[]) =>
+  (module: AnyModule, order?: number) => {
+    registeredModules.push({ ...module, order: order || 0 });
+    return module;
+  }
+
+/** This registeredModules is for tab modules.
+ * ToDo: move this in the navigationModule (that does not exists for the moment.)
+ */
+const registeredTabModules: IRegisteredModule[] = [];
+export const getRegisteredTabModules = createGetRegisteredModules(registeredTabModules);
+export const registerTabModule = createRegisterModule(registeredTabModules);
