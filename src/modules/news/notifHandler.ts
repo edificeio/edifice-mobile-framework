dd7b333b9e1fdfd1b0178c46f386a2f4bbb7f028
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-05-31 15:30:42 +0200

    chore: add empty news module

---
+++ src/modules/news/notifHandler.ts
@@ -0,0 +1,28 @@
+/**
+ * News notif handler
+ */
+
+import type { IResourceUriNotification, ITimelineNotification } from "../../framework/notifications";
+
+import { NotifHandlerThunkAction, registerNotifHandlers } from "../../framework/notifications/routing";
+import { mainNavNavigate } from "../../navigation/helpers/navHelper";
+
+export interface INewsNotification extends ITimelineNotification, IResourceUriNotification {}
+
+const handleBlogNotificationAction: NotifHandlerThunkAction = notification => async (dispatch, getState) => {
+	mainNavNavigate('news/details', {
+		notification
+	});
+	return {
+		managed: true,
+		trackInfo: { action: "News", name: `${notification.type}.${notification["event-type"]}` }
+	}
+}
+
+export default () => registerNotifHandlers([
+	{
+		type: 'NEWS',
+		"event-type": 'INFO-SHARED',
+		notifHandlerAction: handleBlogNotificationAction
+	}
+]);
