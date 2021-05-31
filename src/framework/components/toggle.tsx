3ae2cee1a700289851dd390c72faf5d6d179b2b8
Camille Meshorer <camille.meshorer@opendigitaleducation.com>
2021-05-31 15:30:42 +0200

    chore: created new toggle

---
+++ src/framework/components/toggle.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { Switch } from "react-native";
+import theme from "../theme";
+
+export interface IToggleProps {
+  checked: boolean;
+  onCheckChange: () => void;
+}
+
+export const Toggle = ({ checked, onCheckChange }: IToggleProps) => (
+  <Switch
+    value={checked}
+    onValueChange={() => onCheckChange && onCheckChange()}
+    trackColor={{ false: theme.color.tertiary.light, true: theme.color.secondary.regular }}
+  />
+);
