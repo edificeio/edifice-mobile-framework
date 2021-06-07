f1ac0f01464e10eebbe2995bc8cad9b855b45cc8
Guillaume DROULEZ <guillaume.droulez@mac.com>
2021-06-07 16:16:40 +0200

    fix: iOS file upload nott working

--- src/infra/filePicker.tsx
+++ src/infra/filePicker.tsx
@@ -1,95 +1,136 @@
 import * as React from "react";
 import I18n from "i18n-js";
-import { CameraOptions, ImageLibraryOptions, ImagePickerResponse, launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import {
+  CameraOptions,
+  ImageLibraryOptions,
+  ImagePickerResponse,
+  launchCamera,
+  launchImageLibrary,
+} from "react-native-image-picker";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { ModalBox, ModalContent, ModalContentBlock } from "../ui/Modal";
 import { ButtonTextIcon } from "../ui";
 import { GestureResponderEvent, Platform, TouchableOpacityProps } from "react-native";
-import DocumentPicker, { DocumentPickerOptions, DocumentPickerResponse, DocumentType, PlatformTypes } from "react-native-document-picker";
-import getPath from '@flyerhq/react-native-android-uri-path'
+import DocumentPicker, {
+  DocumentPickerOptions,
+  DocumentPickerResponse,
+  DocumentType,
+  PlatformTypes,
+} from "react-native-document-picker";
+import getPath from "@flyerhq/react-native-android-uri-path";
 
-export type ImagePicked = Required<Pick<ImagePickerResponse, 'uri' | 'type' | 'fileName' | 'fileSize' | 'base64' | 'width' | 'height'>>;
-export type DocumentPicked = Required<Pick<DocumentPickerResponse, 'uri' | 'type'>> & { fileName: string, fileSize: number };
+export type ImagePicked = Required<
+  Pick<ImagePickerResponse, "uri" | "type" | "fileName" | "fileSize" | "base64" | "width" | "height">
+>;
+export type DocumentPicked = Required<Pick<DocumentPickerResponse, "uri" | "type">> & {
+  fileName: string;
+  fileSize: number;
+};
 
-export class FilePicker extends React.PureComponent<{
+export class FilePicker extends React.PureComponent<
+  {
     callback: (document: ImagePicked | DocumentPicked) => void;
-    options?: Partial<ImageLibraryOptions & CameraOptions & DocumentPickerOptions<keyof PlatformTypes>>
-} & TouchableOpacityProps, {
-    showModal: boolean
-}> {
+    options?: Partial<ImageLibraryOptions & CameraOptions & DocumentPickerOptions<keyof PlatformTypes>>;
+  } & TouchableOpacityProps,
+  {
+    showModal: boolean;
+  }
+> {
+  state = { showModal: false };
 
-    state = { showModal: false };
+  render() {
+    const { callback, options, ...props } = this.props;
 
-    render() {
-        const { callback, options, ...props } = this.props;
+    const imageCallback = (image: ImagePickerResponse) => {
+      // console.log("picker returns", image);
+      !image.didCancel && !image.errorCode && !image.errorMessage && image.uri && callback(image as ImagePicked);
+      this.setState({ showModal: false });
+    };
 
-        const imageCallback = (image: ImagePickerResponse) => {
-            // console.log("picker returns", image);
-            !image.didCancel && !image.errorCode && !image.errorMessage && image.uri && callback(image as ImagePicked);
-            this.setState({ showModal: false });
-        };
+    const documentCallback = async (file: DocumentPickerResponse) => {
+      !file.copyError && file.uri;
+      file.uri = Platform.select({
+        android: getPath(file.uri),
+        default: decodeURI(file.uri.indexOf("file://") > -1 ? file.uri.split("file://")[1] : file.uri),
+      });
+      const { name, size, ...fileResolved } = file;
+      callback({ ...fileResolved, fileName: file.name, fileSize: file.size });
+      this.setState({ showModal: false });
+    };
 
-        const documentCallback = async (file: DocumentPickerResponse) => {
-            !file.copyError && file.uri
-            file.uri = Platform.select({
-                android: getPath(file.uri),
-                ios: decodeURI(file.uri).split("file://")[1],
-            });
-            const { name, size, ...fileResolved } = file;
-            callback({ ...fileResolved, fileName: file.name, fileSize: file.size });
-            this.setState({ showModal: false });
-        }
-
-        const menuActions = [{
-            id: 'camera',
-            title: I18n.t('common-photoPicker-take'),
-            action: () => {
-                launchCamera({
-                    ...options,
-                    mediaType: 'photo',
-                }, imageCallback);
-            }
+    const menuActions = [
+      {
+        id: "camera",
+        title: I18n.t("common-photoPicker-take"),
+        action: () => {
+          launchCamera(
+            {
+              ...options,
+              mediaType: "photo",
+            },
+            imageCallback
+          );
         },
-        {
-            id: 'gallery',
-            title: I18n.t('common-photoPicker-pick'),
-            action: () => {
-                launchImageLibrary({
-                    ...this.props.options,
-                    mediaType: 'photo'
-                }, imageCallback);
-            }
+      },
+      {
+        id: "gallery",
+        title: I18n.t("common-photoPicker-pick"),
+        action: () => {
+          launchImageLibrary(
+            {
+              ...this.props.options,
+              mediaType: "photo",
+            },
+            imageCallback
+          );
         },
-        {
-            id: 'document',
-            title: I18n.t('common-picker-document'),
-            action: () => {
-                DocumentPicker.pick({
-                    type: DocumentPicker.types.allFiles as Array<PlatformTypes[keyof PlatformTypes][keyof PlatformTypes[keyof PlatformTypes]]> | DocumentType[keyof PlatformTypes],
-                    ...options
-                }).then(documentCallback);
-            }
+      },
+      {
+        id: "document",
+        title: I18n.t("common-picker-document"),
+        action: () => {
+          DocumentPicker.pick({
+            type: DocumentPicker.types.allFiles as
+              | Array<PlatformTypes[keyof PlatformTypes][keyof PlatformTypes[keyof PlatformTypes]]>
+              | DocumentType[keyof PlatformTypes],
+            ...options,
+          }).then(documentCallback);
         },
-        {
-            id: 'cancel',
-            title: I18n.t('Cancel'),
-            action: () => {
-                this.setState({ showModal: false });
-            }
-        }];
+      },
+      {
+        id: "cancel",
+        title: I18n.t("Cancel"),
+        action: () => {
+          this.setState({ showModal: false });
+        },
+      },
+    ];
 
-        return <>
-            <ModalBox backdropOpacity={0.5} isVisible={this.state.showModal}><ModalContent>
-                {menuActions.map(a => <ModalContentBlock>
-                    <ButtonTextIcon title={a.title} onPress={() => {
-                        a.action();
-                    }}></ButtonTextIcon>
-                </ModalContentBlock>)}
-            </ModalContent>
-            </ModalBox>
-            <TouchableOpacity {...props} disallowInterruption={true} onPress={(event: GestureResponderEvent) => {
-                this.setState({ showModal: true }); props.onPress && props.onPress(event)
-            }}>{this.props.children}</TouchableOpacity>
-        </>;
-    }
+    return (
+      <>
+        <ModalBox backdropOpacity={0.5} isVisible={this.state.showModal}>
+          <ModalContent>
+            {menuActions.map(a => (
+              <ModalContentBlock>
+                <ButtonTextIcon
+                  title={a.title}
+                  onPress={() => {
+                    a.action();
+                  }}></ButtonTextIcon>
+              </ModalContentBlock>
+            ))}
+          </ModalContent>
+        </ModalBox>
+        <TouchableOpacity
+          {...props}
+          disallowInterruption={true}
+          onPress={(event: GestureResponderEvent) => {
+            this.setState({ showModal: true });
+            props.onPress && props.onPress(event);
+          }}>
+          {this.props.children}
+        </TouchableOpacity>
+      </>
+    );
+  }
 }
