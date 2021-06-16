65ae986e26582edefe016099aec8013bd964f603
LACOMMARE <manon.lacommare@cgi.com>
2021-06-16 16:46:52 +0200

    add(zimbra): Activate new framework version and add notifications

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ import messaging from '@react-native-firebase/messaging';
 // Functionnal modules // THIS IS UGLY. it is a workaround for include matomo tracking.
 // require("./timelinev2");
 require("./mailbox");
-require("./zimbra");
+//require("./zimbra");
 //require("./pronote");
 //require("./lvs");
 require("./homework");
