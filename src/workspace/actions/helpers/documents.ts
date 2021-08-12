de805ad24489cb3d41bd4c9a275eb72b860596a3
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-08-12 13:12:00 +0200

    feat(workspace) use new upload service

--- src/workspace/actions/helpers/documents.ts
+++ src/workspace/actions/helpers/documents.ts
@@ -3,16 +3,16 @@
  * Build actions to be dispatched to the hworkspace list reducer.
  */
 
-import RNFB from "rn-fetch-blob";
 import moment from "moment";
-import { Platform } from "react-native";
 import I18n from "i18n-js";
 import { IFile, ContentUri, IFolder, IItems, IItem, FilterId } from "../../types";
 import { filters } from "../../types/filters/helpers/filters";
-import Conf from "../../../../ode-framework-conf";
-import { getAuthHeader } from "../../../infra/oauth";
 import { progressAction, progressEndAction, progressInitAction } from "../../../infra/actions/progress";
 import Toast from "react-native-tiny-toast";
+import workspaceService, { IUploadParams } from "../../../framework/services/workspace";
+import { ThunkDispatch } from "redux-thunk";
+import { LocalFile } from "../../../framework/util/file";
+import { getUserSession } from "../../../framework/util/session";
 
 export type IDocumentArray = any[];
 
@@ -142,56 +142,90 @@ function formatFolderResult(item: IBackendFolder): IFolder {
 
 // UPLOAD --------------------------------------------------------------------------------------
 
-export const uploadDocument = (dispatch: any, content: ContentUri[], parentId?: string) => {
-  const signedHeaders = getAuthHeader();
-  const headers = { ...signedHeaders, "Content-Type": "multipart/form-data" };
-  console.log("cgi headers", headers);
-
-  const parentIdParam = !!parentId && !Object.keys(FilterId).includes(parentId) ? `parentId=${parentId}&` : "";
-  const protectedParam = parentId === FilterId.protected ? "protected=true&application=media-library&" : "";
-
-  const url = `${Conf.currentPlatform.url}/workspace/document?${parentIdParam}${protectedParam}quality=1&thumbnail=120x120&thumbnail=100x100&thumbnail=290x290&thumbnail=381x381&thumbnail=1600x0`;
-
-  console.log("upload document", content, url);
-
-  dispatch(progressInitAction());
-
-  // console.log(content.map((item, index) => ({ name: `document${index}`, type: item.mime, filename: item.name, data: RNFB.wrap(decodeURIComponent(item.uri)) })));
-  console.log("cgi content", content);
-  const response = content.map((item, index) =>
-    RNFB.fetch("POST", url, headers, [
-      {
-        name: `document${index}`, type: item.mime, filename: item.name, data: RNFB.wrap(decodeURIComponent(
-          Platform.OS === 'ios' ? item.uri.replace('file://', '') : item.uri
-        ))
-      },
-    ])
-      .uploadProgress({ interval: 100 }, (written, total) => {
-        // console.log("progress");
-        dispatch(progressAction((written / total) * 100));
-      })
-      .then(response => {
-        // console.log("upload done");
+export const uploadDocumentAction = (content: ContentUri[], parentId?: string) =>
+  async (dispatch: ThunkDispatch<any, any, any>, getState: () => any) => {
+    try {
+      const lcs = content.map(c => new LocalFile({
+        filepath: c.uri,
+        filename: c.name,
+        filetype: c.mime,
+        name: c.name
+      }, { _needIOSReleaseSecureAccess: false }))
+      const jobs = workspaceService.startUploadFiles(getUserSession(getState()), lcs, { parent: parentId as IUploadParams['parent'] }, {
+        onBegin: (res => {
+          dispatch(progressInitAction());
+        }),
+        onProgress: (res => {
+          dispatch(progressAction((res.totalBytesSent / res.totalBytesExpectedToSend) * 100));
+        })
+      });
+      return await Promise.all(jobs.map(j => j.promise)).then(files => {
         dispatch(progressAction(100));
         dispatch(progressEndAction());
+      });
+    } catch (e) {
+      console.warn("error uploading: ", e);
+      if (e === `{"error":"file.too.large"}`) {
+        Toast.show(I18n.t("workspace-quota-overflowText"), {
+          position: Toast.position.BOTTOM,
+          mask: false,
+          containerStyle: { width: "95%", backgroundColor: "black" },
+        });
+      };
+    }
+  }
 
-        if (response && response.respInfo.status >= 200 && response.respInfo.status < 300) {
-          return Promise.resolve(response.data);
-        } else {
-          return Promise.reject(response.data);
-        }
-      })
-      .catch(e => {
-        console.warn("error uploading: ", e);
-        if (e === `{"error":"file.too.large"}`) {
-          Toast.show(I18n.t("workspace-quota-overflowText"), {
-            position: Toast.position.BOTTOM,
-            mask: false,
-            containerStyle: { width: "95%", backgroundColor: "black" },
-          });
-        };
-      })
-  );
 
-  return Promise.all(response);
-};
+// export const uploadDocument = (dispatch: any, content: ContentUri[], parentId?: string) => {
+//   const signedHeaders = getAuthHeader();
+//   const headers = { ...signedHeaders, "Content-Type": "multipart/form-data" };
+//   console.log("cgi headers", headers);
+
+//   const parentIdParam = !!parentId && !Object.keys(FilterId).includes(parentId) ? `parentId=${parentId}&` : "";
+//   const protectedParam = parentId === FilterId.protected ? "protected=true&application=media-library&" : "";
+
+//   const url = `${Conf.currentPlatform.url}/workspace/document?${parentIdParam}${protectedParam}quality=1&thumbnail=120x120&thumbnail=100x100&thumbnail=290x290&thumbnail=381x381&thumbnail=1600x0`;
+
+//   console.log("upload document", content, url);
+
+//   dispatch(progressInitAction());
+
+//   // console.log(content.map((item, index) => ({ name: `document${index}`, type: item.mime, filename: item.name, data: RNFB.wrap(decodeURIComponent(item.uri)) })));
+//   console.log("cgi content", content);
+//   const response = content.map((item, index) =>
+//     RNFB.fetch("POST", url, headers, [
+//       {
+//         name: `document${index}`, type: item.mime, filename: item.name, data: RNFB.wrap(decodeURIComponent(
+//           Platform.OS === 'ios' ? item.uri.replace('file://', '') : item.uri
+//         ))
+//       },
+//     ])
+//       .uploadProgress({ interval: 100 }, (written, total) => {
+//         // console.log("progress");
+//         dispatch(progressAction((written / total) * 100));
+//       })
+//       .then(response => {
+//         // console.log("upload done");
+//         dispatch(progressAction(100));
+//         dispatch(progressEndAction());
+
+//         if (response && response.respInfo.status >= 200 && response.respInfo.status < 300) {
+//           return Promise.resolve(response.data);
+//         } else {
+//           return Promise.reject(response.data);
+//         }
+//       })
+//       .catch(e => {
+//         console.warn("error uploading: ", e);
+//         if (e === `{"error":"file.too.large"}`) {
+//           Toast.show(I18n.t("workspace-quota-overflowText"), {
+//             position: Toast.position.BOTTOM,
+//             mask: false,
+//             containerStyle: { width: "95%", backgroundColor: "black" },
+//           });
+//         };
+//       })
+//   );
+
+//   return Promise.all(response);
+// };
