41b270fafa56a3263b537b370ad16b3084372870
Guillaume DROULEZ <guillaume.droulez@mac.com>
2021-08-13 18:45:20 +0200

    Revert "chore: Try to fix build not working"
    This reverts commit fed678ccdddc1178d0e2952ff837f566ef7c1a84.

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,15 +2,3 @@
 sudo gem install cocoapods
 pod cache clean --all
 pod repo update
-
-#brew unlink node
-#brew install node@14.17.5
-#brew link node
-
-#npm install npm@6.14.14
-
-echo "**********"
-echo "pod = "$(pod --version)
-echo "npm = "$(npm -v)
-echo "node = "$(node -v)
-echo "**********"
