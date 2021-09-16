0c3edf438c6ae94f6e1332427d7316bd526eca66
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-09-16 17:22:25 +0200

    fix: #48801 use androidLayerType="software" on WebView due to crash

--- src/ui/Webview.tsx
+++ src/ui/Webview.tsx
@@ -7,7 +7,7 @@ import WebView, { WebViewProps } from "react-native-webview";
 
 export function SafeWebView<P>(props: WebViewProps & P) {
     const id = Conf.currentPlatform? (Conf.currentPlatform.webviewIdentifier || Conf.webviewIdentifier): (Conf.webviewIdentifier || "ode-unknown");
-    return <WebView {...props}  source={{...(props?props.source:{}), headers:{
+    return <WebView {...props} androidLayerType="software" source={{...(props?props.source:{}), headers:{
         'X-Requested-With': id
     }}} injectedJavaScript={`window.WEBVIEW_BUNDLEID=${id}`} />;
 };
