c749b8adc293e001ef23faaf6d110b2dc99d5bc9
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2021-11-22 12:59:46 +0100

    chore(override): include dev/default present files to git workspace

--- src/app/override/appconf.ts
+++ src/app/override/appconf.ts
@@ -1,1 +1,36 @@
-export default {}
+/**
+ * Global runtime configuration override
+ */
+
+export default {
+    matomo: {
+        url: "https://matomo.opendigitaleducation.com/matomo.php",
+        siteId: 3 // MOBILE DEV
+    },
+    webviewIdentifier: "dev.CC3B2A9FC9C32565FF6237642B73E",
+
+    platforms: [
+
+        // Recettes
+        require('~/platforms/recette'),
+        require('~/platforms/recette-leo'),
+        require('~/platforms/recette-ent77'),
+        require('~/platforms/recette-paris'),
+
+        // Formation
+        require('~/platforms/formation')['formation-one'],
+        require('~/platforms/formation')['formation-neo'],
+
+        // Preprod
+        require('~/platforms/saas')['preprod-one'],
+        require('~/platforms/saas')['preprod-neo'],
+
+        // Prod
+        require('~/platforms/saas')['prod-one'],
+        require('~/platforms/saas')['prod-neo'],
+
+        // Divers
+        require('~/platforms/localhost')
+
+    ]
+}
--- src/app/override/modules.ts
+++ src/app/override/modules.ts
@@ -1,1 +1,11 @@
-export default [];
+export default [
+    require("~/modules/blog"),
+    require("~/modules/conversation"),
+    require("~/modules/lvs"),
+    require("~/modules/news"),
+    require("~/modules/pronote"),
+    require("~/modules/schoolbook"),
+    require("~/modules/support"),
+    require("~/modules/viescolaire"),
+    require("~/modules/zimbra"),
+];
