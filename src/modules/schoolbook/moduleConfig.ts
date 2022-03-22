0f00bfb98353664c81afb73fac85da69d528df7c
cmeshorer <cmeshorer@gmail.com>
2022-03-22 11:32:35 +0100

    feat(schoolbook): MB-351, display in myapps

--- src/modules/schoolbook/moduleConfig.ts
+++ src/modules/schoolbook/moduleConfig.ts
@@ -1,10 +1,14 @@
-import { ISchoolbook_State } from './reducer';
-
+import theme from '~/app/theme';
 import { createNavigableModuleConfig } from '~/framework/util/moduleTool';
+
+import { ISchoolbook_State } from './reducer';
 
 export default createNavigableModuleConfig<'schoolbook', ISchoolbook_State>({
   name: 'schoolbook',
   displayName: 'schoolbook.tabName',
+  iconName: 'chat3',
+  iconColor: theme.themeOpenEnt.green,
   matchEntcoreApp: '/schoolbook',
   entcoreScope: ['schoolbook'],
+  registerAs: 'myAppsModule',
 });
