4e43218e44a05a8051bd7de00978a17ec36880a6
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2022-03-28 18:19:36 +0200

    feat: MB-70 replace some ScrollViews by Lists

--- src/user/components/ChildrenPage.tsx
+++ src/user/components/ChildrenPage.tsx
@@ -1,12 +1,13 @@
 import I18n from 'i18n-js';
 import * as React from 'react';
-import { ScrollView, SafeAreaView, View } from 'react-native';
+import { SectionList, View } from 'react-native';
 import { NavigationInjectedProps, NavigationState } from 'react-navigation';
+import { UI_SIZES } from '~/framework/components/constants';
+
+import { PageView } from '~/framework/components/page';
+import { H4 } from '~/ui/Typography';
 
 import { UserCard } from './UserCard';
-
-import { H4 } from '~/ui/Typography';
-import { PageView } from '~/framework/components/page';
 
 // TYPES ------------------------------------------------------------------------------------------
 
@@ -24,34 +25,46 @@ export interface IChildrenPageProps {
 
 export class ChildrenPage extends React.PureComponent<IChildrenPageProps & NavigationInjectedProps<NavigationState>> {
   render() {
+    interface ISectionListItem {
+      structureName: string;
+      data: {
+        displayName: string;
+        id: string;
+      }[];
+    }
+    const data = [] as ISectionListItem[];
+    if (this.props.schools)
+      for (const school of this.props.schools) {
+        data.push({
+          structureName: school.structureName,
+          data: school.children,
+        });
+      }
+
     return (
       <PageView
         navigation={this.props.navigation}
         navBarWithBack={{
           title: I18n.t('directory-childrenTitle'),
         }}>
-        <ScrollView alwaysBounceVertical={false}>
-          <SafeAreaView>
-            {this.props.schools
-              ? this.props.schools.map(school => {
-                  return (
-                    <View key={school.structureName}>
-                      <H4>{school.structureName}</H4>
-                      {school.children
-                        ? school.children.map(user => {
-                            return (
-                              <View style={{ marginBottom: 15 }} key={user.id}>
-                                <UserCard id={user.id} displayName={user.displayName} type="Student" />
-                              </View>
-                            );
-                          })
-                        : null}
-                    </View>
-                  );
-                })
-              : null}
-          </SafeAreaView>
-        </ScrollView>
+        {this.props.schools ? (
+          <SectionList
+            alwaysBounceVertical={false}
+            sections={data}
+            keyExtractor={item => item.id}
+            renderSectionHeader={({ section }) => {
+              return <H4>{section.structureName}</H4>;
+            }}
+            renderItem={({item: user}) => {
+              return (
+                <View style={{ marginBottom: 15 }} key={user.id}>
+                  <UserCard id={user.id} displayName={user.displayName} type="Student" />
+                </View>
+              );
+            }}
+            ListFooterComponent={<View style={{ paddingBottom: UI_SIZES.screen.bottomInset }} />}
+          />
+        ) : null}
       </PageView>
     );
   }
