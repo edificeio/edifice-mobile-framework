499100a589d100939f892f36c475946a92b153d1
Guillaume DROULEZ <guillaume.droulez@mac.com>
2022-03-31 19:43:21 +0200

    chore(build): Babel Bad Configuration Fix

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  if (api.env('production')) {
+    return {
+      presets: ['module:metro-react-native-babel-preset'],
+      plugins: ['transform-remove-console'],
+    };
+  }
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+  };
 };
