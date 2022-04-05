c66fb0ceedf92aa6e8d3e8d9556e01414743ebe7
Guillaume DROULEZ <guillaume.droulez@mac.com>
2022-04-05 16:20:56 +0200

    Squashed commit of the following:
    commit f481c97825463a1108fe17da37efa4fd3c7efa05
    Author: Guillaume DROULEZ <guillaume.droulez@mac.com>
    Date:   Tue Apr 5 16:06:21 2022 +0200
    
        Squashed commit of the following:
    
        commit a8baf71a7c01bd246c16f4489c5f0e5ffc484851
        Author: Guillaume DROULEZ <guillaume.droulez@mac.com>
        Date:   Tue Apr 5 16:05:47 2022 +0200
    
            Squashed commit of the following:
    
            commit 552488bda1a761f7448a1d529b3975ff247311fd
            Author: LACOMMARE <manon.lacommare@cgi.com>
            Date:   Thu Feb 17 17:12:16 2022 +0100
    
                feat(mediacentre): Initialize mediacentre module
    
                feat(mediacentre): Inititialize advanced search
    
                feat(mediacentre): Full cards for researched resources
    
                feat(mediacentre): advanced search + ButtonGroup ui component
    
                feat(mediacentre): Search filter
    
                feat(mediacentre): Deleted search page, added search content in homepage
    
                feat(mediacentre): Added use of TouchCard and TouchableResourceCard components
    
                feat(mediacentre): wip textbooks backend
    
                feat(mediacentre): textbooks fetch
    
                feat(mediacentre): added favorites carousel
    
                feat(mediacentre): improved typing and conditional rendering
    
                feat(mediacentre): attached simple search backend, wip advanced search backend
    
                feat(mediacentre): edited SmallCard style
    
                feat(mediacentre): added signets to homepage
    
            commit 715ad29d00e0c386507098638d8812c28dbf18bd
            Author: LACOMMARE <manon.lacommare@cgi.com>
            Date:   Wed Jan 26 11:28:22 2022 +0100
    
                chore(refactor): Beautify code in support and zimbra modules
    
        commit 9270c43ae8d74b67c4ceb1eb5c6ad0a639c936c8
        Author: Valentin Mourot <valentin.mourot@opendigitaleducation.com>
        Date:   Mon Apr 4 18:29:55 2022 +0200
    
            tech: MB-215 get rid of glamorous in favor of emotion
    
        commit 861193271d61cbdf14b6e665a1562f0302036e0d
        Author: Valentin Mourot <valentin.mourot@opendigitaleducation.com>
        Date:   Mon Apr 4 17:16:02 2022 +0200
    
            fix: MB-215 remove import cycle in userPage
    
        commit 778f59e0c3fe1bc76613630fe0d692af2183071a
        Author: Valentin Mourot <valentin.mourot@opendigitaleducation.com>
        Date:   Mon Apr 4 16:44:44 2022 +0200
    
            fix: MB-215 remove import cycle in profile
    
        commit e7993219d114d97c52bc9742e065a66f880c1ab0
        Author: Valentin Mourot <valentin.mourot@opendigitaleducation.com>
        Date:   Mon Apr 4 15:46:21 2022 +0200
    
            fix: MB-215 remove conversation require cycle
    
    commit 9ca1827cd498718dc3cd265044a79ca8f53b801e
    Author: Guillaume DROULEZ <guillaume.droulez@mac.com>
    Date:   Tue Apr 5 16:03:09 2022 +0200
    
        upgrade(rn067): 4th round
    
    commit 9be5e234ab8e60d75e6289779bbccc40f0e69797
    Author: Guillaume DROULEZ <guillaume.droulez@mac.com>
    Date:   Tue Apr 5 13:09:10 2022 +0200
    
        upgrade(rn067): 3rd round
    
    commit a321bb16c5d47c9c5f61809bfd4ebcaad655edb0
    Author: Guillaume DROULEZ <guillaume.droulez@mac.com>
    Date:   Tue Apr 5 08:11:29 2022 +0200
    
        upgrade(rn067): 2nd round
    
    commit bc7504b3cede8417ea3a43b1b36a16d92b9b9458
    Author: Guillaume DROULEZ <guillaume.droulez@mac.com>
    Date:   Mon Apr 4 15:04:56 2022 +0200
    
        upgrade(rn067): 1st round

--- android/app/src/main/java/com/ode/appe/MainActivity.java
+++ android/app/src/main/java/com/ode/appe/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this, R.style.SplashScreenTheme);
+        SplashScreen.show(this, R.style.SplashScreenTheme, true);
         super.onCreate(null);
     }
 
--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,12 @@ module.exports = function (api) {
   if (api.env('production')) {
     return {
       presets: ['module:metro-react-native-babel-preset'],
-      plugins: ['transform-remove-console'],
+      plugins: ['transform-remove-console', 'react-native-reanimated/plugin'],
     };
   }
 
   return {
     presets: ['module:metro-react-native-babel-preset'],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
