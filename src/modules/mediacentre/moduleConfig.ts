87590ffeea72adc00489ac79729356a17b55f3a0
Guillaume DROULEZ <guillaume.droulez@mac.com>
2022-04-08 10:01:53 +0200

    chore(merge): Fix merge error

--- src/modules/mediacentre/moduleConfig.ts
+++ src/modules/mediacentre/moduleConfig.ts
@@ -9,11 +9,7 @@ export default createNavigableModuleConfig<'Mediacentre', IMediacentre_State>({
   entcoreScope: ['mediacentre'],
   picture: {
     type: 'Image',
-<<<<<<< HEAD
-    source: require('ASSETS/icons/logo-mediacentre.png'),
-=======
     source: require('ASSETS/images/logo-mediacentre.png'),
->>>>>>> dev/1.7.4
   },
   registerAs: 'myAppsModule',
 });
