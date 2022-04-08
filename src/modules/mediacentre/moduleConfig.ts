f9f96e10560eb94bec41ae88ecba004fd0a5bf1e
Guillaume DROULEZ <guillaume.droulez@mac.com>
2022-04-08 09:58:31 +0200

    Squashed commit of the following:
    commit bbe7aa6b2f4db9f50c2d4acff631f060b4b12a9d
    Author: Guillaume DROULEZ <guillaume.droulez@mac.com>
    Date:   Thu Apr 7 20:46:28 2022 +0200
    
        release: 1.7.4-alpha.5(1070405)
    
    commit ed33752069563a40fae77c8c274d18940b0868a5
    Author: LACOMMARE <manon.lacommare@cgi.com>
    Date:   Thu Apr 7 17:51:31 2022 +0200
    
        fix(mediacentre): Add logo mediacentre in assets
    
    commit 88b21e96618c1454ab25d246d56990616e30480e
    Author: Guillaume DROULEZ <guillaume.droulez@mac.com>
    Date:   Thu Apr 7 17:12:02 2022 +0200
    
        chore(build): remove nvm + node check on ios build
    
    commit 9eeb29197550f191c0a1bb33b9ab5761a2421d71
    Author: Guillaume Roux <guillaume.roux@cgi.com>
    Date:   Thu Apr 7 16:01:24 2022 +0200
    
        feat(mediacentre): set module image
    
    commit cf4d9ef5d2347c6cf3a8d7b14499544a718f47a7
    Author: Guillaume Roux <guillaume.roux@cgi.com>
    Date:   Wed Apr 6 15:19:13 2022 +0200
    
        feat(mediacentre): search content empty state
    
    commit 170400b9c74b49a01c34896b228356d7c8abacc0
    Author: Guillaume Roux <guillaume.roux@cgi.com>
    Date:   Wed Apr 6 15:03:00 2022 +0200
    
        feat(mediacentre): added empty state, added gar resources section, transformed homepage scrollview into flatlist

--- src/modules/mediacentre/moduleConfig.ts
+++ src/modules/mediacentre/moduleConfig.ts
@@ -1,6 +1,6 @@
-import { IMediacentre_State } from './reducer';
-
 import { createNavigableModuleConfig } from '~/framework/util/moduleTool';
+
+import { IMediacentre_State } from './reducer';
 
 export default createNavigableModuleConfig<'Mediacentre', IMediacentre_State>({
   name: 'Mediacentre',
@@ -9,7 +9,11 @@ export default createNavigableModuleConfig<'Mediacentre', IMediacentre_State>({
   entcoreScope: ['mediacentre'],
   picture: {
     type: 'Image',
+<<<<<<< HEAD
     source: require('ASSETS/icons/logo-mediacentre.png'),
+=======
+    source: require('ASSETS/images/logo-mediacentre.png'),
+>>>>>>> dev/1.7.4
   },
   registerAs: 'myAppsModule',
 });
