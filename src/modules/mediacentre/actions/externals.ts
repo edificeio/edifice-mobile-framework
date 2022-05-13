486ceb4c79986d242c3e8de632573dc434f2e015
Guillaume Roux <guillaume.roux@cgi.com>
2022-05-13 16:44:39 +0200

    fix(mediacentre): AMV2-304, removed display all button from favorites section, edited style

--- src/modules/mediacentre/actions/externals.ts
+++ src/modules/mediacentre/actions/externals.ts
@@ -3,6 +3,7 @@ import { Dispatch } from 'redux';
 import { createAsyncActionCreators } from '~/infra/redux/async2';
 import { searchService } from '~/modules/mediacentre/services/search';
 import { IExternals, actionTypes } from '~/modules/mediacentre/state/externals';
+import { Source } from '~/modules/mediacentre/utils/Resource';
 
 // ACTION LIST ------------------------------------------------------------------------------------
 
@@ -12,9 +13,12 @@ const dataActions = createAsyncActionCreators<IExternals>(actionTypes);
 
 export function fetchExternalsAction(sources: string[]) {
   return async (dispatch: Dispatch) => {
+    if (!sources.includes(Source.GAR)) {
+      return;
+    }
     try {
       dispatch(dataActions.request());
-      const data = await searchService.getExternals(sources);
+      const data = await searchService.getSimple([Source.GAR], '.*');
       dispatch(dataActions.receipt(data));
     } catch (errmsg) {
       dispatch(dataActions.error(errmsg));
