a1d23ab62ed817f1ef31cb88cae754a163edd459
Guillaume Roux <guillaume.roux@cgi.com>
2022-05-20 10:14:28 +0200

    fix(zimbra): AMV2-278, dropdown style

--- src/ui/DropdownMenu.tsx
+++ src/ui/DropdownMenu.tsx
@@ -3,8 +3,8 @@ import { FlatList, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, View 
 
 import theme from '~/app/theme';
 import { UI_SIZES } from '~/framework/components/constants';
+import { Icon } from '~/framework/components/picture/Icon';
 import { Text } from '~/ui/Typography';
-import { Icon } from '~/ui/icons/Icon';
 
 const style = StyleSheet.create({
   transparentContainer: {
@@ -22,13 +22,15 @@ const style = StyleSheet.create({
   itemContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 12,
+    paddingHorizontal: 16,
+    paddingVertical: 14,
   },
   itemIcon: {
     marginRight: 8,
     color: 'white',
   },
   itemText: {
+    fontSize: 15,
     color: 'white',
   },
 });
@@ -57,7 +59,7 @@ export const DropdownMenu: React.FunctionComponent<DropdrownMenuProps> = (props:
           renderItem={({ item }) => (
             <TouchableOpacity onPress={item.onPress}>
               <View style={style.itemContainer}>
-                <Icon name={item.icon} size={20} style={style.itemIcon} />
+                <Icon name={item.icon} size={24} style={style.itemIcon} />
                 <Text style={style.itemText}>{item.text}</Text>
               </View>
             </TouchableOpacity>
