19aea544349dea6a39ce8785db54d11649dcc6ed
LACOMMARE <manon.lacommare@cgi.com>
2022-06-02 11:21:38 +0200

    chore: Fix lint and change ScrollView

--- src/ui/Dropdown.tsx
+++ src/ui/Dropdown.tsx
@@ -2,16 +2,38 @@ import { Picker } from '@react-native-picker/picker';
 import I18n from 'i18n-js';
 import * as React from 'react';
 import { useState } from 'react';
-import { View, ViewStyle, Platform } from 'react-native';
+import { Platform, StyleSheet, View, ViewStyle } from 'react-native';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-
-import { ButtonsOkCancel } from '~/ui/ButtonsOkCancel';
-import { Icon } from '~/ui/icons/Icon';
-import { ModalBox, ModalContent, ModalContentBlock, ModalContentText } from './Modal';
 
 import { TextBold } from '~/framework/components/text';
 import { CommonStyles } from '~/styles/common/styles';
+import { ButtonsOkCancel } from '~/ui/ButtonsOkCancel';
+import { Icon } from '~/ui/icons/Icon';
 
+import { ModalBox, ModalContent, ModalContentBlock, ModalContentText } from './Modal';
+
+/* STYLE */
+
+const styles = StyleSheet.create({
+  fullView: {
+    flex: 1,
+  },
+  dropdownButton: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  modalContent: {
+    width: 350,
+  },
+  dropdownPicker: {
+    width: '100%',
+    marginBottom: 35,
+    paddingHorizontal: 20,
+  },
+});
+
+/* COMPONENT */
 interface IDropdownProps {
   keyId?: string;
   style?: ViewStyle;
@@ -37,7 +59,7 @@ const DropdownAndroid = ({ title, style, data, value, onSelect, renderItem, keyE
   const getItemKeyExtractor = keyExtractor ? keyExtractor : item => item.toString();
 
   return (
-    <View style={[selectedStyle, { flex: 1 }, style]}>
+    <View style={[selectedStyle, styles.fullView, style]}>
       <Picker
         style={{
           color: CommonStyles.textColor,
@@ -64,23 +86,21 @@ const DropdownIOS = ({ keyId, title, renderItem, keyExtractor, style, data, plac
   }
 
   return (
-    <View style={{ flex: 1 }}>
-      <TouchableWithoutFeedback
-        style={[selectedStyle, { padding: 10, flexDirection: 'row', alignItems: 'center' }, style]}
-        onPress={() => toggleModal(true)}>
-        <TextBold style={{ flex: 1 }} numberOfLines={1}>
+    <View style={styles.fullView}>
+      <TouchableWithoutFeedback style={[selectedStyle, styles.dropdownButton, style]} onPress={() => toggleModal(true)}>
+        <TextBold style={styles.fullView} numberOfLines={1}>
           {placeholder ? placeholder : value ? getItemRenderer(data.find(item => getItemKeyExtractor(item) === value)) : ' '}
         </TextBold>
         <Icon size={20} name="arrow_down" />
       </TouchableWithoutFeedback>
       <ModalBox isVisible={visible} onDismiss={() => toggleModal(false)}>
-        <ModalContent style={{ width: 350 }}>
+        <ModalContent style={styles.modalContent}>
           {!!title && (
             <ModalContentBlock>
               <ModalContentText>{title}</ModalContentText>
             </ModalContentBlock>
           )}
-          <View style={{ width: '100%', marginBottom: 35, paddingHorizontal: 20 }}>
+          <View style={styles.dropdownPicker}>
             <Picker selectedValue={selected} onValueChange={(value, label) => selectValue(value as string)}>
               {data.map(item => (
                 <Picker.Item label={getItemRenderer(item)} value={getItemKeyExtractor(item)} />
@@ -94,7 +114,7 @@ const DropdownIOS = ({ keyId, title, renderItem, keyExtractor, style, data, plac
               }}
               onValid={() => {
                 toggleModal(false);
-                onSelect(selected);
+                onSelect(selected!);
               }}
               title={I18n.t('common-ok')}
             />
