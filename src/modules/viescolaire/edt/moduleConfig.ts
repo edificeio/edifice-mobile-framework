913b731a96b31b08c767bbc2615d6b5f8e191aab
Guillaume Roux <guillaume.roux@cgi.com>
2022-09-28 17:10:29 +0200

    fix(viescolaire): AMVS-312, hid modules for personnel users

--- src/modules/viescolaire/edt/moduleConfig.ts
+++ src/modules/viescolaire/edt/moduleConfig.ts
@@ -1,11 +1,17 @@
-import { NavigableModuleConfig } from '~/framework/util/moduleTool';
+import { IEntcoreApp, NavigableModuleConfig } from '~/framework/util/moduleTool';
+import { UserType, getUserSession } from '~/framework/util/session';
 
 import { IEdt_State } from './reducer';
+
+function hasNecessaryRight(entcoreApp: IEntcoreApp) {
+  const userType = getUserSession().user.type;
+  return entcoreApp.address === '/edt' && [UserType.Student, UserType.Relative, UserType.Teacher].includes(userType);
+}
 
 export default new NavigableModuleConfig<'edt', IEdt_State>({
   name: 'edt',
   entcoreScope: ['edt'],
-  matchEntcoreApp: '/edt', // temporary until dashboard redesign
+  matchEntcoreApp: entcoreApp => hasNecessaryRight(entcoreApp), // temporary until dashboard redesign
 
   displayI18n: 'viesco-timetable',
   displayAs: 'myAppsModule',
--- src/modules/viescolaire/presences/moduleConfig.ts
+++ src/modules/viescolaire/presences/moduleConfig.ts
@@ -1,11 +1,17 @@
-import { NavigableModuleConfig } from '~/framework/util/moduleTool';
+import { IEntcoreApp, NavigableModuleConfig } from '~/framework/util/moduleTool';
+import { UserType, getUserSession } from '~/framework/util/session';
 
 import { IPresences_State } from './reducer';
+
+function hasNecessaryRight(entcoreApp: IEntcoreApp) {
+  const userType = getUserSession().user.type;
+  return entcoreApp.address === '/presences' && [UserType.Student, UserType.Relative, UserType.Teacher].includes(userType);
+}
 
 export default new NavigableModuleConfig<'presences', IPresences_State>({
   name: 'presences',
   entcoreScope: ['presences'],
-  matchEntcoreApp: '/presences', // temporary until dashboard redesign
+  matchEntcoreApp: entcoreApp => hasNecessaryRight(entcoreApp), // temporary until dashboard redesign
 
   displayI18n: 'viesco-presences',
   displayAs: 'myAppsModule',
