dd18bd20b0a47d96f552a7d8ebc7416118e8f59a
Guillaume Roux <guillaume.roux@cgi.com>
2022-09-28 16:55:50 +0200

    fix(competences): AMVS-311, hid module for teachers

--- src/modules/viescolaire/competences/moduleConfig.ts
+++ src/modules/viescolaire/competences/moduleConfig.ts
@@ -1,11 +1,17 @@
-import { NavigableModuleConfig } from '~/framework/util/moduleTool';
+import { IEntcoreApp, NavigableModuleConfig } from '~/framework/util/moduleTool';
+import { UserType, getUserSession } from '~/framework/util/session';
 
 import { ICompetences_State } from './reducer';
+
+function hasNecessaryRight(entcoreApp: IEntcoreApp) {
+  const userType = getUserSession().user.type;
+  return entcoreApp.address === '/competences' && [UserType.Student, UserType.Relative].includes(userType);
+}
 
 export default new NavigableModuleConfig<'competences', ICompetences_State>({
   name: 'competences',
   entcoreScope: ['competences'],
-  matchEntcoreApp: '/competences', // temporary until dashboard redesign
+  matchEntcoreApp: entcoreApp => hasNecessaryRight(entcoreApp), // temporary until dashboard redesign
 
   displayI18n: 'viesco-tests',
   displayAs: 'myAppsModule',
