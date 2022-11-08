dd9fa21788a46d0d11b61b0177ed44fc0d48a767
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2022-11-08 14:16:58 +0100

    feat: MB-382 use navBar on Carousel

--- src/ui/Carousel2.tsx
+++ src/ui/Carousel2.tsx
@@ -1,19 +1,18 @@
 /**
  * New implementation of Carousel built with react-native-reanimated-carousel !
  */
+import { ImageZoom } from '@likashefqet/react-native-image-zoom';
 import I18n from 'i18n-js';
 import * as React from 'react';
 import { ImageURISource, Platform, StatusBar, StyleSheet, Text } from 'react-native';
-import { Source } from 'react-native-fast-image';
-import RNFastImage from 'react-native-fast-image';
 import PhotoView from 'react-native-photo-view-ex';
 import RNCarousel, { ICarouselInstance } from 'react-native-reanimated-carousel';
 import { NavigationInjectedProps } from 'react-navigation';
-import { ImageZoom } from '@likashefqet/react-native-image-zoom';
 
 import theme from '~/app/theme';
 import { ActionButton } from '~/framework/components/ActionButton';
 import { UI_SIZES } from '~/framework/components/constants';
+import { FakeHeader, HeaderAction } from '~/framework/components/header';
 import { PageView } from '~/framework/components/page';
 import { FastImage, IMedia, Image } from '~/framework/util/media';
 import { urlSigner } from '~/infra/oauth';
@@ -31,17 +30,19 @@ function getItemKey({ item, index }: { item: IMedia; index: number }) {
 
 const styles = StyleSheet.create({
   page: { backgroundColor: theme.palette.grey.black },
-  closeButton: {
+  header: {
     position: 'absolute',
-    top: UI_SIZES.screen.topInset + UI_SIZES.spacing.minor,
-    left: UI_SIZES.spacing.medium,
-    backgroundColor: theme.palette.grey.black,
+    backgroundColor: '#00000055',
+    zIndex: 10,
+  },
+  closeButton: {
     width: UI_SIZES.dimensions.width.huge,
     height: UI_SIZES.dimensions.width.huge,
     padding: 0,
     paddingHorizontal: 0,
-    zIndex: 1,
-    borderWidth: 0
+    borderWidth: 0,
+    backgroundColor: 'transparent',
+    marginHorizontal: UI_SIZES.spacing.minor,
   },
 });
 
@@ -62,11 +63,20 @@ function renderCarouselItem({ item, index }: { item: IMedia; index: number }) {
             maximumZoomScale={3}
             resizeMode="center"
             style={imageStyle}
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
           />
         ),
         android: (
-          <ImageZoom source={urlSigner.signURISource(item.src)} minScale={1} maxScale={3} containerStyle={imageStyle} resizeMode="contain" resizeMethod="scale" />
-        // <FastImage source={item.src as Source} style={imageStyle} resizeMode={RNFastImage.resizeMode.contain} />
+          <ImageZoom
+            source={urlSigner.signURISource(item.src)}
+            minScale={1}
+            maxScale={3}
+            containerStyle={imageStyle}
+            resizeMode="contain"
+            resizeMethod="scale"
+          />
+          // <FastImage source={item.src as Source} style={imageStyle} resizeMode={RNFastImage.resizeMode.contain} />
         ),
       })
     );
@@ -91,7 +101,7 @@ export function Carousel(props: ICarouselProps) {
   return (
     <PageView navigation={navigation} style={styles.page}>
       <StatusBar backgroundColor={theme.palette.grey.black} barStyle="light-content" />
-      {closeButton}
+      <FakeHeader left={closeButton} style={styles.header} />
       <RNCarousel
         // Layout
         width={UI_SIZES.screen.width}
