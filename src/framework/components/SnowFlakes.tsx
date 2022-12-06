4f21c51003c2b8853f15ccebe34ede0059125b4d
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2022-12-06 18:09:12 +0100

    fix: MB-870, MB-871 fix snowfall 2

--- src/framework/components/SnowFlakes.tsx
+++ src/framework/components/SnowFlakes.tsx
@@ -2,15 +2,14 @@
  * All the logic needed for printing the snow flakes on screen
  */
 import * as React from 'react';
-import { Animated, EmitterSubscription, Vibration } from 'react-native';
-import RNShake from 'react-native-shake';
+import { Animated } from 'react-native';
 import Snow from 'react-native-snow-bg';
 import { NavigationFocusInjectedProps, NavigationInjectedProps } from 'react-navigation';
 import { connect } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 
 import { IGlobalState } from '~/AppStore';
-import { importXmasThemeAction, letItSnowAction, stopItSnowAction } from '~/user/actions/xmas';
+import { importXmasThemeAction, letItSnowAction, setXmasThemeAction, stopItSnowAction } from '~/user/actions/xmas';
 
 import { IUserSession, getUserSession } from '../util/session';
 
@@ -23,99 +22,61 @@ interface SnowFlakesReduxProps extends SnowFlakesProps {
   handleImportXmasThemeAction: () => Promise<boolean>;
   letItSnow: () => Promise<void>;
   stopItSnow: () => Promise<void>;
+  activateXmasTheme: () => Promise<void>;
 }
-
-let isFirstScreenEver = true;
-let hasXmasAutoShowFlakesOnce = false;
-let previousUserId: string | undefined;
-
-let shakeListener: EmitterSubscription | undefined;
 
 function SnowFlakes(props: SnowFlakesReduxProps) {
   // Determine xmas theme setting here & if snow must falling
   const [snowfall, setSnowfall] = React.useState(false);
   const [fadeAnim, setFadeAnim] = React.useState(new Animated.Value(1));
+  const wasFlaskesFalling = React.useRef(props.isFlakesFalling);
 
   const isXmasActivatedRef = React.useRef<boolean | undefined | null>(props.isXmasActivated);
-  const hasAutoShowFlakesOnce = React.useRef<boolean>(false);
-  const shouldHideFlakesRef = React.useRef<boolean>(props.isFlakesFalling);
 
-  if ((previousUserId === undefined && props.session?.user?.id !== undefined) || previousUserId !== props.session?.user?.id) {
-    // We just log in
-    isFirstScreenEver = true;
-    hasXmasAutoShowFlakesOnce = false;
-    previousUserId = props.session?.user?.id;
-  }
-
+  // End snow when go to another page
   React.useEffect(() => {
-    if (!props.session?.user?.id) return; // No session, no flakes !
     const blurListener = props.navigation.addListener('didBlur', () => {
-      if (props.isXmasActivated === false) return;
-      hasXmasAutoShowFlakesOnce = false;
       props.stopItSnow();
     });
-    if (!shakeListener && props.isXmasActivated !== false) {
-      shakeListener = RNShake.addListener(() => {
-        if (props.isXmasActivated === false) return;
-        Vibration.vibrate();
-        props.letItSnow();
-      });
-    }
     return () => {
       blurListener.remove();
-      if (props.isXmasActivated === false) return;
       props.stopItSnow();
-      if (shakeListener) {
-        shakeListener.remove();
-        shakeListener = undefined;
-      }
     };
-    // We want to execute this on mount/unmount
+    // We want to execute this on mount/unmount only
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.isXmasActivated]);
+  }, []);
 
   const getShouldSnowFall = React.useCallback(
     async (p: SnowFlakesReduxProps, isSnowFalling: boolean, fadeAnimation: Animated.Value) => {
-      if (!p.session?.user?.id) return; // No session, no flakes !
-      if (p.navigation.state.routeName === 'Xmas' && hasXmasAutoShowFlakesOnce === false && p.isFocused) {
-        p.isFlakesFalling = true;
-        shouldHideFlakesRef.current = true;
-        hasAutoShowFlakesOnce.current = false; // will be true at the bottom
-        hasXmasAutoShowFlakesOnce = true;
-      }
-      if (p.isFlakesFalling === false && shouldHideFlakesRef.current === true) {
+      // Check to let it snow / stop snow
+      if (p.isFlakesFalling === true && wasFlaskesFalling.current !== true) {
+        setSnowfall(true);
+        setFadeAnim(new Animated.Value(1));
+        wasFlaskesFalling.current = true;
+      } else if (p.isFlakesFalling === false && wasFlaskesFalling.current !== false) {
         // stop snow if needed without condition
+        const RND = Math.random();
+        wasFlaskesFalling.current = false;
         Animated.timing(fadeAnimation, {
           toValue: 0,
           duration: 1000,
           useNativeDriver: true,
         }).start(() => {
-          setSnowfall(false);
-          setFadeAnim(new Animated.Value(1));
+          if (wasFlaskesFalling.current !== true) {
+            setSnowfall(false);
+            setFadeAnim(new Animated.Value(1));
+          }
         });
       }
-      if (!p.session?.user?.id) return; // No session, no flakes !
-      shouldHideFlakesRef.current = p.isFlakesFalling;
+
+      // Check the user xmas preference here
       if (isXmasActivatedRef.current === undefined) {
         isXmasActivatedRef.current = null; // no value, but do not try to import then
         isXmasActivatedRef.current = await p.handleImportXmasThemeAction();
-      }
-      if (isXmasActivatedRef.current !== false) {
-        if (p.navigation.state.routeName === 'Xmas' && hasAutoShowFlakesOnce.current === false) {
-          hasAutoShowFlakesOnce.current = true;
-          if (isXmasActivatedRef.current || p.isXmasActivated) {
-            p.letItSnow();
-          }
-        }
-        if (isFirstScreenEver === true && hasXmasAutoShowFlakesOnce === false && p.navigation.state.routeName === 'timeline') {
-          hasXmasAutoShowFlakesOnce = true;
-          isFirstScreenEver = false;
-          p.letItSnow();
-        }
-        if (p.isFocused && p.isFlakesFalling) {
-          if (isSnowFalling === false) {
-            setSnowfall(true);
-          }
+        if (isXmasActivatedRef.current !== false) {
+          isXmasActivatedRef.current = true;
+          props.letItSnow();
+          props.activateXmasTheme();
         }
       }
     },
@@ -123,11 +84,14 @@ function SnowFlakes(props: SnowFlakesReduxProps) {
   );
   React.useEffect(() => {
     getShouldSnowFall(props, snowfall, fadeAnim);
-  }, [props, getShouldSnowFall, snowfall, fadeAnim]);
+    // We want to control whenever this check re-run
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.isXmasActivated, props.isFlakesFalling]);
 
   const snowStyle = React.useMemo(() => ({ position: 'absolute' as 'absolute', opacity: fadeAnim }), [fadeAnim]);
+  const isSnowEligible = props.isXmasActivated !== false && props.isFocused && props.session?.user?.id; // No session, no flakes !
 
-  return snowfall && props.isXmasActivated !== false ? (
+  return snowfall && isSnowEligible ? (
     <Animated.View style={snowStyle}>
       <Snow fullScreen snowflakesCount={150} fallSpeed="medium" />
     </Animated.View>
@@ -144,6 +108,7 @@ const SnowFlakesConnected = connect(
     handleImportXmasThemeAction: () => dispatch(importXmasThemeAction()),
     letItSnow: () => dispatch(letItSnowAction()),
     stopItSnow: () => dispatch(stopItSnowAction()),
+    activateXmasTheme: () => dispatch(setXmasThemeAction(true)),
   }),
 )(SnowFlakes);
 
--- src/user/actions/xmas.ts
+++ src/user/actions/xmas.ts
@@ -1,3 +1,5 @@
+import { EmitterSubscription, Vibration } from 'react-native';
+import RNShake from 'react-native-shake';
 import { ThunkDispatch } from 'redux-thunk';
 
 import { IGlobalState } from '~/AppStore';
@@ -11,11 +13,60 @@ const computeAsyncStorageKey = () => {
   return `${moduleConfig.name}.xmasThemeSetting.${userId}`;
 };
 
+const SNOW_DURATION = 20000;
+let snowfallTimer: NodeJS.Timeout;
+
+const getIsXmasActive = (state: IGlobalState) => state.user.xmas.xmasTheme !== false;
+
+export const letItSnowAction = () => async (dispatch: ThunkDispatch<any, any, any>, getState: () => IGlobalState) => {
+  try {
+    if (!getIsXmasActive(getState())) return;
+    dispatch({ type: 'setFlakes', value: true });
+    if (snowfallTimer) {
+      clearTimeout(snowfallTimer);
+    }
+    snowfallTimer = setTimeout(() => dispatch({ type: 'setFlakes', value: false }), SNOW_DURATION);
+  } catch (e) {
+    // ToDo: Error handling
+  }
+};
+
+export const stopItSnowAction = () => async (dispatch: ThunkDispatch<any, any, any>, getState: () => IGlobalState) => {
+  try {
+    if (!getIsXmasActive(getState())) return;
+    dispatch({ type: 'setFlakes', value: false });
+    if (snowfallTimer) {
+      clearTimeout(snowfallTimer);
+    }
+  } catch (e) {
+    // ToDo: Error handling
+  }
+};
+
+let shakeListener: EmitterSubscription | undefined;
+
+const updateShakeListenerAction = () => async (dispatch: ThunkDispatch<any, any, any>, getState: () => IGlobalState) => {
+  try {
+    if (shakeListener === undefined && getIsXmasActive(getState())) {
+      shakeListener = RNShake.addListener(() => {
+        Vibration.vibrate();
+        dispatch(letItSnowAction());
+      });
+    } else if (shakeListener !== undefined && !getIsXmasActive(getState())) {
+      shakeListener.remove();
+      shakeListener = undefined;
+    }
+  } catch (e) {
+    // Nothing
+  }
+};
+
 export const importXmasThemeAction = () => async (dispatch: ThunkDispatch<any, any, any>, getState: () => IGlobalState) => {
   try {
     const asyncStorageKey = computeAsyncStorageKey();
     const xmasSetting = (await getItemJson(asyncStorageKey)) as boolean | undefined;
     dispatch({ type: 'toggleXmasTheme', value: xmasSetting });
+    dispatch(updateShakeListenerAction());
     return xmasSetting ?? true; // default value
   } catch (e) {
     // If error, we reset to the inital behaviour => snow theme on
@@ -29,34 +80,12 @@ export const setXmasThemeAction = (xmasTheme: boolean) => async (dispatch: Thunk
     const asyncStorageKey = computeAsyncStorageKey();
     dispatch({ type: 'toggleXmasTheme', value: xmasTheme });
     await setItemJson(asyncStorageKey, xmasTheme);
+    dispatch(updateShakeListenerAction());
+    if (xmasTheme === true) {
+      dispatch(letItSnowAction());
+    }
   } catch (e) {
     // If error, we dactivate snow theme for now
     dispatch({ type: 'toggleXmasTheme', value: false });
-  }
-};
-
-const SNOW_DURATION = 30000;
-let snowfallTimer: NodeJS.Timeout;
-
-export const letItSnowAction = () => async (dispatch: ThunkDispatch<any, any, any>, getState: () => IGlobalState) => {
-  try {
-    dispatch({ type: 'setFlakes', value: true });
-    if (snowfallTimer) {
-      clearTimeout(snowfallTimer);
-    }
-    snowfallTimer = setTimeout(() => dispatch({ type: 'setFlakes', value: false }), SNOW_DURATION);
-  } catch (e) {
-    // ToDo: Error handling
-  }
-};
-
-export const stopItSnowAction = () => async (dispatch: ThunkDispatch<any, any, any>, getState: () => IGlobalState) => {
-  try {
-    dispatch({ type: 'setFlakes', value: false });
-    if (snowfallTimer) {
-      clearTimeout(snowfallTimer);
-    }
-  } catch (e) {
-    // ToDo: Error handling
   }
 };
