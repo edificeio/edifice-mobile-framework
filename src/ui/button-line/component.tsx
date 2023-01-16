ecaa4c5facbad56d6c4fc36319c3bf6181945eed
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-01-16 12:20:40 +0100

    fix(user): fix xmas button visibility + ButtonLineGroup component

--- src/ui/button-line/component.tsx
+++ src/ui/button-line/component.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/native';
 import I18n from 'i18n-js';
 import * as React from 'react';
+import * as ReactIs from 'react-is';
 
 import theme from '~/app/theme';
 import { UI_SIZES } from '~/framework/components/constants';
@@ -53,4 +54,36 @@ export const ButtonLine = ({
       <Icon name="arrow_down" color={theme.palette.primary.regular} style={styles.buttonLine_icon} />
     </TouchableOpacity>
   );
+};
+
+export const ButtonLineGroup = ({
+  children,
+  allowFirst = true,
+  allowLast = true,
+  allowAlone = true,
+}: React.PropsWithChildren & { allowFirst?: boolean; allowLast?: boolean; allowAlone?: boolean }) => {
+  const childrenAsArray = React.Children.toArray(children); // We use `childrenAsArray` instead of `count` to ignore empty/null/undefined nodes.
+  const mapFunction = (node: Parameters<Parameters<typeof childrenAsArray.map>[0]>[0], index: number) => {
+    if (ReactIs.isFragment(node)) {
+      return (
+        <ButtonLineGroup
+          allowFirst={index === 0}
+          allowLast={index === childrenAsArray.length - 1}
+          allowAlone={childrenAsArray.length === 1}>
+          {/* must cast this as ReactElement becasue ReactFragment type doesn't allow props (even though props does exist)*/}
+          {(node as React.ReactElement).props.children}
+        </ButtonLineGroup>
+      );
+    } else if (ReactIs.isElement(node)) {
+      return (
+        <ButtonLine
+          {...((node as React.ReactElement).props ?? {})}
+          first={allowFirst && childrenAsArray.length > 1 && index === 0}
+          last={allowLast && childrenAsArray.length > 1 && index === childrenAsArray.length - 1}
+          alone={allowAlone && childrenAsArray.length === 1}
+        />
+      );
+    } else return node;
+  };
+  return <>{childrenAsArray.map(mapFunction)}</>;
 };
