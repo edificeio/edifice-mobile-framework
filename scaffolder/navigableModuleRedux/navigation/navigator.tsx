66c6b6bf44bface19d220a18b513057cf16f2266
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-01-20 15:18:29 +0100

    chore: comment other screen in scaffolder

--- scaffolder/navigableModuleRedux/navigation/navigator.tsx
+++ scaffolder/navigableModuleRedux/navigation/navigator.tsx
@@ -6,7 +6,7 @@ import { IEntcoreApp, IEntcoreWidget } from '~/framework/util/moduleTool';
 import { {{moduleName | toCamelCase | capitalize}}NavigationParams, {{moduleName | toCamelCase}}RouteNames } from '.';
 import moduleConfig from '../moduleConfig';
 import {{moduleName | toCamelCase | capitalize}}HomeScreen, { computeNavBar as homeNavBar } from '../screens/home';
-import {{moduleName | toCamelCase | capitalize}}OtherScreen, { computeNavBar as otherNavBar } from '../screens/other';
+// import {{moduleName | toCamelCase | capitalize}}OtherScreen, { computeNavBar as otherNavBar } from '../screens/other';
 
 export default (apps: IEntcoreApp[], widgets: IEntcoreWidget[]) =>
   createModuleNavigator<{{moduleName | toCamelCase | capitalize}}NavigationParams>(moduleConfig.routeName, Stack => (
@@ -17,11 +17,11 @@ export default (apps: IEntcoreApp[], widgets: IEntcoreWidget[]) =>
         options={homeNavBar}
         initialParams={{}}
       />
-      <Stack.Screen
+      {/* <Stack.Screen
         name={{{moduleName | toCamelCase}}RouteNames.other}
         component={{{moduleName | toCamelCase | capitalize}}OtherScreen}
         options={otherNavBar}
         initialParams={{}}
-      />
+      /> */}
     </>
   ));
