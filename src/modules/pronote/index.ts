b7a98e2b590549b27d8b1441bfa7dd667c9d353d
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-01-26 16:10:23 +0100

    chore(pronote): MB-792 remove old pronote module

--- src/modules/pronote/index.ts
+++
@@ -1,7 +0,0 @@
-import { NavigableModule } from '~/framework/util/moduleTool';
-
-import config from './moduleConfig';
-import getRoot from './navigator';
-import reducer from './state/reducer';
-
-module.exports = new NavigableModule({ config, getRoot, reducer });
