a98c4cf7d8be23e2c1c85ebcac5d3de44591f3bb
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-01-31 16:17:25 +0100

    chore: add Scaffolder for RN6

---
+++ scaffolder/navigableModuleRN6/navigation/index.ts
@@ -0,0 +1,14 @@
+import { ParamListBase } from '@react-navigation/native';
+
+import moduleConfig from '../module-config';
+// import type { {{moduleName | toCamelCase | capitalize}}HomeScreenNavParams } from '../screens/home';
+// import type { {{moduleName | toCamelCase | capitalize}}OtherScreenNavParams } from '../screens/other';
+
+export const {{moduleName | toCamelCase}}RouteNames = {
+  // home: `${moduleConfig.routeName}` as 'home', // keep 'Home' as Typescript type for the main screen
+  // other: `${moduleConfig.routeName}/other` as 'other', // @scaffolder Typescript must have static types for code-completion. Use PascalCase for the type
+};
+export interface {{moduleName | toCamelCase | capitalize}}NavigationParams extends ParamListBase {
+  // home: {{moduleName | toCamelCase | capitalize}}HomeScreenNavParams;
+  // other: {{moduleName | toCamelCase | capitalize}}OtherScreenNavParams;
+}
