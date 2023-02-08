82f8474d815bbb3e2ff44f4a71d89dbb74c0d292
Léa De Amorin <43986934+leadmrn@users.noreply.github.com>
2023-02-08 11:34:28 +0100

    chore(blog): add notif to blog in rn6

--- src/framework/modules/blog/notif-handler.ts
+++ src/framework/modules/blog/notif-handler.ts
@@ -5,31 +5,61 @@
  * The notifHandler registers some behaviours for given notif types and event-types.
  * It applicates to both timelineNotififation and pushNotifications.
  */
+import { assertSession } from '~/framework/modules/auth/reducer';
+import { BlogNavigationParams, blogRouteNames } from '~/framework/modules/blog/navigation';
+import { blogService, blogUriCaptureFunction } from '~/framework/modules/blog/service';
 import { navigate } from '~/framework/navigation/helper';
+import { getAsResourceUriNotification } from '~/framework/util/notifications';
 import { NotifHandlerThunkAction, registerNotifHandlers } from '~/framework/util/notifications/routing';
 
-import { BlogNavigationParams, blogRouteNames } from './navigation';
+const handleBlogPostNotificationAction: NotifHandlerThunkAction = notification => async (dispatch, getState) => {
+  const navParams = {};
+  const blogNotif = getAsResourceUriNotification(notification);
+  if (!blogNotif) return { managed: 0 };
 
-const handleSomeNotificationAction: NotifHandlerThunkAction = notification => async (dispatch, getState) => {
-  // @scaffolder extract info from notification here
-  
-  // @scaffolder navigate somewhere here
+  navigate<BlogNavigationParams, typeof blogRouteNames.blogPostDetails>(blogRouteNames.blogPostDetails, {
+    notification: blogNotif,
+    useNotification: true,
+    ...navParams,
+  });
 
-  // @scaffolder put `return {managed: 0}` if notification is skipped
-  return {managed: 0}
-  
-  // return {
-  //   managed: 1,
-  //   trackInfo: { action: 'Blog', name: `${notification.type}.${notification['event-type']}` },
-  // };
+  return {
+    managed: 1,
+    trackInfo: { action: 'Blog', name: `${notification.type}.${notification['event-type']}` },
+  };
+};
+
+const handleBlogNotificationAction: NotifHandlerThunkAction = notification => async (dispatch, getState) => {
+  try {
+    const blogNotif = getAsResourceUriNotification(notification);
+    if (!blogNotif) return { managed: 0 };
+    const blogId = blogUriCaptureFunction(blogNotif.resource.uri).blogId;
+    if (!blogId) return { managed: 0 };
+    const session = assertSession();
+    const blogInfo = await blogService.get(session, blogId);
+    if (!blogInfo) return { managed: 0 };
+    navigate<BlogNavigationParams, typeof blogRouteNames.blogPostList>(blogRouteNames.blogPostList, {
+      selectedBlog: blogInfo,
+    });
+    return {
+      managed: 1,
+      trackInfo: { action: 'Blog', name: `${notification.type}.${notification['event-type']}` },
+    };
+  } catch {
+    return { managed: 0 };
+  }
 };
 
 export default () =>
   registerNotifHandlers([
     {
-      type: 'SOME-TYPE', // Replace this with the backend notification type
-      'event-type': 'SOME-EVENT-TYPE', // Replace this with the backend notification event-type
-      notifHandlerAction: handleSomeNotificationAction,
+      type: 'BLOG',
+      'event-type': ['PUBLISH-POST', 'SUBMIT-POST', 'PUBLISH-COMMENT'],
+      notifHandlerAction: handleBlogPostNotificationAction,
+    },
+    {
+      type: 'BLOG',
+      'event-type': 'SHARE',
+      notifHandlerAction: handleBlogNotificationAction,
     },
   ]);
- 
