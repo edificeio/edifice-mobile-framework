ca52561157388a328d83f1656a08b195dc842db4
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-02-14 20:12:14 +0100

    fix: remove crash when mediaplayer is displayed

--- src/framework/components/media/player/index.ts
+++ src/framework/components/media/player/index.ts
@@ -1,12 +1,12 @@
-import { NavigationInjectedProps } from 'react-navigation';
+import { navigate } from '~/framework/navigation/helper';
 
 import MediaPlayer from './component';
 import { MediaPlayerParams, MediaType } from './types';
 
 export { MediaType };
 
-export function openMediaPlayer(props: MediaPlayerParams, navigation: NavigationInjectedProps['navigation']) {
-  navigation.navigate('mediaPlayerModal', props);
+export function openMediaPlayer(props: MediaPlayerParams) {
+  navigate('mediaPlayerModal', props);
 }
 
 export default MediaPlayer;
