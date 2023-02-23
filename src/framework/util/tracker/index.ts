1e74f41a8334b9fdd5d0d014b58168c9acc3f921
Guillaume DROULEZ <guillaume.droulez@mac.com>
2023-02-23 20:02:55 +0100

    upgrade(rn6): Remove old DEPRECATED_getCurrentPlatform()  from trackers.

--- src/framework/util/tracker/index.ts
+++ src/framework/util/tracker/index.ts
@@ -6,13 +6,10 @@ import AppCenter from 'appcenter';
 import Analytics from 'appcenter-analytics';
 import Matomo from 'react-native-matomo';
 
-
-
-import { DEPRECATED_getCurrentPlatform } from '~/framework/util/_legacy_appConf';
-import appConf from '~/framework/util/appConf';
+import { assertSession } from '~/framework/modules/auth/reducer';
+import appConf, { Platform } from '~/framework/util/appConf';
 import { AnyNavigableModuleConfig, IAnyModuleConfig } from '~/framework/util/moduleTool';
 import { urlSigner } from '~/infra/oauth';
-
 
 export type TrackEventArgs = [string, string, string?, number?];
 export type TrackEventOfModuleArgs = [IAnyModuleConfig, string, string?, number?];
@@ -20,7 +17,9 @@ export type DoTrackArg = undefined | TrackEventOfModuleArgs;
 
 export abstract class AbstractTracker<OptionsType> {
   debugName: string;
+
   opts: OptionsType;
+
   protected _isReady: boolean;
 
   constructor(debugName: string, opts: OptionsType) {
@@ -34,12 +33,15 @@ export abstract class AbstractTracker<OptionsType> {
   }
 
   // Init procedure. Override _init() function to create custom trackers.
-  protected async _init() {}
+  protected async _init() {
+    // Nothing to do for AbstractTracker
+  }
+
   async init() {
     try {
       await this._init();
       this._isReady = true;
-    } catch (e) {
+    } catch {
       this._isReady = false;
     }
   }
@@ -48,6 +50,7 @@ export abstract class AbstractTracker<OptionsType> {
   protected _isDebugTracker(): boolean {
     return false;
   }
+
   isDebugTracker(): boolean {
     return this._isDebugTracker();
   }
@@ -56,13 +59,14 @@ export abstract class AbstractTracker<OptionsType> {
   protected async _setUserId(id: string): Promise<boolean> {
     throw new Error('not implemented');
   }
+
   async setUserId(id: string) {
     try {
       if (!this.isReady) {
         throw new Error('Tracker is not initialized');
       }
       await this._setUserId(id);
-    } catch (e) {
+    } catch {
       // TODO: Manage error
     }
   }
@@ -71,13 +75,14 @@ export abstract class AbstractTracker<OptionsType> {
   protected async _setCustomDimension(id: number, name: string, value: string): Promise<boolean> {
     throw new Error('not implemented');
   }
+
   async setCustomDimension(id: number, name: string, value: string) {
     try {
       if (!this.isReady) {
         throw new Error('Tracker is not initialized');
       }
       await this._setCustomDimension(id, name, value);
-    } catch (e) {
+    } catch {
       // TODO: Manage error
     }
   }
@@ -86,16 +91,18 @@ export abstract class AbstractTracker<OptionsType> {
   protected async _trackEvent(category: string, action: string, name?: string, value?: number): Promise<boolean> {
     throw new Error('not implemented');
   }
+
   async trackEvent(category: string, action: string, name?: string, value?: number) {
     try {
       if (!this.isReady) {
         throw new Error('Tracker is not initialized');
       }
       await this._trackEvent(category, action, name, value);
-    } catch (e) {
+    } catch {
       // TODO: Manage error
     }
   }
+
   async trackEventOfModule(moduleConfig: AnyNavigableModuleConfig, action: string, name?: string, value?: number) {
     await this.trackEvent(moduleConfig.trackingName, action, name, value);
   }
@@ -104,16 +111,18 @@ export abstract class AbstractTracker<OptionsType> {
   protected async _trackView(path: string[]): Promise<boolean> {
     throw new Error('not implemented');
   }
+
   async trackView(path: string[]) {
     try {
       if (!this.isReady) {
         throw new Error('Tracker is not initialized');
       }
       await this._trackView(path);
-    } catch (e) {
+    } catch {
       // TODO: Manage error
     }
   }
+
   async trackViewOfModule(moduleConfig: AnyNavigableModuleConfig, path: string[]) {
     await this._trackView([moduleConfig.routeName, ...path]);
   }
@@ -128,18 +137,22 @@ export class ConcreteMatomoTracker extends AbstractTracker<IMatomoTrackerOptions
   async _init() {
     return Matomo.initTracker(this.opts.url, this.opts.siteId);
   }
+
   async _setUserId(id: string) {
     await Matomo.setUserId(id);
     return true;
   }
+
   async _setCustomDimension(id: number, name: string, value: string) {
     await Matomo.setCustomDimension(id, value);
     return true;
   }
+
   async _trackEvent(category: string, action: string, name?: string, value?: number) {
     await Matomo.trackEvent(category, action, name, value);
     return true;
   }
+
   async _trackView(path: string[]) {
     const viewPath = path.join('/');
     await Matomo.trackScreen(viewPath, null);
@@ -149,20 +162,25 @@ export class ConcreteMatomoTracker extends AbstractTracker<IMatomoTrackerOptions
 
 export class ConcreteAppCenterTracker extends AbstractTracker<undefined> {
   protected _properties = {};
+
   async _init() {
     // Nothing to do, configuration comes from native appcenter config files
   }
+
   protected _isDebugTracker(): boolean {
     return true;
   }
+
   async _setUserId(id: string) {
     await AppCenter.setUserId(id);
     return true;
   }
+
   async _setCustomDimension(id: number, name: string, value: string) {
     this._properties[name] = value;
     return true;
   }
+
   async _trackEvent(category: string, action: string, name?: string, value?: number) {
     await Analytics.trackEvent(`${category} ${action} ${name} ${value}`, {
       category,
@@ -173,6 +191,7 @@ export class ConcreteAppCenterTracker extends AbstractTracker<undefined> {
     });
     return true;
   }
+
   async _trackView(path: string[]) {
     const viewPath = path.join('/');
     await Analytics.trackEvent(`View ${viewPath}`);
@@ -181,10 +200,15 @@ export class ConcreteAppCenterTracker extends AbstractTracker<undefined> {
 }
 
 export class ConcreteEntcoreTracker extends AbstractTracker<undefined> {
-  reportQueue: Request[] = [];
-  sending: boolean = false;
   errorCount: number = 0;
+
   lastModulename: string | undefined = undefined;
+
+  platform?: Platform = undefined;
+
+  reportQueue: Request[] = [];
+
+  sending: boolean = false;
 
   async sendReportQueue() {
     if (this.sending) return; // Once at a time
@@ -199,7 +223,7 @@ export class ConcreteEntcoreTracker extends AbstractTracker<undefined> {
         } else {
           throw new Error('    [EntcoreTracker] Report failed. ' + (await req?.text()));
         }
-      } catch (e) {
+      } catch {
         if (++this.errorCount >= 3) this.sending = false;
       }
     }
@@ -207,17 +231,21 @@ export class ConcreteEntcoreTracker extends AbstractTracker<undefined> {
   }
 
   async _init() {
-    // Nothing to do, configuration comes oauth connection
+    this.platform = assertSession()?.platform;
   }
+
   async _setUserId(id: string) {
     return false; // Nothing here
   }
+
   async _setCustomDimension(id: number, name: string, value: string) {
     return false; // Nothing here
   }
+
   async _trackEvent(category: string, action: string, name?: string, value?: number) {
     return false; // Nothing here
   }
+
   async _trackView(path: string[]) {
     const moduleName = (
       path[0] === 'timeline' ? (['blog', 'news', 'schoolbook'].includes(path[2]?.toLowerCase()) ? path[2] : 'timeline') : path[0]
@@ -234,9 +262,9 @@ export class ConcreteEntcoreTracker extends AbstractTracker<undefined> {
       viesco: 'Presences',
     };
     let willLog = false;
-    if (this.lastModulename !== moduleName && moduleAccessMap.hasOwnProperty(moduleName)) {
+    if (this.platform && this.lastModulename !== moduleName && Object.prototype.hasOwnProperty.call(moduleAccessMap, moduleName)) {
       this.reportQueue.push(
-        new Request(`${DEPRECATED_getCurrentPlatform()!.url}/infra/event/mobile/store`, {
+        new Request(`${this.platform!.url}/infra/event/mobile/store`, {
           method: 'POST',
           body: JSON.stringify({ module: moduleAccessMap[moduleName] }),
         }),
@@ -251,36 +279,47 @@ export class ConcreteEntcoreTracker extends AbstractTracker<undefined> {
 
 export class ConcreteTrackerSet {
   private _trackers: AbstractTracker<any>[] = [];
+
   constructor(...trackers: AbstractTracker<any>[]) {
     this._trackers = trackers;
   }
+
   addTracker(t: AbstractTracker<any>) {
     this._trackers.push(t);
   }
+
   async init() {
     await Promise.all(this._trackers.map(t => t.init()));
   }
+
   async trackDebugEvent(category: string, action: string, name?: string, value?: number) {
     await Promise.all(this._trackers.filter(t => t.isDebugTracker()).map(t => t.trackEvent(category, action, name, value)));
   }
+
   async trackEvent(category: string, action: string, name?: string, value?: number) {
     await Promise.all(this._trackers.map(t => t.trackEvent(category, action, name, value)));
   }
+
   async trackEventOfModule(moduleConfig: IAnyModuleConfig, action: string, name?: string, value?: number) {
     await this.trackEvent(moduleConfig.trackingName, action, name, value);
   }
+
   async trackView(path: string[]) {
     await Promise.all(this._trackers.map(t => t.trackView(path)));
   }
+
   async trackViewOfModule(moduleConfig: AnyNavigableModuleConfig, path: string[]) {
     await this.trackView([moduleConfig.routeName, ...path]);
   }
+
   async setUserId(id: string) {
     await Promise.all(this._trackers.map(t => t.setUserId(id)));
   }
+
   async setCustomDimension(id: number, name: string, value: string) {
     await Promise.all(this._trackers.map(t => t.setCustomDimension(id, name, value)));
   }
+
   get isReady() {
     return this._trackers.every(t => t.isReady);
   }
