0742a17174f79d754f942da1ed660af3c454b329
Guillaume Roux <guillaume.roux@cgi.com>
2023-02-28 15:34:10 +0100

    upgrade(rn6): delete dashboard and presences old modules

--- src/modules/viescolaire/presences/reducers/teacherCourseRegister.ts
+++
@@ -1,4 +0,0 @@
-import { createSessionAsyncReducer } from '~/infra/redux/async2';
-import { actionTypes, initialStateRegister } from '~/modules/viescolaire/presences/state/teacherCourseRegister';
-
-export default createSessionAsyncReducer(initialStateRegister, actionTypes);
