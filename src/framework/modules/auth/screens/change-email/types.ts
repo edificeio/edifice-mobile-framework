0cd8cbea0168d1a916510ffcfa77d09d3c46ab7a
Camille MESHORER <camille.meshorer@opendigitaleducation.com>
2023-03-24 10:39:03 +0100

    chore(rn6): 2fa change-email

--- src/framework/modules/auth/screens/change-email/types.ts
+++ src/framework/modules/auth/screens/change-email/types.ts
@@ -1,23 +1,36 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
+import { ModificationType } from '~/framework/modules/user/screens/home/types';
 import { Platform } from '~/framework/util/appConf';
 
-import { IAuthContext } from '../../model';
+import { IAuthContext, IAuthCredentials } from '../../model';
 import type { AuthRouteNames, IAuthNavigationParams } from '../../navigation';
+
+export interface AuthChangeEmailScreenDispatchProps {
+  onLogout(): void;
+}
+
+export interface AuthChangeEmailScreenNavParams {
+  context: IAuthContext;
+  credentials: IAuthCredentials;
+  defaultEmail: string;
+  modificationType: ModificationType;
+  navBarTitle: string;
+  platform: Platform;
+}
 
 export interface AuthChangeEmailScreenProps {}
 
-export interface AuthChangeEmailScreenNavParams {
-  platform: Platform;
-  context: IAuthContext;
-}
-
 export interface AuthChangeEmailScreenStoreProps {}
 
-export interface AuthChangeEmailScreenDispatchProps {}
-
 export interface AuthChangeEmailScreenPrivateProps
-  extends NativeStackScreenProps<IAuthNavigationParams, AuthRouteNames.changeEmail>,
+  extends NativeStackScreenProps<IAuthNavigationParams, typeof AuthRouteNames.changeEmail>,
     AuthChangeEmailScreenProps,
     AuthChangeEmailScreenStoreProps,
     AuthChangeEmailScreenDispatchProps {}
+
+export enum EmailState {
+  EMAIL_ALREADY_VERIFIED = 'emailAlreadyVerified',
+  EMAIL_FORMAT_INVALID = 'emailFormatInvalid',
+  PRISTINE = 'pristine',
+}
