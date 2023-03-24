65ce01d83c359b3352d0723399af1cfd7928b8e6
Camille MESHORER <camille.meshorer@opendigitaleducation.com>
2023-03-24 12:02:16 +0100

    chore(rn6): 2fa change-mobile

--- src/framework/modules/auth/screens/change-mobile/types.ts
+++ src/framework/modules/auth/screens/change-mobile/types.ts
@@ -1,23 +1,38 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
+import { ModificationType } from '~/framework/modules/user/screens/home/types';
 import { Platform } from '~/framework/util/appConf';
+import type { IUpdatableProfileValues } from '~/user/actions/profile';
 
-import { IAuthContext } from '../../model';
+import { IAuthContext, IAuthCredentials } from '../../model';
 import type { AuthRouteNames, IAuthNavigationParams } from '../../navigation';
+
+export interface AuthChangeMobileScreenDispatchProps {
+  onLogout(): void;
+  onSaveNewMobile(updatedProfileValues: IUpdatableProfileValues): void;
+}
+
+export interface AuthChangeMobileScreenNavParams {
+  context: IAuthContext;
+  credentials: IAuthCredentials;
+  defaultMobile: string;
+  modificationType: ModificationType;
+  navBarTitle: string;
+  platform: Platform;
+}
 
 export interface AuthChangeMobileScreenProps {}
 
-export interface AuthChangeMobileScreenNavParams {
-  platform: Platform;
-  context: IAuthContext;
-}
-
 export interface AuthChangeMobileScreenStoreProps {}
 
-export interface AuthChangeMobileScreenDispatchProps {}
-
 export interface AuthChangeMobileScreenPrivateProps
-  extends NativeStackScreenProps<IAuthNavigationParams, AuthRouteNames.changeMobile>,
+  extends NativeStackScreenProps<IAuthNavigationParams, typeof AuthRouteNames.changeMobile>,
     AuthChangeMobileScreenProps,
     AuthChangeMobileScreenStoreProps,
     AuthChangeMobileScreenDispatchProps {}
+
+export enum MobileState {
+  MOBILE_ALREADY_VERIFIED = 'mobileAlreadyVerified',
+  MOBILE_FORMAT_INVALID = 'mobileFormatInvalid',
+  PRISTINE = 'pristine',
+}
