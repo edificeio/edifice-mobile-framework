472f2a02a194c75c1d107d57d3e0ff7caba010e1
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-04-17 18:43:28 +0200

    chore: cleanup some files

--- src/ui/icons/SearchIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import theme from '~/app/theme';
-
-import { Icon } from './Icon';
-
-export const SearchIcon = () => <Icon size={22} name={'search'} color={theme.ui.text.inverse} />;
-
-export const CloseIcon = () => <Icon size={22} name={'close'} color={theme.ui.text.inverse} />;
