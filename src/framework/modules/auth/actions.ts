8c6a5e8a3f6cd22659f8fda48d67e540f77b00aa
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-04-18 17:03:13 +0200

    fix: MB-1516 fix store flush on session end

--- src/framework/modules/auth/actions.ts
+++ src/framework/modules/auth/actions.ts
@@ -5,6 +5,7 @@ import { ThunkDispatch } from 'redux-thunk';
 
 import { SupportedLocales } from '~/app/i18n';
 import { Platform } from '~/framework/util/appConf';
+import { createEndSessionAction } from '~/framework/util/redux/reducerFactory';
 import { Trackers } from '~/framework/util/tracker';
 import { clearRequestsCache } from '~/infra/fetchWithCache';
 import { OAuth2ErrorCode, destroyOAuth2, uniqueId, urlSigner } from '~/infra/oauth';
@@ -336,6 +337,7 @@ function sessionDestroyAction(platform: Platform) {
     await destroyOAuth2();
     // Validate log out
     dispatch(authActions.sessionEnd());
+    dispatch(createEndSessionAction()); // flush sessionReducers
   };
 }
 
