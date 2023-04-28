67efa587e80078bb3a56a41bfe4858e0db932b30
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-04-28 15:19:43 +0200

    fix: MB-1570 toaster show 3 sec

--- src/framework/components/toast/component.tsx
+++ src/framework/components/toast/component.tsx
@@ -3,6 +3,8 @@ import ToastMessage from 'react-native-toast-message';
 
 import Feedback from '~/framework/util/feedback/feedback';
 
+const TOAST_VISIBILITY_TIME = 3000;
+
 export default class Toast {
   private static showToast(type: string, text: string) {
     ToastMessage.show({
@@ -10,12 +12,14 @@ export default class Toast {
       text1: text,
       position: 'top',
       topOffset: 8,
+      visibilityTime: TOAST_VISIBILITY_TIME,
     });
   }
 
   static showError(text: string = I18n.t('common.error.text')) {
     this.showToast('error', text);
     Feedback.errorDisplayed();
+    e;
   }
 
   static showInfo(text: string) {
