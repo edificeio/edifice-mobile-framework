5b31f7f93be67a28f43fb9a7dee6a79a1d341f2c
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-05-09 15:57:09 +0200

    fix: tabBar icon size on Android

--- src/framework/navigation/mainNavigation.tsx
+++ src/framework/navigation/mainNavigation.tsx
@@ -180,8 +180,19 @@ export function useTabNavigator(apps?: IEntcoreApp[], widgets?: IEntcoreWidget[]
           height: UI_SIZES.elements.tabbarHeight + UI_SIZES.screen.bottomInset,
           ...getAndroidTabBarStyleForNavState(navigation.getState()),
         },
-        tabBarLabelStyle: { fontSize: TextSizeStyle.Small.fontSize, marginBottom: UI_SIZES.elements.tabbarLabelMargin },
-        tabBarIconStyle: { marginTop: UI_SIZES.elements.tabbarLabelMargin },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          lineHeight: undefined,
+          marginBottom: UI_SIZES.elements.tabbarLabelMarginBottom,
+        },
+        tabBarIconStyle: {
+          marginTop: UI_SIZES.elements.tabbarLabelMarginTop,
+          height: UI_SIZES.elements.tabbarIconSize,
+          width: UI_SIZES.elements.tabbarIconSize,
+        },
+        tabBarItemStyle: {
+          justifyContent: 'space-between',
+        },
         tabBarActiveTintColor: theme.palette.primary.regular.toString(), // 😡 F U React Nav 6, using plain string instead of ColorValue
         tabBarInactiveTintColor: theme.ui.text.light.toString(), // 😡 F U React Nav 6, using plain string instead of ColorValue
         tabBarHideOnKeyboard: Platform.select({ ios: false, android: true }),
