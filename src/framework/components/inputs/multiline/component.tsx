d7d8b2872563eaeb0203be3447d31e64833c2d02
Camille MESHORER <camille.meshorer@opendigitaleducation.com>
2023-08-04 16:55:12 +0200

    chore: MB-1851, add height logic inside multiline-text-input

--- src/framework/components/inputs/multiline/component.tsx
+++ src/framework/components/inputs/multiline/component.tsx
@@ -3,18 +3,26 @@ import { Platform, TextInput as RNTextInput } from 'react-native';
 
 import TextInput from '~/framework/components/inputs/text';
 import { MultilineTextInputProps } from './types';
-import { UI_SIZES } from '~/framework/components/constants';
 
 import styles from './styles';
 
 const MultilineTextInput = forwardRef<RNTextInput, MultilineTextInputProps>((props: MultilineTextInputProps, ref) => {
-  const heightInput = 22 * props.numberOfLines + 2 * UI_SIZES.spacing.small;
+  const { numberOfLines } = props;
+  const [contentNumberOfLines, setContentNumberOfLines] = React.useState(1);
+  let heightInput: number | undefined = styles.multilineInput.lineHeight * numberOfLines + 2 * styles.multilineInput.paddingTop;
+  if (contentNumberOfLines > numberOfLines) heightInput = undefined;
+
   return (
     <TextInput
       {...props}
-      multiline={true}
-      numberOfLines={Platform.OS === 'ios' ? undefined : props.numberOfLines}
-      style={[{ height: Platform.OS === 'ios' ? heightInput : undefined }, styles.multilineInput]}
+      multiline
+      onContentSizeChange={e => {
+        const contentHeight = e.nativeEvent.contentSize.height;
+        const contentNumberOfLines = contentHeight / styles.multilineInput.lineHeight;
+        setContentNumberOfLines(contentNumberOfLines);
+      }}
+      numberOfLines={Platform.OS === 'ios' ? undefined : numberOfLines}
+      style={[styles.multilineInput, { height: Platform.OS === 'ios' ? heightInput : undefined }]}
       ref={ref}
     />
   );
