e0da7360c1c2fe7f80198beac2b1bbcc2fa9f90f
Camille MESHORER <camille.meshorer@opendigitaleducation.com>
2023-08-21 15:50:05 +0200

    feat(homework): MB-187, timeline access when rights

--- src/framework/modules/homework/rights.ts
+++ src/framework/modules/homework/rights.ts
@@ -1,7 +1,12 @@
 /**
  * Homework workflow
  */
+import { I18n } from '~/app/i18n';
 import { ISession } from '~/framework/modules/auth/model';
+import { navigate } from '~/framework/navigation/helper';
+
+import { homeworkRouteNames } from './navigation';
+import { registerTimelineWorkflow } from '../timeline/timeline-modules';
 
 export const viewHomeworkResourceRight = 'fr.wseduc.homeworks.controllers.HomeworksController|view';
 export const createHomeworkResourceRight = 'fr.wseduc.homeworks.controllers.HomeworksController|createHomework';
@@ -10,3 +15,16 @@ export const getHomeworkWorkflowInformation = (session: ISession) => ({
   view: session.authorizedActions.some(a => a.name === viewHomeworkResourceRight),
   create: session.authorizedActions.some(a => a.name === createHomeworkResourceRight),
 });
+
+export default () =>
+  registerTimelineWorkflow(session => {
+    const wk = getHomeworkWorkflowInformation(session);
+    return (
+      wk.create && {
+        title: I18n.get('homework-resourcename'),
+        action: () => {
+          navigate(homeworkRouteNames.home);
+        },
+      }
+    );
+  });
