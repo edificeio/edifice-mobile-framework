03acb1ef694a70410d1818dbc46844a4f73dc3e3
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-09-01 16:31:15 +0200

    feat: MB-1954 end-of-the-road screen for MonLyceeNet

--- src/framework/navigation/RootNavigator.tsx
+++ src/framework/navigation/RootNavigator.tsx
@@ -2,28 +2,24 @@
  * Startup is a conditionnaly rendered content based on app startup status.
  * It can render the SplashScreen, auth flow or main flow in function of token loading and status.
  */
-import { NavigationContainer } from '@react-navigation/native';
+import { Header } from '@react-navigation/elements';
 import * as React from 'react';
-import { Platform, StatusBar } from 'react-native';
+import { Platform, ScrollView, StatusBar, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 
-import { useAppStartup } from '~/app/startup';
 import { IGlobalState } from '~/app/store';
 import theme from '~/app/theme';
-import { RootToastHandler } from '~/framework/components/toast';
 import { ILoginResult } from '~/framework/modules/auth/actions';
 import { ISession } from '~/framework/modules/auth/model';
-import { getAuthNavigationState } from '~/framework/modules/auth/navigation';
-import useAuthNavigation from '~/framework/modules/auth/navigation/navigator';
 import { getState as getAuthState, getSession } from '~/framework/modules/auth/reducer';
-import { AppPushNotificationHandlerComponent } from '~/framework/util/notifications/cloudMessaging';
-import { useNavigationTracker } from '~/framework/util/tracker/useNavigationTracker';
+import PFMonLycee from '~/platforms/idf';
 
-import { navigationRef } from './helper';
-import { useMainNavigation } from './mainNavigation';
-import modals from './modals/navigator';
+import { UI_SIZES } from '../components/constants';
+import { PageViewStyle } from '../components/page';
+import { PFLogo } from '../components/pfLogo';
+import { BodyText, TextFontStyle } from '../components/text';
 import { getTypedRootStack } from './navigators';
 import { StartupState, getState as getAppStartupState } from './redux';
 
@@ -48,60 +44,88 @@ export type RootNavigatorProps = RootNavigatorStoreProps;
 const RootStack = getTypedRootStack();
 
 function RootNavigator(props: RootNavigatorProps) {
-  const { logged, session, isReady, autoLoginResult, dispatch } = props;
-  const isFullyLogged = !!(logged && session); // Partial sessions scenarios have session = {...} && logged = false, and must stay on auth stack.
-
   React.useEffect(() => {
     if (Platform.OS === 'android') StatusBar.setBackgroundColor(theme.palette.primary.regular);
+    StatusBar.setBarStyle('light-content');
+    SplashScreen.hide();
   }, []);
 
   // === Compute initial auth state ===
   // ToDo : verify that the state is correctly reset after logout & auto-login
 
-  const lastLoadedPlatform = useAppStartup(dispatch, session?.platform);
-  const initialNavState = React.useMemo(() => {
-    return isReady && !logged ? getAuthNavigationState(lastLoadedPlatform, autoLoginResult) : undefined;
-  }, [isReady, logged, lastLoadedPlatform, autoLoginResult]);
+  // const lastLoadedPlatform = useAppStartup(dispatch, session?.platform);
+  // const initialNavState = React.useMemo(() => {
+  //   return isReady && !logged ? getAuthNavigationState(lastLoadedPlatform, autoLoginResult) : undefined;
+  // }, [isReady, logged, lastLoadedPlatform, autoLoginResult]);
 
   // === If initialNavState changed during the runtime, we need to update it in navigationRef by hand ===
-  React.useLayoutEffect(() => {
-    // useLayoutEffect is used to prevent to have a one-frame flash showing the old navigation state
-    if (initialNavState && navigationRef.isReady()) navigationRef.reset(initialNavState);
-  }, [initialNavState]);
+  // React.useLayoutEffect(() => {
+  //   // useLayoutEffect is used to prevent to have a one-frame flash showing the old navigation state
+  //   if (initialNavState && navigationRef.isReady()) navigationRef.reset(initialNavState);
+  // }, [initialNavState]);
 
   // === Auth/Main switch ===
-  const mainNavigation = useMainNavigation(session?.apps ?? [], session?.widgets ?? []);
-  const authNavigation = useAuthNavigation();
-  const routes = React.useMemo(() => {
-    return isFullyLogged ? mainNavigation : authNavigation;
-  }, [authNavigation, isFullyLogged, mainNavigation]);
+  // const mainNavigation = useMainNavigation(session?.apps ?? [], session?.widgets ?? []);
+  // const authNavigation = useAuthNavigation();
+  // const routes = React.useMemo(() => {
+  //   return isFullyLogged ? mainNavigation : authNavigation;
+  // }, [authNavigation, isFullyLogged, mainNavigation]);
 
   // No need to initialize navState when fully logged, because it will load the default MainStack behaviour (= Tabs view)
 
   // === Render navigation container with initialState ===
 
-  const trackNavState = useNavigationTracker();
+  // const trackNavState = useNavigationTracker();
 
   const ret = React.useMemo(() => {
     return (
       <>
-        <SplashScreenComponent key={isReady} />
-        {isReady ? (
-          <>
-            <NavigationContainer ref={navigationRef} initialState={initialNavState} onStateChange={trackNavState}>
-              <AppPushNotificationHandlerComponent>
-                <RootStack.Navigator screenOptions={{ headerShown: true }}>
-                  {routes}
-                  {modals}
-                </RootStack.Navigator>
-              </AppPushNotificationHandlerComponent>
-              <RootToastHandler />
-            </NavigationContainer>
-          </>
-        ) : null}
+        <Header
+          {...{
+            title: 'MonLycée.net',
+            headerStyle: {
+              backgroundColor: theme.palette.primary.regular,
+            },
+            headerTitleAlign: 'center',
+            headerTitleStyle: {
+              ...TextFontStyle.Bold,
+              color: theme.ui.text.inverse,
+            },
+            headerTintColor: theme.ui.text.inverse.toString(),
+            headerBackVisible: false,
+            headerShadowVisible: true,
+            headerBackButtonMenuEnabled: false,
+            freezeOnBlur: true,
+          }}
+        />
+        <ScrollView
+          alwaysBounceVertical={false}
+          style={{
+            backgroundColor: theme.ui.background.page,
+          }}
+          contentContainerStyle={{
+            flex: 1,
+          }}>
+          <PageViewStyle
+            style={{
+              alignItems: 'center',
+              paddingVertical: UI_SIZES.spacing.huge,
+              paddingHorizontal: UI_SIZES.spacing.big,
+              flex: 1,
+              justifyContent: 'space-evenly',
+            }}>
+            <View style={{ flex: 1, justifyContent: 'center', transform: 'scale(1.333)' }}>
+              <PFLogo pf={PFMonLycee.prod} />
+            </View>
+            <BodyText style={{ marginVertical: UI_SIZES.spacing.huge, textAlign: 'center', flex: 2 }}>
+              L'application mobile Monlycee.net n'est plus disponible depuis le 01/09/2023 et sera remplacée par un autre service.
+              Nous vous invitons à vous connecter sur votre plateforme web monlycee.net.
+            </BodyText>
+          </PageViewStyle>
+        </ScrollView>
       </>
     );
-  }, [isReady, initialNavState, trackNavState, routes]);
+  }, []);
 
   return ret;
 }
