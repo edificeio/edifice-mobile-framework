9131865c461f2dde3492d1ae6e71acca113cddfe
Guillaume Roux <guillaume.roux@cgi.com>
2023-09-29 14:21:10 +0200

    fix(viescolaire): MB-2099 prevent child picker from having child without class selected

--- src/framework/modules/viescolaire/common/components/ChildPicker.tsx
+++ src/framework/modules/viescolaire/common/components/ChildPicker.tsx
@@ -17,7 +17,7 @@ interface ChildPickerDispatchProps {
 }
 
 interface ChildPickerProps extends ChildPickerDispatchProps {
-  initialSelectedChildId: string;
+  selectedChildId: string;
   userChildren: UserChildrenFlattened;
   contentContainerStyle?: ViewStyle;
   style?: ViewStyle;
@@ -25,30 +25,19 @@ interface ChildPickerProps extends ChildPickerDispatchProps {
 }
 
 const ChildPicker = (props: ChildPickerProps) => {
-  const [selectedChildId, setSelectedChildId] = React.useState(props.initialSelectedChildId);
-
   React.useEffect(() => {
     props.tryLoadStoredChild();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  React.useEffect(() => {
-    const { initialSelectedChildId, userId } = props;
-    if (selectedChildId && selectedChildId !== initialSelectedChildId) props.handleSelectChild(selectedChildId, userId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedChildId]);
-
-  React.useEffect(() => {
-    if (props.initialSelectedChildId !== selectedChildId) setSelectedChildId(props.initialSelectedChildId);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [props.initialSelectedChildId]);
+  const selectChild = (id: string) => props.handleSelectChild(id, props.userId);
 
   return props.userChildren.length > 1 ? (
     <UserList
       horizontal
       data={props.userChildren.map(child => ({ id: child.id, name: child.firstName }))}
-      selectedId={selectedChildId}
-      onSelect={setSelectedChildId}
+      selectedId={props.selectedChildId}
+      onSelect={selectChild}
       style={props.style}
       contentContainerStyle={props.contentContainerStyle}
     />
@@ -61,7 +50,7 @@ export default connect(
     const session = getSession();
 
     return {
-      initialSelectedChildId: dashboardState.selectedChildId,
+      selectedChildId: dashboardState.selectedChildId,
       userChildren: getFlattenedChildren(session?.user.children)?.filter(c => c.classesNames.length > 0) ?? [],
       userId: session?.user.id,
     };
