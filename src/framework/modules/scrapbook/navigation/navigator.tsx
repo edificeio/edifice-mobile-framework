5b5d7e35fe8da12845911ff6f3d30dcafd4fe6e4
Léa DE AMORIN <leadeamorin.ps@gmail.com>
2023-11-02 19:02:02 +0100

    feat(scrapbook): add scrapbook to my apps

--- src/framework/modules/scrapbook/navigation/navigator.tsx
+++ src/framework/modules/scrapbook/navigation/navigator.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import moduleConfig from '~/framework/modules/scrapbook/module-config';
 import ScrapbookDetailsScreen, { computeNavBar as detailsNavBar } from '~/framework/modules/scrapbook/screens/details';
+import ScrapbookHomeScreen, { computeNavBar as homeNavBar } from '~/framework/modules/scrapbook/screens/home';
 import { setModalModeForRoutes } from '~/framework/navigation/hideTabBarAndroid';
 import { createModuleNavigator } from '~/framework/navigation/moduleScreens';
 import { IEntcoreApp, IEntcoreWidget } from '~/framework/util/moduleTool';
@@ -10,14 +11,17 @@ import { ScrapbookNavigationParams, scrapbookRouteNames } from '.';
 
 export default (apps: IEntcoreApp[], widgets: IEntcoreWidget[]) =>
   createModuleNavigator<ScrapbookNavigationParams>(moduleConfig.name, Stack => (
-    <Stack.Group screenOptions={{ presentation: 'fullScreenModal' }}>
-      <Stack.Screen
-        name={scrapbookRouteNames.details}
-        component={ScrapbookDetailsScreen}
-        options={detailsNavBar}
-        initialParams={{}}
-      />
-    </Stack.Group>
+    <>
+      <Stack.Screen name={scrapbookRouteNames.home} component={ScrapbookHomeScreen} options={homeNavBar} initialParams={{}} />
+      <Stack.Group screenOptions={{ presentation: 'fullScreenModal' }}>
+        <Stack.Screen
+          name={scrapbookRouteNames.details}
+          component={ScrapbookDetailsScreen}
+          options={detailsNavBar}
+          initialParams={{}}
+        />
+      </Stack.Group>
+    </>
   ));
 
 setModalModeForRoutes([scrapbookRouteNames.details]);
