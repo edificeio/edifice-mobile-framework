c2f101f9f5537af9ea14ed4b9447f66ee084b9af
Valentin Mourot <valentin.mourot@opendigitaleducation.com>
2023-11-27 11:08:39 +0100

    chore: add babel pluginsfor private methods

--- babel.config.js
+++ babel.config.js
@@ -2,12 +2,21 @@ module.exports = function (api) {
   if (api.env('production')) {
     return {
       presets: ['module:metro-react-native-babel-preset'],
-      plugins: ['transform-remove-console', 'react-native-reanimated/plugin'],
+      plugins: [
+        '@babel/plugin-transform-flow-strip-types',
+        'transform-remove-console',
+        'react-native-reanimated/plugin',
+        ['@babel/plugin-transform-private-methods', { loose: true }],
+      ],
     };
   }
 
   return {
     presets: ['module:metro-react-native-babel-preset'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      '@babel/plugin-transform-flow-strip-types',
+      'react-native-reanimated/plugin',
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+    ],
   };
 };
