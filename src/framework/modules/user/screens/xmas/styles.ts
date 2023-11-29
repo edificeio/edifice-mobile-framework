d554ce9f7b73db27efb9cf9f80d6e6943b9865b5
Camille MESHORER <camille.meshorer@opendigitaleducation.com>
2023-11-29 18:13:32 +0100

    feat(xmas): MB-2314, xmas screen design + jingle sound + react-native-sound package

--- src/framework/modules/user/screens/xmas/styles.ts
+++ src/framework/modules/user/screens/xmas/styles.ts
@@ -1,19 +1,53 @@
 import { StyleSheet } from 'react-native';
 
+import theme from '~/app/theme';
 import { UI_SIZES } from '~/framework/components/constants';
 
 export default StyleSheet.create({
-  textContainer: { paddingHorizontal: UI_SIZES.spacing.medium },
+  hintText: {
+    flex: 1,
+  },
+  informationBar: {
+    backgroundColor: theme.palette.complementary.blue.regular,
+    width: 5,
+    height: '100%',
+    borderTopLeftRadius: UI_SIZES.radius.medium,
+    borderBottomLeftRadius: UI_SIZES.radius.medium,
+  },
+  informationContainer: {
+    borderWidth: UI_SIZES.elements.border.default,
+    borderColor: theme.palette.complementary.blue.regular,
+    marginTop: UI_SIZES.spacing.medium,
+    borderRadius: UI_SIZES.radius.medium,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  informationIcon: {
+    marginHorizontal: UI_SIZES.spacing.minor,
+  },
+  page: {
+    padding: UI_SIZES.spacing.medium,
+    paddingBottom: 0,
+  },
   toggleContainer: {
     flexDirection: 'row',
-    paddingTop: UI_SIZES.spacing.large,
-    paddingBottom: UI_SIZES.spacing.major,
+    marginTop: UI_SIZES.spacing.medium,
     justifyContent: 'space-between',
+  },
+  wishTextContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  xmasTree: {
+    position: 'absolute',
+    bottom: UI_SIZES.spacing.minor,
+    right: -UI_SIZES.spacing.small,
   },
   xmasTreeContainer: {
     flex: 1,
+    flexDirection: 'row',
   },
-  xmasTree: {
-    left: 120,
+  xmasTreeSubcontainer: {
+    flex: 1,
   },
 });
