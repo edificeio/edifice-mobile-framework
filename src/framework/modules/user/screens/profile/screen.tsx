e6229c0e4453d19c7255f78a35c33123179a1fc9
Léa DE AMORIN <leadeamorin.ps@gmail.com>
2023-11-30 22:18:35 +0100

    fix(profile): MB-2323 , relative can navigate on children profile

--- src/framework/modules/user/screens/profile/screen.tsx
+++ src/framework/modules/user/screens/profile/screen.tsx
@@ -233,7 +233,10 @@ const UserProfileScreen = (props: ProfilePageProps) => {
   };
 
   const renderPersonFamily = user => {
-    if (!isMyProfile && (session?.user.type === UserType.Teacher || session?.user.type === UserType.Personnel))
+    if (
+      (!isMyProfile && (session?.user.type === UserType.Teacher || session?.user.type === UserType.Personnel)) ||
+      (isMyProfile && session?.user.type === UserType.Relative)
+    )
       return (
         <TouchableOpacity
           key={user.relatedId}
