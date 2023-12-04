210f5d893114cba702a69885674eb64961640b01
Camille MESHORER <camille.meshorer@opendigitaleducation.com>
2023-12-04 17:45:27 +0100

    fix(xmas): MB-2333, android toggle colors

--- src/framework/components/toggle.tsx
+++ src/framework/components/toggle.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Switch } from 'react-native';
+import { Platform, StyleSheet, Switch } from 'react-native';
 
 import theme from '~/app/theme';
+import { UI_SIZES } from './constants';
 
 export interface IToggleProps {
   checked: boolean;
@@ -9,8 +10,16 @@ export interface IToggleProps {
   disabled?: boolean;
 }
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.palette.grey.pearl,
+    borderRadius: UI_SIZES.radius.mediumPlus,
+  },
+});
+
 export const Toggle = ({ checked, disabled, onCheckChange }: IToggleProps) => (
   <Switch
+    style={Platform.select({ android: [styles.container, disabled && { backgroundColor: theme.palette.grey.fog }] })}
     value={checked}
     onValueChange={() => onCheckChange && onCheckChange()}
     trackColor={{ false: theme.palette.grey.fog, true: theme.palette.secondary.regular }}
