db4648d25c8c359c05fbd27700216bb41cc2c688
Guillaume DROULEZ <guillaume.droulez@edifice.io>
2023-12-28 15:54:01 +0100

    chore(i18n): sync phrase-translations with dev/1.12.0

--- src/framework/modules/auth/screens/login-home/index.ts
+++
@@ -1,6 +0,0 @@
-import LoginHomeScreen from './screen';
-import type { LoginHomeScreenNavParams, LoginHomeScreenProps } from './types';
-
-export default LoginHomeScreen;
-export { LoginHomeScreenNavParams, LoginHomeScreenProps };
-export { computeNavBar } from './screen';
