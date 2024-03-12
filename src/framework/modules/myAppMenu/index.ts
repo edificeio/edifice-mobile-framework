ad0312059324c8b0a36ac8d136358a1198fb6e51
Valentin Mourot <thaledric@gmail.com>
2024-03-12 15:48:38 +0100

    chore(timeline): MB-2016 new storage for myapps

--- src/framework/modules/myAppMenu/index.ts
+++ src/framework/modules/myAppMenu/index.ts
@@ -2,7 +2,8 @@ import { NavigableModule } from '~/framework/util/moduleTool';
 
 import config from './moduleConfig';
 import getRoot from './navigation/navigator';
+import { storage } from './storage';
 
 const reducer = () => null;
 
-module.exports = new NavigableModule({ config, getRoot, reducer });
+module.exports = new NavigableModule({ config, getRoot, reducer, storage });
---
+++ src/framework/modules/myAppMenu/storage.ts
@@ -0,0 +1,17 @@
+import { Storage } from '~/framework/util/storage';
+
+import moduleConfig from './moduleConfig';
+
+export interface MyAppsStorageData {
+  'infobubble-ack': boolean;
+}
+
+const oldStorageKey = 'infoBubbleAck-myAppsScreen.redirect';
+
+export const storage = Storage.create<MyAppsStorageData>(moduleConfig).setAppInit(function () {
+  const oldValue = Storage.global.getString(oldStorageKey) as 'true' | 'false' | undefined;
+  if (oldValue === 'true') {
+    this.set('infobubble-ack', true);
+  }
+  Storage.global.delete(oldStorageKey);
+});
