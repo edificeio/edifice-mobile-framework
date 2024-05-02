f487246e1151ea80f3c15c05190793c746f5906b
Léa DE AMORIN <leadeamorin.ps@gmail.com>
2024-05-02 14:04:57 +0200

    fix(editor): MB-2642 , fix open video when click on play button

--- src/framework/components/inputs/rich-text/editor/editor.js
+++ src/framework/components/inputs/rich-text/editor/editor.js
@@ -161,7 +161,7 @@ function createHTML(options = {}) {
         th {text-align: left; background-color: ${theme.palette.grey.pearl};}
         iframe {border: none; max-width: 100%;}
         .video-wrapper {position: relative}
-        .video-wrapper::before {content: ""; background-image: url(${playIcon}); background-size: ${playIconSize}px ${playIconSize}px; height: ${playIconSize}px; width: ${playIconSize}px; position: absolute; top: 0; left: 0; z-index: 1; top: 50%; left: 50%; transform: translate(-50%, -50%);}
+        .video-wrapper::before {content: ""; background-image: url(${playIcon}); background-size: ${playIconSize}px ${playIconSize}px; height: ${playIconSize}px; width: ${playIconSize}px; position: absolute; top: 0; left: 0; z-index: 1; top: 50%; left: 50%; transform: translate(-50%, -50%); pointer-events: none;}
         video {border-radius: ${UI_SIZES.radius.small}px;}
         img {max-width: 100vw; max-height: 100vw; height: auto; width: auto; border-radius: ${UI_SIZES.radius.small}px; margin: ${UI_SIZES.spacing.tiny}px auto; display: flex;}
     </style>
