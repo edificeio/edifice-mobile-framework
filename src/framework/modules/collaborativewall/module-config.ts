b1b362e8b60b299f345e35d69fa491b927b2d426
Valentin Mourot <valentin.mourot@edifice.io>
2024-06-25 18:12:09 +0200

    chore: remove totally collaborativewall when prod mode

--- src/framework/modules/collaborativewall/module-config.ts
+++ src/framework/modules/collaborativewall/module-config.ts
@@ -1,3 +1,4 @@
+import appConf from '~/framework/util/appConf';
 import { NavigableModuleConfig } from '~/framework/util/moduleTool';
 
 export default new NavigableModuleConfig<'collaborativewall', null>({
@@ -5,6 +6,7 @@ export default new NavigableModuleConfig<'collaborativewall', null>({
   entcoreScope: ['collaborativewall'],
   matchEntcoreApp: '/collaborativewall',
   storageName: 'collaborativewall',
+  hasRight: ((matchingApps, matchingWidgets) => appConf.isDevOrAlpha && matchingApps.length > 0), // Remove this line when this modules goes in production !
 
   displayI18n: 'collaborativewall-tabname',
 });
