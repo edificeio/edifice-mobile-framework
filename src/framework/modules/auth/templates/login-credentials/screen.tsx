b728c328e0742bd077fdf7a8114f6cf47569d74a
Valentin Mourot <valentin.mourot@edifice.io>
2024-07-09 19:08:05 +0200

    Revert "fix(ent77): MB-2842 hide platform name when there is only one"
    This reverts commit edaf4c9f5eb99d349f05ad164d4af7873a58041c.

--- src/framework/modules/auth/templates/login-credentials/screen.tsx
+++ src/framework/modules/auth/templates/login-credentials/screen.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { ScrollView, View } from 'react-native';
 
-import appconf from '~/app/appconf';
 import { I18n } from '~/app/i18n';
 import theme from '~/app/theme';
 import DefaultButton from '~/framework/components/buttons/default';
@@ -155,7 +154,7 @@ const LoginCredentialsScreen = (props: LoginCredentialsScreenPrivateProps) => {
     return (
       <View style={styles.platform}>
         <Picture type={platform.logoType} source={platform.logo} name={platform.logo} style={logoStyle} resizeMode="contain" />
-        {appconf.platforms.length > 1 ? <HeadingXSText style={styles.platformName}>{platform.displayName}</HeadingXSText> : null}
+        <HeadingXSText style={styles.platformName}>{platform.displayName}</HeadingXSText>
       </View>
     );
   }, [platform]);
