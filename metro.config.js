f9a4fd2175b6b42dd7668af7afbf58f7236bbedc
Guillaume DROULEZ <guillaume@Host-003.lan>
2024-10-16 13:14:58 +0200

    chore(build): review eslint && prettier rules && configuration

--- metro.config.js
+++ metro.config.js
@@ -10,12 +10,12 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-  transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
-  },
   resolver: {
     assetExts: assetExts.filter(ext => ext !== 'svg'),
     sourceExts: [...sourceExts, 'svg', 'cjs'],
+  },
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
   },
 };
 
