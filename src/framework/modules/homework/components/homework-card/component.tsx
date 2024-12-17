7234ec30b20b97eb2f53d2932d96a72a156e2eb0
Francois Touchard <francois.touchard@edifice.io>
2024-12-17 15:17:52 +0100

    refacto folder arborescence + cleaning

--- src/framework/modules/homework/components/homework-card/component.tsx
+++ src/framework/modules/homework/components/homework-card/component.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { StyleSheet, TouchableOpacity, View, ViewStyle } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 
-import { Moment } from 'moment';
+import styles from './styles';
+import { HomeworkCardProps } from './types';
 
 import theme from '~/app/theme';
 import { UI_SIZES } from '~/framework/components/constants';
@@ -13,46 +14,7 @@ import { extractMediaFromHtml } from '~/framework/util/htmlParser/content';
 import { IMedia } from '~/framework/util/notifications';
 import HtmlToText from '~/infra/htmlConverter/text';
 
-export interface IHomeworkCardProps {
-  title: string;
-  content: string;
-  date: Moment;
-  finished: boolean;
-  onPress: () => void;
-  style?: ViewStyle;
-}
-
-const styles = StyleSheet.create({
-  item: {
-    backgroundColor: theme.ui.background.card,
-    borderRadius: UI_SIZES.radius.card,
-    elevation: 7,
-    flexDirection: 'row',
-    marginLeft: UI_SIZES.spacing.big,
-    marginTop: UI_SIZES.spacing.small,
-    padding: UI_SIZES.spacing.medium,
-    shadowColor: theme.ui.shadowColor,
-    shadowOffset: { height: 4, width: 0 },
-    shadowOpacity: 0.15,
-    shadowRadius: 5,
-  },
-  status: {
-    marginLeft: UI_SIZES.spacing.minor,
-  },
-  viewArrow: {
-    justifyContent: 'center',
-    marginLeft: UI_SIZES.spacing.small,
-  },
-  viewTexts: {
-    flex: 1,
-  },
-  viewTitle: {
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-});
-
-const HomeworkCard = ({ content, date, finished, onPress, style, title }: IHomeworkCardProps) => {
+const HomeworkCard = ({ content, date, finished, onPress, style, title }: HomeworkCardProps) => {
   const isPastDate = date.isBefore(today(), 'day');
   const dayOfTheWeek = getDayOfTheWeek(date);
   const dayColor = theme.color.homework.days[dayOfTheWeek]?.accent ?? theme.palette.grey.stone;
@@ -99,7 +61,6 @@ const HomeworkCard = ({ content, date, finished, onPress, style, title }: IHomew
     <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
       <View style={styles.viewTexts}>
         <View style={styles.viewTitle}>
-          {/* {title ? <BodyBoldText numberOfLines={1}>{title}</BodyBoldText> : null} */}
           {renderTitle()}
           {finished === undefined ? null : (
             <NamedSVG
