8a606979cfa296cd42864f5fa2e6b01bc0442f2b
Francois Touchard <francois.touchard@edifice.io>
2024-12-18 15:18:42 +0100

    feat(homework): WB2-2228 adding media preview in title

--- src/framework/modules/homework/components/homework-card/component.tsx
+++ src/framework/modules/homework/components/homework-card/component.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { StyleSheet, TouchableOpacity, View, ViewStyle } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 
-import { Moment } from 'moment';
+import styles from './styles';
+import { HomeworkCardProps } from './types';
 
 import theme from '~/app/theme';
 import { UI_SIZES } from '~/framework/components/constants';
@@ -9,59 +10,58 @@ import { Icon } from '~/framework/components/icon';
 import { NamedSVG } from '~/framework/components/picture';
 import { BodyBoldText, SmallText, TextSizeStyle } from '~/framework/components/text';
 import { getDayOfTheWeek, today } from '~/framework/util/date';
+import { extractMediaFromHtml } from '~/framework/util/htmlParser/content';
+import { IMedia } from '~/framework/util/notifications';
 import HtmlToText from '~/infra/htmlConverter/text';
 
-export interface IHomeworkCardProps {
-  title: string;
-  content: string;
-  date: Moment;
-  finished: boolean;
-  onPress: () => void;
-  style?: ViewStyle;
-}
-
-const styles = StyleSheet.create({
-  item: {
-    backgroundColor: theme.ui.background.card,
-    borderRadius: UI_SIZES.radius.card,
-    elevation: 7,
-    flexDirection: 'row',
-    marginLeft: UI_SIZES.spacing.big,
-    marginTop: UI_SIZES.spacing.small,
-    padding: UI_SIZES.spacing.medium,
-    shadowColor: theme.ui.shadowColor,
-    shadowOffset: { height: 4, width: 0 },
-    shadowOpacity: 0.15,
-    shadowRadius: 5,
-  },
-  status: {
-    marginLeft: UI_SIZES.spacing.minor,
-  },
-  viewArrow: {
-    justifyContent: 'center',
-    marginLeft: UI_SIZES.spacing.small,
-  },
-  viewTexts: {
-    flex: 1,
-  },
-  viewTitle: {
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-});
-
-const HomeworkCard = ({ content, date, finished, onPress, style, title }: IHomeworkCardProps) => {
+const HomeworkCard = ({ content, date, finished, onPress, style, title }: HomeworkCardProps) => {
   const isPastDate = date.isBefore(today(), 'day');
   const dayOfTheWeek = getDayOfTheWeek(date);
   const dayColor = theme.color.homework.days[dayOfTheWeek]?.accent ?? theme.palette.grey.stone;
   const arrowColor = isPastDate ? theme.palette.grey.stone : dayColor;
   const formattedContent = content && HtmlToText(content, false).render;
 
+  const renderTitle = React.useCallback(() => {
+    /**
+     * We want to render title along with icons representing media types featured in the task
+     */
+    const mediaTypes: IMedia[] = content ? extractMediaFromHtml(content) || [] : [];
+    const mediaTypesPerTask = [...new Set(mediaTypes.map(media => media.type))];
+
+    const mediaIcons: { [key: string]: string } = {
+      audio: 'ui-mic-preview',
+      image: 'ui-image-preview',
+      video: 'ui-recordVideo-preview',
+    };
+
+    if (mediaTypesPerTask.length === 0) {
+      return <View style={styles.viewTitle}>{title ? <BodyBoldText numberOfLines={1}>{title}</BodyBoldText> : null}</View>;
+    } else {
+      return (
+        <View style={styles.viewTitle}>
+          {title ? <BodyBoldText numberOfLines={1}>{title}</BodyBoldText> : null}
+          {mediaTypesPerTask.map((type, index) => (
+            <NamedSVG
+              key={index}
+              name={mediaIcons[type]}
+              style={{
+                marginLeft: index === 0 ? UI_SIZES.spacing.tiny : -(UI_SIZES.spacing.tiny + UI_SIZES.spacing._LEGACY_tiny),
+                zIndex: mediaTypesPerTask.length + index,
+              }}
+              width={UI_SIZES.elements.icon.medium}
+              height={UI_SIZES.elements.icon.medium}
+            />
+          ))}
+        </View>
+      );
+    }
+  }, [content, title]);
+
   return (
     <TouchableOpacity onPress={onPress} style={[styles.item, style]}>
       <View style={styles.viewTexts}>
         <View style={styles.viewTitle}>
-          {title ? <BodyBoldText numberOfLines={1}>{title}</BodyBoldText> : null}
+          {renderTitle()}
           {finished === undefined ? null : (
             <NamedSVG
               fill={finished ? theme.palette.status.success.regular : theme.palette.grey.stone}
