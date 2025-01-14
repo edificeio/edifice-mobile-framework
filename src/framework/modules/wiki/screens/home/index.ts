e98245f906506db55dcf848bcb2ed0f85acd864d
Valentin Mourot <valentin.mourot@edifice.io>
2025-01-14 17:34:03 +0100

    chore: add wiki module

---
+++ src/framework/modules/wiki/screens/home/index.ts
@@ -0,0 +1,6 @@
+import WikiHomeScreen from './screen';
+
+export default WikiHomeScreen;
+export { computeNavBar } from './screen';
+export type { WikiHomeScreenNavParams, WikiHomeScreenProps } from './types';
+
