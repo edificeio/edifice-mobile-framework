ce64a1009ffec5ac70bfe396aa2ab259695c22d9
Guillaume Roux <guillaume.roux@cgi.com>
2025-02-04 14:32:20 +0100

    Revert "chore: MB-2998 restore mediacentre version 1.13.4"
    This reverts commit 5d98a5f3f257e0c76ef122390eef343770c3aead.

--- src/framework/modules/mediacentre/module-config.ts
+++ src/framework/modules/mediacentre/module-config.ts
@@ -1,14 +1,14 @@
+import type { MediacentreReduxState } from './reducer';
+
 import { NavigableModuleConfig } from '~/framework/util/moduleTool';
 
-import type { IMediacentreReduxState } from './reducer';
-
-export default new NavigableModuleConfig<'mediacentre', IMediacentreReduxState>({
-  name: 'mediacentre',
-  entcoreScope: ['mediacentre'],
-  matchEntcoreApp: '/mediacentre',
-  storageName: 'mediacentre',
-
-  displayI18n: 'mediacentre-moduleconfig-appname',
+export default new NavigableModuleConfig<'mediacentre', MediacentreReduxState>({
   displayAs: 'myAppsSecondaryModule',
-  displayPicture: { type: 'NamedSvg', name: 'mediacentre' },
+  displayI18n: 'mediacentre-moduleconfig-appname',
+  displayPicture: { name: 'mediacentre', type: 'NamedSvg' },
+  entcoreScope: ['mediacentre'],
+
+  matchEntcoreApp: '/mediacentre',
+  name: 'mediacentre',
+  storageName: 'mediacentre',
 });
