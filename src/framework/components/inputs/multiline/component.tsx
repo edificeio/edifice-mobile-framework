d30fff2cc9e303b9e072904590c18d17f338bd0e
Valentin Mourot <valentin.mourot@edifice.io>
2025-02-17 14:19:02 +0100

    chore: better estimate initial hieght of multiline inputs

--- src/framework/components/inputs/multiline/component.tsx
+++ src/framework/components/inputs/multiline/component.tsx
@@ -1,17 +1,20 @@
 import React, { forwardRef } from 'react';
-import { TextInput as RNTextInput } from 'react-native';
+import { PixelRatio, TextInput as RNTextInput } from 'react-native';
 
+import stylesTextInput from '../text/styles';
 import styles from './styles';
 import { MultilineTextInputProps } from './types';
 
-import TextInput, { styles as stylesTextInput } from '~/framework/components/inputs/text';
-import { TextSizeStyle } from '~/framework/components/text';
+import TextInput from '~/framework/components/inputs/text';
 
-const IOS_TEXTINPUT_LINE_HEIGHT = 1.2786885246;
+const IOS_TEXTINPUT_LINE_HEIGHT = 1.23; // This is a magic value because IOS overrides line-height in TextInputs with a unknown value. This is an estimation of that value.
 
 const MultilineTextInput = forwardRef<RNTextInput, MultilineTextInputProps>((props: MultilineTextInputProps, ref) => {
   const { numberOfLines } = props;
-  const initialHeight = TextSizeStyle.Medium.lineHeight! * numberOfLines + 2 * stylesTextInput.input.paddingTop;
+  const initialHeight =
+    stylesTextInput.input.paddingTop +
+    stylesTextInput.input.paddingBottom +
+    stylesTextInput.input.fontSize * IOS_TEXTINPUT_LINE_HEIGHT * numberOfLines * PixelRatio.getFontScale();
 
   return (
     <TextInput
