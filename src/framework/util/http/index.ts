bce7b5fa345668d3b5533d44a8bf7a9815fa263d
Valentin Mourot <valentin.mourot@edifice.io>
2025-02-27 19:50:04 +0100

    chore: WB2-2292 source image signing

--- src/framework/util/http/index.ts
+++ src/framework/util/http/index.ts
@@ -2,10 +2,11 @@ import { HTTPError } from './error';
 import { fetch, fetchJson } from './fetch';
 import { fetchForAccount, fetchForSession, fetchJsonForAccount, fetchJsonForSession } from './fetch-for-account';
 import { fetchForPlatform, fetchJsonForPlatform } from './fetch-for-platform';
+import { imagePropsForAccount, imagePropsForSession } from './source';
 
 /**
  * Utility functions for making HTTP requests.
- * 
+ *
  * @module http
  * @exports fetch - Function to perform a generic fetch request.
  * @exports fetchJson - Function to perform a fetch request and parse the response as JSON.
@@ -15,16 +16,20 @@ import { fetchForPlatform, fetchJsonForPlatform } from './fetch-for-platform';
  * @exports fetchJsonForSession - Function to perform a fetch request for the active account and parse the response as JSON.
  * @exports fetchForPlatform - Function to perform a fetch request for a specific platform.
  * @exports fetchJsonForPlatform - Function to perform a fetch request for a specific platform and parse the response as JSON.
+ * @exports imagePropsForAccount - Function to call with image prop to format source for the given account
+ * @exports imagePropsForSession - Function to call with image prop to format source for the active account
  */
 export default {
   fetch,
-  fetchJson,
   fetchForAccount,
-  fetchJsonForAccount,
-  fetchForSession,
-  fetchJsonForSession,
   fetchForPlatform,
+  fetchForSession,
+  fetchJson,
+  fetchJsonForAccount,
   fetchJsonForPlatform,
-}
+  fetchJsonForSession,
+  imagePropsForAccount,
+  imagePropsForSession,
+};
 
 export { HTTPError };
