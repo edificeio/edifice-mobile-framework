b54ddd5c38fb91b5517b83ccfbb2b723b6a9421c
Francois Touchard <francois.touchard@edifice.io>
2025-02-27 17:08:28 +0100

    to reset after eslint fix

--- src/framework/modules/wiki/screens/home/screen.tsx
+++ src/framework/modules/wiki/screens/home/screen.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
+import { KeyboardAvoidingView, Platform, TouchableOpacity, View } from 'react-native';
 
 import type { NativeStackNavigationOptions, NativeStackScreenProps } from '@react-navigation/native-stack';
 
+import ImageInput from '../../components/image-input';
+import moduleConfig from '../../module-config';
+import styles from './styles';
 import type { WikiHomeScreenPrivateProps } from './types';
 
 import { I18n } from '~/app/i18n';
+import theme from '~/app/theme';
+import DefaultButton from '~/framework/components/buttons/default';
+import { UI_SIZES } from '~/framework/components/constants';
+import BottomSheetModal from '~/framework/components/modals/bottom-sheet';
 import { PageView } from '~/framework/components/page';
-import { BodyBoldText } from '~/framework/components/text';
+import { Svg } from '~/framework/components/picture';
+import { BodyBoldText, BodyText } from '~/framework/components/text';
 import { WikiNavigationParams, wikiRouteNames } from '~/framework/modules/wiki/navigation';
 import { navBarOptions } from '~/framework/navigation/navBar';
 
@@ -21,10 +30,52 @@ export const computeNavBar = ({
   }),
 });
 
+const choosePicsMenu = () => {
+  return (
+    // <BottomSheetModal ref={choosePicsMenuRef} onDismiss={handleChoosePicsMenuDismissed}>
+    <BottomSheetModal>
+      <DefaultButton
+        iconLeft="ui-image"
+        text={I18n.get('pickfile-image')}
+        contentColor={theme.palette.complementary.green.regular}
+        disabled
+        style={styles.choosePicsMenuTitle}
+      />
+      {/* <TouchableOpacity style={styles.choosePicsMenuElement} onPress={handleTakePic}> */}
+      <TouchableOpacity style={styles.choosePicsMenuElement}>
+        <Svg
+          height={UI_SIZES.elements.icon.default}
+          width={UI_SIZES.elements.icon.default}
+          name="ui-camera"
+          fill={theme.palette.grey.black}
+        />
+        <BodyText>{I18n.get('pickfile-take')}</BodyText>
+      </TouchableOpacity>
+      <View style={styles.choosePicsMenuSeparator} />
+      {/* <TouchableOpacity style={styles.choosePicsMenuElement} onPress={handleChoosePics}> */}
+      <TouchableOpacity style={styles.choosePicsMenuElement}>
+        <Svg
+          height={UI_SIZES.elements.icon.default}
+          width={UI_SIZES.elements.icon.default}
+          name="ui-smartphone"
+          fill={theme.palette.grey.black}
+        />
+        <BodyText>{I18n.get('pickfile-pick')}</BodyText>
+      </TouchableOpacity>
+    </BottomSheetModal>
+  );
+};
+
 export default function WikiHomeScreen(props: WikiHomeScreenPrivateProps) {
   return (
     <PageView>
-      <BodyBoldText>wiki home screen</BodyBoldText>
+      <KeyboardAvoidingView
+        // keyboardVerticalOffset={headerHeight}
+        style={styles.container}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <BodyBoldText>wiki home screen</BodyBoldText>
+        <ImageInput moduleConfig={moduleConfig} />
+      </KeyboardAvoidingView>
     </PageView>
   );
 }
--- src/framework/modules/wiki/screens/home/styles.ts
+++ src/framework/modules/wiki/screens/home/styles.ts
@@ -1,5 +1,63 @@
 import { StyleSheet } from 'react-native';
 
+import theme from '~/app/theme';
+import { getScaleWidth, UI_SIZES } from '~/framework/components/constants';
+
 export default StyleSheet.create({
-  // [SCAFFOLDER] create your styles here
+  addButton: {
+    marginTop: UI_SIZES.spacing.big,
+  },
+  addFilesResults: {
+    padding: UI_SIZES.spacing.medium,
+    paddingBottom: UI_SIZES.screen.bottomInset,
+  },
+  addFilesResultsFile: { flex: 1 },
+  addFilesResultsItem: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingVertical: UI_SIZES.spacing.minor,
+  },
+  addFilesResultsTitle: {
+    marginBottom: UI_SIZES.spacing.minor,
+    textAlign: 'center',
+  },
+  addFilesResultsType: {
+    alignItems: 'center',
+    aspectRatio: 1,
+    borderRadius: UI_SIZES.radius.card,
+    justifyContent: 'center',
+    marginRight: UI_SIZES.spacing.minor,
+    width: getScaleWidth(36),
+  },
+  choosePicsMenuElement: {
+    columnGap: UI_SIZES.spacing.small,
+    flexDirection: 'row',
+    paddingVertical: UI_SIZES.spacing.minor,
+  },
+  choosePicsMenuSeparator: {
+    borderBottomColor: theme.palette.grey.cloudy,
+    borderBottomWidth: 1,
+    marginHorizontal: UI_SIZES.spacing.minor,
+    marginVertical: UI_SIZES.spacing.small,
+  },
+  choosePicsMenuTitle: {
+    alignSelf: 'flex-start',
+    marginBottom: UI_SIZES.spacing.big,
+  },
+  container: {
+    flex: 1,
+    marginBottom: UI_SIZES.screen.bottomInset,
+  },
+  page: {
+    backgroundColor: theme.palette.grey.white,
+  },
+  richEditor: {
+    minHeight: '90%',
+  },
+  scrollView: {
+    flexGrow: 1,
+    padding: UI_SIZES.spacing.medium,
+    paddingBottom: 0,
+  },
+  toolbar: { marginTop: -UI_SIZES.elements.editor.toolbarHeight },
 });
