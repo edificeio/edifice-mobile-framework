8de1c54db7a18e601d311958d90fd96edcb83a6b
Guillaume Roux <guillaume.roux@cgi.com>
2025-03-11 11:09:36 +0100

    feat(zimbra): MO-440 disable rich signature edition

--- src/framework/modules/zimbra/components/modals/SignatureModal.tsx
+++ src/framework/modules/zimbra/components/modals/SignatureModal.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 
 import { I18n } from '~/app/i18n';
 import theme from '~/app/theme';
+import AlertCard from '~/framework/components/alert';
 import PrimaryButton from '~/framework/components/buttons/primary';
 import { Checkbox } from '~/framework/components/checkbox';
 import { UI_SIZES } from '~/framework/components/constants';
@@ -12,8 +13,16 @@ import Toast from '~/framework/components/toast';
 import { AuthLoggedAccount } from '~/framework/modules/auth/model';
 import { ISignature } from '~/framework/modules/zimbra/model';
 import { zimbraService } from '~/framework/modules/zimbra/service';
+import { isSignatureRich } from '~/framework/modules/zimbra/utils/drafts';
+import HtmlContentView from '~/ui/HtmlContentView';
 
 const styles = StyleSheet.create({
+  alertContainer: {
+    marginBottom: UI_SIZES.spacing.medium,
+  },
+  disabledInput: {
+    backgroundColor: theme.palette.grey.pearl,
+  },
   isGlobalContainer: {
     alignItems: 'center',
     flexDirection: 'row',
@@ -42,14 +51,16 @@ interface ISignatureModalProps {
 }
 
 const SignatureModal = React.forwardRef<ModalBoxHandle, ISignatureModalProps>((props, ref) => {
-  const [text, setText] = React.useState<string>(props.signature.preference.signature);
-  const [isGlobal, setGlobal] = React.useState<boolean>(props.signature.preference.useSignature);
+  const [content, setContent] = React.useState<string>(props.signature.content);
+  const [isGlobal, setGlobal] = React.useState<boolean>(props.signature.useSignature);
+  const [isRichContent, setRichContent] = React.useState<boolean>(isSignatureRich(props.signature.content));
   const [isUpdating, setUpdating] = React.useState<boolean>(false);
 
   React.useEffect(() => {
-    setText(props.signature.preference.signature);
-    setGlobal(props.signature.preference.useSignature);
-  }, [props.signature.preference]);
+    setContent(props.signature.content);
+    setGlobal(props.signature.useSignature);
+    setRichContent(isSignatureRich(props.signature.content));
+  }, [props.signature]);
 
   const updateSignature = async () => {
     try {
@@ -57,8 +68,8 @@ const SignatureModal = React.forwardRef<ModalBoxHandle, ISignatureModalProps>((p
 
       setUpdating(true);
       if (!session) throw new Error();
-      await zimbraService.signature.update(session, text, isGlobal);
-      props.onChange(text);
+      await zimbraService.signature.update(session, content, isGlobal);
+      props.onChange(content);
       setUpdating(false);
     } catch {
       setUpdating(false);
@@ -72,13 +83,20 @@ const SignatureModal = React.forwardRef<ModalBoxHandle, ISignatureModalProps>((p
       content={
         <View>
           <BodyText>{I18n.get('zimbra-composer-signaturemodal-title')}</BodyText>
-          <TextInput
-            value={text}
-            onChangeText={value => setText(value)}
-            multiline
-            textAlignVertical="top"
-            style={styles.textInput}
-          />
+          {isRichContent ? (
+            <React.Fragment>
+              <HtmlContentView html={content} style={[styles.textInput, styles.disabledInput]} />
+              <AlertCard type="info" text={I18n.get('zimbra-composer-signaturemodal-richinfo')} style={styles.alertContainer} />
+            </React.Fragment>
+          ) : (
+            <TextInput
+              value={content}
+              onChangeText={value => setContent(value)}
+              multiline
+              textAlignVertical="top"
+              style={styles.textInput}
+            />
+          )}
           <TouchableOpacity onPress={() => setGlobal(!isGlobal)} style={styles.isGlobalContainer}>
             <Checkbox checked={isGlobal} onPress={() => setGlobal(!isGlobal)} />
             <SmallText style={styles.isGlobalText}>{I18n.get('zimbra-composer-signaturemodal-globaluse')}</SmallText>
