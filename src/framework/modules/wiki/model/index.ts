6d64751b8368e26e1d69f31bcb338077d2797882
Valentin Mourot <valentin.mourot@edifice.io>
2025-03-26 15:50:19 +0100

    chore: wiki service + print values

--- src/framework/modules/wiki/model/index.ts
+++ src/framework/modules/wiki/model/index.ts
@@ -2,6 +2,24 @@
  * Data model for the module wiki
  */
 
-// [SCAFFOLDER] In this file, export your data model and functions associated.
+import { Resource } from '~/framework/modules/explorer/model/types';
 
-export interface WikiData { }
+export type WikiResourceMetadata = Pick<
+  Resource,
+  'assetId' | 'createdAt' | 'creatorId' | 'creatorName' | 'name' | 'updatedAt' | 'thumbnail'
+>;
+
+export interface Wiki extends WikiResourceMetadata {
+  description?: string;
+  pages: WikiPageMetaData[];
+}
+
+export interface WikiPageMetaData {
+  id: string;
+  isVisible: boolean;
+  title: string;
+  position: number;
+  depth: number;
+  parentId?: WikiPageMetaData['id'];
+  childrenIds: WikiPageMetaData['id'][];
+}
--- src/framework/modules/wiki/store.ts
+++ src/framework/modules/wiki/store.ts
@@ -1,26 +1,52 @@
 import { combineReducers } from 'redux';
+import { ThunkAction } from 'redux-thunk';
 
+import { Wiki } from './model';
 import moduleConfig from './module-config';
 
+import { IGlobalState } from '~/app/store';
 import {
   createExplorerActions,
   createExplorerReducer,
   createExplorerSelectors,
   ExplorerState,
 } from '~/framework/modules/explorer/store';
+import { createSessionReducer } from '~/framework/util/redux/reducerFactory';
+
+export type WikiAction = { type: `${string}_LOAD_WIKI`; data: Wiki };
+
+const wikiActionTypes = {
+  LOAD_WIKI: moduleConfig.namespaceActionType('LOAD_WIKI') as `${string}_LOAD_WIKI`,
+};
 
 export interface WikiStore {
   explorer: ExplorerState;
+  wikis: Record<Wiki['assetId'], Wiki>;
+  pages: object;
 }
+
+export const wikisReducer = createSessionReducer<WikiStore['wikis'], WikiAction>(
+  {},
+  {
+    [wikiActionTypes.LOAD_WIKI]: (state, action) => ({ ...state, [action.data.assetId]: action.data }),
+  },
+);
 
 export const reducer = combineReducers({
   explorer: createExplorerReducer(moduleConfig),
+  wikis: wikisReducer,
 });
 
 export const actions = {
   explorer: createExplorerActions(moduleConfig),
+  loadWiki:
+    (data: Wiki): ThunkAction<void, IGlobalState, object, WikiAction> =>
+    dispatch => {
+      dispatch({ data, type: wikiActionTypes.LOAD_WIKI });
+    },
 };
 
 export const selectors = {
   explorer: createExplorerSelectors(moduleConfig, state => moduleConfig.getState(state).explorer),
+  wiki: (id: keyof WikiStore['wikis']) => (state: IGlobalState) => moduleConfig.getState(state).wikis[id],
 };
