9af9b3a92b98c3e1286aecaeb0758adad2b050e4
thegdz <guillaume.droulez@mac.com>
2025-03-28 16:36:50 +0100

    feat(nabook): ENABLING-234, nabook module skeleton addition

--- src/framework/modules/nabook/module-config.ts
+++ src/framework/modules/nabook/module-config.ts
@@ -3,7 +3,7 @@ import { NavigableModuleConfig } from '~/framework/util/moduleTool';
 
 export default new NavigableModuleConfig<'nabook', null>({
   displayAs: 'myAppsModule',
-  displayI18n: 'nabook.tabName',
+  displayI18n: 'nabook-tabname',
   displayPicture: { fill: theme.palette.complementary.orange.regular, name: 'ui-tool', type: 'NamedSvg' },
   entcoreScope: ['nabook'],
   matchEntcoreApp: '/nabook',
