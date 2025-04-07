dd2c0f484e834474dbf24ad9963dec46e96e8298
Valentin Mourot <valentin.mourot@edifice.io>
2025-04-07 17:11:46 +0200

    feat: wiki reader basic

--- src/framework/modules/wiki/screens/reader/screen.tsx
+++ src/framework/modules/wiki/screens/reader/screen.tsx
@@ -1,16 +1,21 @@
 import * as React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, View } from 'react-native';
 
 import type { NativeStackNavigationOptions, NativeStackScreenProps } from '@react-navigation/native-stack';
 import { useDispatch, useSelector } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 
+import { WikiPage } from '../../model';
+import styles from './styles';
 import type { WikiReaderScreen } from './types';
 
 import { I18n } from '~/app/i18n';
 import { IGlobalState } from '~/app/store';
+import { SingleAvatar } from '~/framework/components/avatar';
+import TertiaryButton from '~/framework/components/buttons/tertiary';
+import { RichEditorViewer } from '~/framework/components/inputs/rich-text/viewer';
 import { PageView } from '~/framework/components/page';
-import { BodyBoldText, BodyText } from '~/framework/components/text';
+import { BodyBoldText, HeadingMText, SmallText } from '~/framework/components/text';
 import { ContentLoader, ContentLoaderProps } from '~/framework/hooks/loader';
 import { WikiNavigationParams, wikiRouteNames } from '~/framework/modules/wiki/navigation';
 import service from '~/framework/modules/wiki/service';
@@ -38,35 +43,106 @@ export default function WikiReaderScreen({
   const pageData = useSelector(selectors.page(pageId)) ?? {};
   const dispatch = useDispatch<ThunkDispatch<IGlobalState, any, WikiAction | WikiPageAction>>();
 
+  const pageIndex = React.useMemo(() => wikiData.pages.findIndex(p => (p.id = pageId)), [pageId, wikiData.pages]);
+  const prevPageId = React.useMemo(
+    () => (pageIndex > 0 ? wikiData.pages.at(pageIndex - 1)?.id : undefined),
+    [pageIndex, wikiData.pages],
+  );
+  const nextPageId = React.useMemo(
+    () => (pageIndex !== -1 ? wikiData.pages.at(pageIndex + 1)?.id : undefined),
+    [pageIndex, wikiData.pages],
+  );
+  console.debug('INDEX', wikiData.pages, pageId, pageIndex, prevPageId, nextPageId);
+
+  const switchToPage = React.useCallback(
+    (id: WikiPage['id']) => {
+      navigation.replace(wikiRouteNames.reader, { pageId: id, resourceId });
+    },
+    [navigation, resourceId],
+  );
+
+  const [webViewReady, setWebViewReady] = React.useState(false);
+
   const loadContent: ContentLoaderProps['loadContent'] = React.useCallback(async () => {
     const newWikiData = await service.wiki.get({ id: resourceId });
     const newPageData = await service.page.get({ id: resourceId, pageId: pageId });
+    console.debug('LOAD PAGE', pageId, newPageData.id, newPageData.title);
     dispatch(actions.loadWiki(newWikiData));
     dispatch(actions.loadPage(resourceId, newPageData));
   }, [dispatch, resourceId, pageId]);
 
   const renderLoading: ContentLoaderProps['renderLoading'] = React.useCallback(
-    () => <BodyBoldText>LOADING {resourceId}</BodyBoldText>,
+    () => (
+      <View style={styles.loader}>
+        <BodyBoldText>LOADING {resourceId}</BodyBoldText>
+      </View>
+    ),
     [resourceId],
   );
 
   const renderContent: ContentLoaderProps['renderContent'] = React.useCallback(
     refreshControl => {
       return (
-        <ScrollView refreshControl={refreshControl}>
-          <BodyBoldText>
-            Page {pageId} OF {resourceId} ({wikiData.name})
-          </BodyBoldText>
-          <BodyBoldText>{pageData.title}</BodyBoldText>
-          <BodyText>{pageData.content}</BodyText>
-        </ScrollView>
+        <>
+          <ScrollView refreshControl={refreshControl}>
+            <View style={styles.topNavigation} />
+            <View style={styles.header}>
+              {!pageData.isVisible && <BodyBoldText>{I18n.get('wiki-page-hidden')}</BodyBoldText>}
+              <HeadingMText>{pageData.title}</HeadingMText>
+              <View style={styles.headerAuthorInfo}>
+                <SingleAvatar userId={pageData.creatorId} size="md" />
+                <View style={styles.headerAuthorInfoText}>
+                  <BodyBoldText numberOfLines={1}>{pageData.creatorName}</BodyBoldText>
+                  <SmallText numberOfLines={1}>
+                    {I18n.get('wiki-page-published-at', { date: I18n.date(pageData.updatedAt ?? pageData.createdAt) })}
+                  </SmallText>
+                </View>
+              </View>
+            </View>
+            <View style={styles.content}>
+              <RichEditorViewer content={pageData.content} onLoad={() => setWebViewReady(true)} />
+            </View>
+            <View style={styles.bottomNavigation}>
+              <TertiaryButton
+                iconLeft="ui-arrowLeft"
+                disabled={!prevPageId}
+                text={I18n.get('wiki-page-previous')}
+                action={() => {
+                  switchToPage(prevPageId!);
+                }}
+              />
+              <TertiaryButton
+                iconRight="ui-arrowRight"
+                disabled={!nextPageId}
+                text={I18n.get('wiki-page-next')}
+                action={() => {
+                  switchToPage(nextPageId!);
+                }}
+              />
+            </View>
+          </ScrollView>
+          {!webViewReady && <View style={styles.webViewPlaceholder}>{renderLoading()}</View>}
+        </>
       );
     },
-    [pageData.content, pageData.title, pageId, resourceId, wikiData.name],
+    [
+      nextPageId,
+      pageData.content,
+      pageData.createdAt,
+      pageData.creatorId,
+      pageData.creatorName,
+      pageData.isVisible,
+      pageData.title,
+      pageData.updatedAt,
+      prevPageId,
+      renderLoading,
+      switchToPage,
+      webViewReady,
+    ],
   );
 
   return (
-    <PageView>
+    <PageView style={styles.page}>
       <ContentLoader loadContent={loadContent} renderContent={renderContent} renderLoading={renderLoading} />
     </PageView>
   );
--- src/framework/modules/wiki/screens/reader/styles.ts
+++ src/framework/modules/wiki/screens/reader/styles.ts
@@ -1,11 +1,50 @@
 import { StyleSheet } from 'react-native';
 
 import theme from '~/app/theme';
-import { UI_SIZES, UI_STYLES } from '~/framework/components/constants';
+import { UI_SIZES } from '~/framework/components/constants';
 
 export default StyleSheet.create({
-  // @scaffolder add styles here.
-  // Please use UI_STYLES for basic ones (like flex:1, flex:0, etc...) instead redefine them here.
-  // Never use inline colors, pick them from theme.
-  // Never use inline dimensions/spacers, pick them from UI_SIZES.
+  bottomNavigation: {
+    paddingBottom: UI_SIZES.spacing.medium,
+    paddingHorizontal: UI_SIZES.spacing.big,
+  },
+  content: {
+    margin: UI_SIZES.spacing.big,
+  },
+  header: {
+    alignItems: 'stretch',
+    backgroundColor: theme.palette.primary.pale,
+    borderBottomColor: theme.palette.primary.light,
+    borderBottomWidth: UI_SIZES.border.thin,
+    borderTopColor: theme.palette.primary.light,
+    borderTopWidth: UI_SIZES.border.thin,
+    gap: UI_SIZES.spacing.minor,
+    paddingHorizontal: UI_SIZES.spacing.big,
+    paddingVertical: UI_SIZES.spacing.medium,
+  },
+  headerAuthorInfo: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: UI_SIZES.spacing.small,
+  },
+  headerAuthorInfoText: {
+    flex: 1,
+  },
+  loader: {
+    backgroundColor: theme.ui.background.card,
+    flex: 1,
+  },
+  page: {
+    backgroundColor: theme.ui.background.card,
+  },
+  topNavigation: {
+    alignItems: 'stretch',
+    paddingHorizontal: UI_SIZES.spacing.big,
+    paddingVertical: UI_SIZES.spacing.minor,
+  },
+  webViewPlaceholder: {
+    height: '100%',
+    position: 'absolute',
+    width: '100%',
+  },
 });
