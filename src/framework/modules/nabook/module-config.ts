457e6bf318522f7a4896b2ca7c99bc4540b5c56a
lulu-dev11 <miallet.lucas@gmail.com>
2025-04-11 15:53:36 +0200

    feat(nabook): ENABLING-237, modif icone module

--- src/framework/modules/nabook/module-config.ts
+++ src/framework/modules/nabook/module-config.ts
@@ -1,10 +1,9 @@
-import theme from '~/app/theme';
 import { NavigableModuleConfig } from '~/framework/util/moduleTool';
 
 export default new NavigableModuleConfig<'nabook', null>({
   displayAs: 'myAppsModule',
   displayI18n: 'nabook-tabname',
-  displayPicture: { fill: theme.palette.complementary.orange.regular, name: 'ui-tool', type: 'NamedSvg' },
+  displayPicture: { name: 'nabook', type: 'NamedSvg' },
   entcoreScope: ['nabook'],
   matchEntcoreApp: '/nabook',
   name: 'nabook',
