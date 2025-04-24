841cc95d7dd83ae47536e7b88263500dd94566ba
Francois Touchard <francois.touchard@edifice.io>
2025-04-24 16:56:53 +0200

    feat(wiki): WB2-2202, WB2-2203, WB2-2309 adding notifications handler

--- src/framework/modules/wiki/notif-handler.ts
+++ src/framework/modules/wiki/notif-handler.ts
@@ -1,34 +1,96 @@
-/**
- * Wiki notif handler
- * @scaffolder Remove this file if your module handles no notification.
- *
- * The notifHandler registers some behaviours for given notif types and event-types.
- * It applicates to both timelineNotififation and pushNotifications.
- */
+import { CommonActions } from '@react-navigation/native';
 
-import { NotifHandlerThunkAction, registerNotifHandlers } from '~/framework/util/notifications/routing';
+import timelineModuleConfig from '~/framework/modules/timeline/module-config';
+import { wikiRouteNames } from '~/framework/modules/wiki/navigation';
+import { computeTabRouteName } from '~/framework/navigation/tabModules';
+import { getAsResourceUriNotification } from '~/framework/util/notifications';
+import {
+  handleNotificationNavigationAction,
+  NotifHandlerThunkAction,
+  registerNotifHandlers,
+} from '~/framework/util/notifications/routing';
 
-const handleSomeNotificationAction: NotifHandlerThunkAction = notification => async (dispatch, getState) => {
-  // @scaffolder extract info from notification here
+const WIKI_ID_REGEX = /\/wiki\/id\/([a-f0-9\-]+)/i;
+const PAGE_ID_REGEX = /\/page\/([a-f0-9\-]+)/i;
 
-  // @scaffolder navigate somewhere here
+const handleWikiNotificationAction: NotifHandlerThunkAction = notification => async (dispatch, getState) => {
+  try {
+    const wikiNotif = getAsResourceUriNotification(notification);
+    if (!wikiNotif) return { managed: 0 };
 
-  // @scaffolder put `return {managed: 0}` if notification is skipped
-  return { managed: 0 };
+    const wikiUri = wikiNotif?.resource.uri;
+    const wikiId = wikiUri?.match(WIKI_ID_REGEX)?.[1];
+    if (!wikiId) return { managed: 0 };
 
-  // return {
-  //   managed: 1,
-  //   trackInfo: { action: 'Wiki', name: `${notification.type}.${notification['event-type']}` },
-  // };
+    const navAction = CommonActions.navigate({
+      name: computeTabRouteName(timelineModuleConfig.routeName),
+      params: {
+        initial: false,
+        params: {
+          notification: wikiNotif,
+          resourceId: wikiId,
+        },
+        screen: wikiRouteNames.summary,
+      },
+    });
+
+    handleNotificationNavigationAction(navAction);
+
+    return {
+      managed: 1,
+      trackInfo: { action: 'Wiki', name: `${notification.type}.${notification['event-type']}` },
+    };
+  } catch {
+    return { managed: 0 };
+  }
+};
+
+const handlePageNotificationAction: NotifHandlerThunkAction = notification => async (dispatch, getState) => {
+  try {
+    const wikiNotif = getAsResourceUriNotification(notification);
+    if (!wikiNotif) return { managed: 0 };
+
+    const wikiUri = wikiNotif?.resource.uri;
+    const wikiId = wikiUri?.match(WIKI_ID_REGEX)?.[1];
+    if (!wikiId) return { managed: 0 };
+
+    const pageId = wikiUri?.match(PAGE_ID_REGEX)?.[1];
+    if (!pageId) return { managed: 0 };
+
+    const navAction = CommonActions.navigate({
+      name: computeTabRouteName(timelineModuleConfig.routeName),
+      params: {
+        initial: false,
+        params: {
+          notification: wikiNotif,
+          pageId: pageId,
+          resourceId: wikiId,
+        },
+        screen: wikiRouteNames.reader,
+      },
+    });
+
+    handleNotificationNavigationAction(navAction);
+
+    return {
+      managed: 1,
+      trackInfo: { action: 'Wiki', name: `${notification.type}.${notification['event-type']}` },
+    };
+  } catch {
+    return { managed: 0 };
+  }
 };
 
 export default () =>
   registerNotifHandlers([
     {
-      // Replace this with the backend notification type
-      'event-type': 'SOME-EVENT-TYPE',
-      // Replace this with the backend notification event-type
-      'notifHandlerAction': handleSomeNotificationAction,
-      'type': 'SOME-TYPE',
+      'event-type': ['SHARED', 'PAGE-DELETED'],
+      'notifHandlerAction': handleWikiNotificationAction,
+      'type': 'WIKI',
+    },
+    {
+      'event-type': ['COMMENT-ADDED', 'PAGE-UPDATED', 'PAGE-CREATED', 'PAGE-VISIBLE'],
+      'notifHandlerAction': handlePageNotificationAction,
+      'type': 'WIKI',
     },
   ]);
