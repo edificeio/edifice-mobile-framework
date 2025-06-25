852a793121f25aaf2f09b1bfdb173abcb1686845
Valentin Mourot <valentin.mourot@edifice.io>
2025-06-25 16:48:21 +0200

    chore: import communities client

--- src/framework/modules/communities/screens/home/screen.tsx
+++ src/framework/modules/communities/screens/home/screen.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { CommunityClient } from '@edifice.io/community-client-rest';
 import type { NativeStackNavigationOptions, NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import type { CommunitiesHomeScreenPrivateProps } from './types';
@@ -7,6 +8,7 @@ import type { CommunitiesHomeScreenPrivateProps } from './types';
 import { I18n } from '~/app/i18n';
 import { PageView } from '~/framework/components/page';
 import { BodyBoldText } from '~/framework/components/text';
+import { getSession } from '~/framework/modules/auth/reducer';
 import { CommunitiesNavigationParams, communitiesRouteNames } from '~/framework/modules/communities/navigation';
 import { navBarOptions } from '~/framework/navigation/navBar';
 
@@ -22,6 +24,21 @@ export const computeNavBar = ({
 });
 
 export default function CommunitiesHomeScreen(props: CommunitiesHomeScreenPrivateProps) {
+  React.useEffect(() => {
+    (async () => {
+      const session = getSession();
+      if (!session) return;
+      const client = new CommunityClient({
+        baseUrl: session.platform.url,
+        defaultHeaders: {
+          Authorization: `Bearer ${session.tokens.access.value}`,
+        },
+      });
+      const communities = await client.getCommunities();
+      console.info('COMMUNITIES', communities);
+    })();
+  }, []);
+
   return (
     <PageView>
       <BodyBoldText>communities home screen</BodyBoldText>
