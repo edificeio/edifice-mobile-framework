8b1968201dedf0675bb51320bade3fcf15fc3015
Francois Touchard <francois.touchard@edifice.io>
2025-06-30 17:28:45 +0200

    adding navParams type for home screen

--- src/framework/modules/communities/screens/home/types.ts
+++ src/framework/modules/communities/screens/home/types.ts
@@ -3,7 +3,9 @@ import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import type { CommunitiesNavigationParams } from '~/framework/modules/communities/navigation';
 
 export namespace CommunitiesHomeScreen {
-  export interface NavParams { }
+  export interface NavParams {
+    communityId: string;
+  }
   export type NavigationProps = NativeStackScreenProps<CommunitiesNavigationParams, 'home'>;
   export type AllProps = CommunitiesHomeScreen.NavigationProps;
 }
