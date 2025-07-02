78b929d531bbf69140f79b8c3bce355d72713fb4
Francois Touchard <francois.touchard@edifice.io>
2025-07-02 09:37:00 +0200

    feat(communities): PEDAGO-2854 adding CommunityCardSmall
    # Conflicts:
    #	src/framework/modules/communities/screens/list/screen.tsx

--- src/framework/modules/communities/screens/list/screen.tsx
+++ src/framework/modules/communities/screens/list/screen.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
 
-import { CommunityClient, CommunityResponseDto } from '@edifice.io/community-client-rest-rn';
+import { InvitationClient, InvitationResponseDto } from '@edifice.io/community-client-rest-rn';
 import { useFocusEffect } from '@react-navigation/native';
 import type { NativeStackNavigationOptions, NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import type { CommunitiesListScreen } from './types';
-import moduleConfig from '../../module-config';
 
 import { I18n } from '~/app/i18n';
-import PrimaryButton from '~/framework/components/buttons/primary';
 import { PageView } from '~/framework/components/page';
-import { BodyBoldText } from '~/framework/components/text';
+import CommunityCardSmall from '~/framework/modules/communities/components/community-card-small';
+import moduleConfig from '~/framework/modules/communities/module-config';
 import { CommunitiesNavigationParams, communitiesRouteNames } from '~/framework/modules/communities/navigation';
 import { navBarOptions } from '~/framework/navigation/navBar';
 import http from '~/framework/util/http';
@@ -27,30 +26,41 @@ export const computeNavBar = ({
 });
 
 export default function CommunitiesListScreen({ navigation }: CommunitiesListScreen.AllProps) {
-  const [communities, setCommunities] = React.useState<CommunityResponseDto[]>([]);
+  const [communities, setCommunities] = React.useState<InvitationResponseDto[]>([]);
+
   const fetchData = React.useCallback(() => {
     (async () => {
-      const commus = await http.sessionApi(moduleConfig, CommunityClient).getCommunities();
-      console.info('COMMUNITIES', commus);
-      setCommunities(commus.items);
+      const data = await http.sessionApi(moduleConfig, InvitationClient).getUserInvitations({ fields: ['stats', 'community'] });
+      setCommunities(data.items);
     })();
   }, []);
 
   useFocusEffect(fetchData);
 
+  const navigateToCommunityHomePage = React.useCallback(
+    (communityId: string) => {
+      navigation.navigate(communitiesRouteNames.home, { communityId });
+    },
+    [navigation],
+  );
+
   return (
-    <PageView>
-      <BodyBoldText>communities list screen</BodyBoldText>
+    // virer align items quand il y aura la liste
+    <PageView gutters="both" style={{ alignItems: 'center' }}>
       {communities &&
-        communities.map(c => (
-          <PrimaryButton
-            key={c.id}
-            text={c.title}
-            action={() => navigation.navigate(communitiesRouteNames.home, { communityId: c.id })}
-            style={{ marginVertical: 2 }}
-          />
-        ))}
-      {/* <PrimaryButton text="HomeScreen" action={() => navigation.navigate(communitiesRouteNames.home, {})} /> */}
+        communities.map((c: InvitationResponseDto) =>
+          c.community ? (
+            <CommunityCardSmall
+              key={c.id}
+              title={c.community.title}
+              image={c.community.image}
+              invitationStatus={c.status}
+              membersCount={c.communityStats?.totalMembers}
+              moduleConfig={moduleConfig}
+              onPress={() => navigateToCommunityHomePage(String(c.id))}
+            />
+          ) : undefined,
+        )}
     </PageView>
   );
 }
