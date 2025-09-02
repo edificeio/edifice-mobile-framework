1dd0e978fc57fcc40e371bd9d3fd3b195877ad34
thegdz <guillaume.droulez@mac.com>
2025-09-02 15:26:34 +0200

    feat(votil): VOT-21, add votil to app scope

--- src/framework/modules/auth/module-config.ts
+++ src/framework/modules/auth/module-config.ts
@@ -3,7 +3,7 @@ import type { IAuthState } from './reducer';
 import { ModuleConfig } from '~/framework/util/moduleTool';
 
 export default new ModuleConfig<'auth', IAuthState>({
-  entcoreScope: ['auth', 'userinfo'],
+  entcoreScope: ['auth', 'userinfo', 'votil'],
   matchEntcoreApp: () => true,
   name: 'auth', // Auth always included
   storageName: 'auth',
