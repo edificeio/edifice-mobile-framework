702bbfad1a4a46156f4f0758bb13e010f88b9fbd
Francois Touchard <francois.touchard@edifice.io>
2025-09-09 17:46:00 +0200

    feat(communities): PEDAGO-2918 adding notif handler

--- src/framework/modules/communities/notif-handler.ts
+++ src/framework/modules/communities/notif-handler.ts
@@ -1,7 +1,78 @@
-/**
- * Communities notif handler
- */
+import { CommonActions } from '@react-navigation/native';
 
-import { registerNotifHandlers } from '~/framework/util/notifications/routing';
+import { communitiesRouteNames } from './navigation';
 
-export default () => registerNotifHandlers([]);
+import timelineModuleConfig from '~/framework/modules/timeline/module-config';
+import { computeTabRouteName } from '~/framework/navigation/tabModules';
+import { getAsResourceUriNotification } from '~/framework/util/notifications';
+import {
+  handleNotificationNavigationAction,
+  NotifHandlerThunkAction,
+  registerNotifHandlers,
+} from '~/framework/util/notifications/routing';
+
+const COMMUNITY_ID_REGEX = /\/community\/id\/([a-f0-9-]+)/i;
+
+const handleCommunitiesNotificationAction: NotifHandlerThunkAction =
+  (notification, allowSwitchTab) => async (dispatch, getState) => {
+    try {
+      const communityNotif = getAsResourceUriNotification(notification);
+      if (!communityNotif) return { managed: 0 };
+
+      const communityUri = communityNotif?.resource.uri;
+      const communityId = communityUri?.match(COMMUNITY_ID_REGEX)?.[1];
+      if (!communityId) return { managed: 0 };
+
+      const navAction = CommonActions.navigate(
+        allowSwitchTab
+          ? {
+              name: computeTabRouteName(timelineModuleConfig.routeName),
+              params: {
+                initial: false,
+                params: {
+                  fromTimeline: false,
+                  notification: communityNotif,
+                  resourceId: communityId,
+                },
+                screen: communitiesRouteNames.home,
+              },
+            }
+          : {
+              name: communitiesRouteNames.home,
+              params: {
+                fromTimeline: true,
+                id: communityId,
+                notification: communityNotif,
+              },
+            },
+      );
+
+      handleNotificationNavigationAction(navAction);
+
+      return {
+        managed: 1,
+        trackInfo: { action: 'Communities', name: `${notification.type}.${notification['event-type']}` },
+      };
+    } catch {
+      return { managed: 0 };
+    }
+  };
+
+export default () =>
+  registerNotifHandlers([
+    {
+      'event-type': [
+        'add_member',
+        'announcement_new',
+        'course_page_new',
+        'discussion_message_new',
+        'join_request_new',
+        'join_request_pending',
+        'join_request_accepted',
+        'join_request_rejected',
+        'resource_new',
+      ],
+      'notifHandlerAction': handleCommunitiesNotificationAction,
+      'type': 'communities',
+    },
+  ]);
