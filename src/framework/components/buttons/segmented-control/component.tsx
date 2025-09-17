32dd725dc8bead01337d41a9dcad84120a7b48f1
francois <francois.touchard@edifice.io>
2025-09-17 15:10:22 +0200

    fix(communities): PEDAGO-3170 SegmentedControl border

--- src/framework/components/buttons/segmented-control/component.tsx
+++ src/framework/components/buttons/segmented-control/component.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, View } from 'react-native';
 
 import { styles } from './styles';
 import { SegmentedControlProps } from './types';
@@ -10,29 +10,27 @@ const SegmentedControl = ({ canUnselect, initialSelectedIndex, onChange, segment
   const [selectedIndex, setSelectedIndex] = React.useState<number | undefined>(initialSelectedIndex);
 
   return (
-    <ScrollView
-      alwaysBounceHorizontal={false}
-      contentContainerStyle={styles.scrollContainer}
-      horizontal
-      showsHorizontalScrollIndicator={false}>
-      {segments.map((segment, index) => {
-        const isSelected = selectedIndex === index;
+    <ScrollView alwaysBounceHorizontal={false} horizontal showsHorizontalScrollIndicator={false}>
+      <View style={styles.scrollContainer}>
+        {segments.map((segment, index) => {
+          const isSelected = selectedIndex === index;
 
-        return (
-          <SegmentedControlItem
-            count={segment.count}
-            id={segment.id}
-            isActive={isSelected}
-            key={segment.id}
-            onPress={() => {
-              const newIndex = isSelected && canUnselect ? undefined : index;
-              setSelectedIndex(newIndex);
-              onChange?.(newIndex);
-            }}
-            text={segment.text}
-          />
-        );
-      })}
+          return (
+            <SegmentedControlItem
+              count={segment.count}
+              id={segment.id}
+              isActive={isSelected}
+              key={segment.id}
+              onPress={() => {
+                const newIndex = isSelected && canUnselect ? undefined : index;
+                setSelectedIndex(newIndex);
+                onChange?.(newIndex);
+              }}
+              text={segment.text}
+            />
+          );
+        })}
+      </View>
     </ScrollView>
   );
 };
