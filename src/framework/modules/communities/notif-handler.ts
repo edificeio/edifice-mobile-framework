6658bde3f0c51f4216b29dd24156b7b4188ab38e
Valentin Mourot <valentin.mourot@edifice.io>
2025-09-26 10:05:44 +0200

    fix(communities): open add_member notification properly

--- src/framework/modules/communities/notif-handler.ts
+++ src/framework/modules/communities/notif-handler.ts
@@ -1,17 +1,85 @@
+import { CommunityClient } from '@edifice.io/community-client-rest-rn';
 import { CommonActions } from '@react-navigation/native';
 
+import moduleConfig from './module-config';
 import { communitiesRouteNames } from './navigation';
 
 import timelineModuleConfig from '~/framework/modules/timeline/module-config';
 import { computeTabRouteName } from '~/framework/navigation/tabModules';
-import { getAsResourceUriNotification } from '~/framework/util/notifications';
+import { getAsNamedResourceNotification, getAsResourceUriNotification } from '~/framework/util/notifications';
 import {
   handleNotificationNavigationAction,
   NotifHandlerThunkAction,
   registerNotifHandlers,
 } from '~/framework/util/notifications/routing';
+import { sessionApi } from '~/framework/util/transport';
 
 const COMMUNITY_ID_REGEX = /\/community\/id\/([a-f0-9-]+)/i;
+
+const handleCommunitiesInvitationNotificationAction: NotifHandlerThunkAction =
+  (notification, allowSwitchTab) => async (dispatch, getState) => {
+    try {
+      const communityNotif = getAsNamedResourceNotification(notification);
+      if (!communityNotif) return { managed: 0 };
+
+      // If community invitation has already been accepted, we must navigate to the home screen of it.
+      const resource = await sessionApi(moduleConfig, CommunityClient)
+        .getCommunity(parseInt(communityNotif.resource.id, 10))
+        .catch(() => null);
+
+      if (resource !== null) {
+        handleNotificationNavigationAction(
+          CommonActions.navigate(
+            allowSwitchTab
+              ? {
+                  name: computeTabRouteName(moduleConfig.routeName),
+                  params: {
+                    initial: false,
+                    params: {
+                      communityId: resource.id,
+                    },
+                    screen: communitiesRouteNames.home,
+                  },
+                }
+              : {
+                  name: communitiesRouteNames.home,
+                  params: {
+                    communityId: resource.id,
+                  },
+                },
+          ),
+        );
+      } else {
+        handleNotificationNavigationAction(
+          CommonActions.navigate(
+            allowSwitchTab
+              ? {
+                  name: computeTabRouteName(moduleConfig.routeName),
+                  params: {
+                    initial: false,
+                    params: {
+                      pending: true,
+                    },
+                    screen: communitiesRouteNames.list,
+                  },
+                }
+              : {
+                  name: communitiesRouteNames.list,
+                  params: {
+                    pending: true,
+                  },
+                },
+          ),
+        );
+      }
+      return {
+        managed: 1,
+        trackInfo: { action: 'Communities', name: `${notification.type}.${notification['event-type']}` },
+      };
+    } catch {
+      return { managed: 0 };
+    }
+  };
 
 const handleCommunitiesNotificationAction: NotifHandlerThunkAction =
   (notification, allowSwitchTab) => async (dispatch, getState) => {
@@ -62,17 +130,21 @@ export default () =>
   registerNotifHandlers([
     {
       'event-type': [
-        'add_member',
-        'announcement_new',
-        'course_page_new',
-        'discussion_message_new',
-        'join_request_new',
-        'join_request_pending',
-        'join_request_accepted',
-        'join_request_rejected',
-        'resource_new',
+        'ANNOUNCEMENT_NEW',
+        'COURSE_PAGE_NEW',
+        'DISCUSSION_MESSAGE_NEW',
+        'JOIN_REQUEST_NEW',
+        'JOIN_REQUEST_PENDING',
+        'JOIN_REQUEST_ACCEPTED',
+        'JOIN_REQUEST_REJECTED',
+        'RESOURCE_NEW',
       ],
       'notifHandlerAction': handleCommunitiesNotificationAction,
-      'type': 'communities',
+      'type': 'COMMUNITIES',
+    },
+    {
+      'event-type': ['ADD_MEMBER'],
+      'notifHandlerAction': handleCommunitiesInvitationNotificationAction,
+      'type': 'COMMUNITIES',
     },
   ]);
