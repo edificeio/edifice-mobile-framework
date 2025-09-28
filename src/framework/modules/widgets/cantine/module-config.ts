5e296c5f215453f852c417339fa7581891fd4639
Moustapha HENNAWI <moustapha.hennawi@edifice.io>
2025-09-28 13:09:02 +0200

    chore: the new style

--- src/framework/modules/widgets/cantine/module-config.ts
+++ src/framework/modules/widgets/cantine/module-config.ts
@@ -1,10 +1,9 @@
-import theme from '~/app/theme';
 import { NavigableModuleConfig } from '~/framework/util/moduleTool';
 
 export default new NavigableModuleConfig<'cantine', null>({
   displayAs: 'myAppsWidgets',
-  displayI18n: 'widget-cantine.tabName',
-  displayPicture: { fill: theme.palette.complementary.orange.regular, name: 'ui-widget', type: 'Svg' },
+  displayI18n: 'widget-cantine-title',
+  displayPicture: { name: 'ui-widget-cantine', type: 'Svg' },
   entcoreScope: ['appregistry'],
   matchEntcoreApp: () => true,
   matchEntcoreWidget: entcoreWidget => entcoreWidget.name === 'cantine-widget',
