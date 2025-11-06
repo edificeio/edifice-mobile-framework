f92835856e8c76349d0000e22866a8290e96ed30
Valentin Mourot <valentin.mourot@edifice.io>
2025-11-06 11:52:21 +0100

    feat: PEDAGO-3458, PEDAGO-3177 use explorer in scrapbook

--- src/framework/modules/scrapbook/index.ts
+++ src/framework/modules/scrapbook/index.ts
@@ -4,11 +4,15 @@ import config from './module-config';
 import { scrapbookRouteNames } from './navigation';
 import getRoot from './navigation/navigator';
 import setUpNotifHandlers from './notif-handler';
+import reducer from './store';
 
 import { INTENT_TYPE, registerIntent } from '~/app/intents';
+import { Reducers } from '~/app/store';
 import { NavigableModule } from '~/framework/util/moduleTool';
 
-module.exports = new NavigableModule({ config, getRoot, reducer: () => null });
+module.exports = new NavigableModule({ config, getRoot, reducer });
+
+Reducers.register(config.reducerName, reducer);
 
 setUpNotifHandlers();
 
