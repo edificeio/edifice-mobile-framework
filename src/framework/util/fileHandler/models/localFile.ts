51f0c9ce74e958f627c67e7d6b9b3b10ec5021d1
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2025-11-14 13:48:23 +0100

    feat(fileHandler): coco-4883, split logic, some type fix

---
+++ src/framework/util/fileHandler/models/localFile.ts
@@ -0,0 +1,100 @@
+import { Platform } from 'react-native';
+
+import getPath from '@flyerhq/react-native-android-uri-path';
+import DocumentPicker, { DocumentPickerResponse } from '@react-native-documents/picker';
+import moment from 'moment';
+import { DownloadDirectoryPath, moveFile, scanFile, UploadFileItem } from 'react-native-fs';
+
+import { openDocument } from '../actions';
+import { Asset } from '../types';
+
+import { assertPermissions } from '~/framework/util/permissions';
+
+namespace LocalFileNS {
+  export type IPickOptionsType = 'image' | 'audio' | 'video';
+  export type CustomUploadFileItem = Omit<UploadFileItem, 'name'>;
+}
+
+export class LocalFile implements LocalFileNS.CustomUploadFileItem {
+  filename: string;
+  filepath: string;
+  _filepathNative: string;
+  filesize?: number;
+  filetype: string;
+  nativeInfo: DocumentPickerResponse | Asset;
+  _needIOSReleaseSecureAccess?: boolean;
+
+  constructor(
+    file: DocumentPickerResponse | Asset | LocalFileNS.CustomUploadFileItem,
+    opts: { _needIOSReleaseSecureAccess: boolean },
+  ) {
+    this._needIOSReleaseSecureAccess = opts._needIOSReleaseSecureAccess;
+
+    this.filename =
+      (file as LocalFileNS.CustomUploadFileItem).filename || (file as DocumentPickerResponse).name || (file as Asset).fileName!;
+
+    this._filepathNative = (file as LocalFileNS.CustomUploadFileItem).filepath || (file as DocumentPickerResponse | Asset).uri!;
+
+    this.filepath = LocalFile.formatUrlForUpload(this._filepathNative);
+    this.filetype = (file as LocalFileNS.CustomUploadFileItem).filetype || (file as DocumentPickerResponse | Asset).type!;
+
+    this.filesize = (file as any).fileSize || (file as any).size || undefined;
+
+    this.nativeInfo = file as DocumentPickerResponse | Asset;
+  }
+
+  setExtension(ext: string) {
+    this.filename += `.${ext}`;
+    this.setPath(`${this.filepath}.${ext}`);
+  }
+
+  setPath(path: string) {
+    this.filename = path.split('/').pop()!;
+    this.filepath = path;
+    this._filepathNative = path;
+  }
+
+  releaseIfNeeded = () => {
+    this._needIOSReleaseSecureAccess && DocumentPicker.releaseSecureAccess([this._filepathNative]);
+    this._needIOSReleaseSecureAccess = false;
+  };
+
+  static releaseLocalFiles(files: LocalFile[]) {
+    files.forEach(f => f.releaseIfNeeded());
+  }
+
+  static removeProtocol = (url: string) => url.replace(/^\w*?:\/\/(.+)/, '$1');
+
+  static formatUrlForUpload = (url: string) =>
+    Platform.select({
+      default: decodeURI(LocalFile.removeProtocol(getPath(url))),
+      ios: decodeURI(LocalFile.removeProtocol(url)),
+    }) || url;
+
+  async open() {
+    if (!this.filepath.includes('://')) {
+      this.filepath = 'file://' + this.filepath;
+      this._filepathNative = 'file://' + this._filepathNative;
+    }
+    await openDocument(this as any);
+  }
+
+  async moveToDownloadFolder() {
+    await assertPermissions('documents.write');
+    const destFolder = DownloadDirectoryPath;
+
+    const splitFilename = this.filename.split('.');
+    const ext = splitFilename.pop();
+    const destPath = `${destFolder}/` + `${splitFilename.join('.')}-${moment().format('YYYYMMDD-HHmmss')}.${ext}`;
+
+    await moveFile(this.filepath, destPath);
+    this.setPath(destPath);
+
+    if (Platform.OS === 'android') await scanFile(destPath);
+  }
+}
+
+export namespace LocalFile {
+  export type IPickOptionsType = LocalFileNS.IPickOptionsType;
+  export type CustomUploadFileItem = LocalFileNS.CustomUploadFileItem;
+}
---
+++ src/framework/util/fileHandler/models/syncedFile.ts
@@ -0,0 +1,44 @@
+import { LocalFile } from './localFile';
+
+export interface IDistantFile {
+  url: string;
+  filename?: string;
+  filetype?: string;
+  filesize?: number;
+}
+
+export interface IDistantFileWithId extends IDistantFile {
+  id: string;
+}
+
+export class SyncedFile<DFType extends IDistantFile = IDistantFile> implements IDistantFile {
+  df: DFType;
+  lf: LocalFile;
+
+  constructor(localFile: LocalFile, distantFile: DFType) {
+    this.df = distantFile;
+    this.lf = localFile;
+  }
+
+  get url() {
+    return this.df.url;
+  }
+  get filename() {
+    return this.df.filename ?? this.lf.filename;
+  }
+  get filetype() {
+    return this.df.filetype ?? this.lf.filetype;
+  }
+  get filepath() {
+    return this.lf.filepath;
+  }
+
+  releaseIfNeeded = () => this.lf.releaseIfNeeded();
+  open = () => this.lf.open();
+}
+
+export class SyncedFileWithId extends SyncedFile<IDistantFileWithId> {
+  get id() {
+    return this.df.id;
+  }
+}
