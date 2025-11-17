99f931d65e6e4dcca49762ae429ef1b0c51a2a58
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2025-11-17 12:38:33 +0100

    feat(viescolaire-common): enabling-427, migrate http service to transport service

--- src/framework/modules/viescolaire/common/service/index.ts
+++ src/framework/modules/viescolaire/common/service/index.ts
@@ -11,35 +11,35 @@ import {
   IBackendTermList,
   IBackendUserList,
 } from '~/framework/modules/viescolaire/common/service/types';
-import { fetchJSONWithCache } from '~/infra/fetchWithCache';
+import { sessionFetch } from '~/framework/util/transport';
 
 export const viescoService = {
   classGroups: {
     get: async (session: AuthActiveAccount, classes: string[], studentId?: string) => {
       let api = `/viescolaire/group/from/class?classes=${classes.join('&classes=')}`;
       if (studentId) api += `&student=${studentId}`;
-      const classGroups = (await fetchJSONWithCache(api)) as IBackendClassGroupsList;
+      const classGroups = await sessionFetch.json<IBackendClassGroupsList>(api);
       return classGroups.map(classGroupsAdapter);
     },
   },
   schoolYear: {
     get: async (session: AuthActiveAccount, structureId: string) => {
       const api = `/viescolaire/settings/periode/schoolyear?structureId=${structureId}`;
-      const schoolYear = (await fetchJSONWithCache(api)) as IBackendSchoolYear;
+      const schoolYear = await sessionFetch.json<IBackendSchoolYear>(api);
       return schoolYearAdapter(schoolYear);
     },
   },
   teachers: {
     get: async (session: AuthActiveAccount, structureId: string) => {
       const api = `/viescolaire/user/list?profile=Teacher&structureId=${structureId}`;
-      const teachers = (await fetchJSONWithCache(api)) as IBackendUserList;
+      const teachers = await sessionFetch.json<IBackendUserList>(api);
       return teachers.map(userAdapter);
     },
   },
   terms: {
     get: async (session: AuthActiveAccount, structureId: string, groupId: string) => {
       const api = `/viescolaire/periodes?idEtablissement=${structureId}&idGroupe=${groupId}`;
-      const terms = (await fetchJSONWithCache(api)) as IBackendTermList;
+      const terms = await sessionFetch.json<IBackendTermList>(api);
       return terms.map(termAdapter);
     },
   },
