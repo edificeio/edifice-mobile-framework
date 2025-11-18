43885f3033e25be1a488f527698bcf8f7dda77a0
Moustapha HENNAWI <moustapha.hennawi@edifice.io>
2025-11-18 11:58:17 +0100

    chore: add color to screen time

--- src/framework/modules/widgets/screen-time/module-config.ts
+++ src/framework/modules/widgets/screen-time/module-config.ts
@@ -1,7 +1,9 @@
+import theme from '~/app/theme';
 import { ModuleType, NavigableModuleConfig } from '~/framework/util/moduleTool';
 
 export default new NavigableModuleConfig<'screenTime', null>({
   displayAs: ModuleType.MYAPPS_WIDGET,
+  displayColor: theme.palette.complementary.orange,
   displayI18n: 'widget-screen-time-title',
   displayPicture: { name: 'ui-widget-screen-time', type: 'Svg' },
   entcoreScope: ['appregistry'],
