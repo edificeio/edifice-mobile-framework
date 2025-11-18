ef31901e49ede21da9e8c87fc5c2028ef28c686c
Moustapha HENNAWI <moustapha.hennawi@edifice.io>
2025-11-18 11:39:19 +0100

    fix: use Picture component

--- src/framework/modules/timeline/components/widget-chip/component.tsx
+++ src/framework/modules/timeline/components/widget-chip/component.tsx
@@ -2,34 +2,28 @@ import * as React from 'react';
 import { FlatList, Pressable, View } from 'react-native';
 
 import { I18n } from '~/app/i18n';
-import { UI_SIZES } from '~/framework/components/constants';
-import { Svg } from '~/framework/components/picture';
+import { Picture } from '~/framework/components/picture';
 import { BodyBoldText } from '~/framework/components/text';
 import styles from '~/framework/modules/timeline/components/widget-chip/styles';
 import { IWidgetChipProps, IWidgetChipsContainerProps } from '~/framework/modules/timeline/components/widget-chip/types';
-import { Image } from '~/framework/util/media-deprecated';
 import { AnyNavigableModule } from '~/framework/util/moduleTool';
 
 function WidgetChipItem(props: IWidgetChipProps) {
   const { onPress, testID, widget } = props;
   const label = I18n.get(widget.config.displayI18n);
 
-  const renderPicture = () => {
-    switch (widget.config.displayPicture?.type) {
-      case 'Svg':
-        return (
-          <Svg {...widget.config.displayPicture} height={UI_SIZES.elements.icon.xlarge} width={UI_SIZES.elements.icon.xlarge} />
-        );
-      case 'Image':
-        return <Image source={widget.config.displayPicture.source} style={styles.imagePicture} />;
-      default:
-        return null;
-    }
-  };
+  const type = widget.config.displayPicture?.type as 'Icon' | 'Svg' | 'Image';
+  var name = '';
+
+  if (widget.config.displayPicture?.type === 'Icon' || widget.config.displayPicture?.type === 'Svg') {
+    name = widget.config.displayPicture.name;
+  } else if (widget.config.displayPicture?.type === 'Image') {
+    name = widget.config.displayPicture.source?.toString() || '';
+  }
 
   return (
-    <Pressable onPress={onPress} style={styles.chipButton} testID={testID}>
-      {renderPicture()}
+    <Pressable onPress={onPress} style={[styles.chipButton, { backgroundColor: widget.config.displayColor?.pale }]} testID={testID}>
+      {name && name !== '' && <Picture type={type} name={name} height={16} width={16} />}
       <BodyBoldText style={styles.chipsText}>{label}</BodyBoldText>
     </Pressable>
   );
@@ -39,7 +33,6 @@ export function WidgetChip(props: IWidgetChipProps | IWidgetChipsContainerProps)
   // Check if this is a container props (has widgets and navigation)
   if ('widgets' in props && 'navigation' in props) {
     const { navigation, widgets } = props as IWidgetChipsContainerProps;
-
     return (
       <View style={styles.chipsContainer}>
         <FlatList
