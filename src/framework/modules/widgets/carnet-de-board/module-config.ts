b0d49589e88e563a4a9a2e535efa2ab1a94b1b50
Moustapha HENNAWI <moustapha.hennawi@edifice.io>
2025-11-18 11:57:57 +0100

    chore: remove displaypicture and change timeline name to home

--- src/framework/modules/widgets/carnet-de-board/module-config.ts
+++ src/framework/modules/widgets/carnet-de-board/module-config.ts
@@ -5,7 +5,6 @@ import { ModuleType, NavigableModuleConfig } from '~/framework/util/moduleTool';
 export default new NavigableModuleConfig<'carnet-de-bord', ReturnType<typeof reducer>>({
   displayAs: ModuleType.MYAPPS_WIDGET,
   displayI18n: 'pronote',
-  displayPicture: { source: require('ASSETS/images/logo-pronote.png'), type: 'Image' },
   entcoreScope: ['pronote'],
   hasRight: param => param.matchingWidgets.length > 0,
   matchEntcoreApp: entcoreApp => entcoreApp.casType === 'PronoteRegisteredService',
