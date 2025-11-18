153bb254e8e39d7069d7988c435fbc5a3510c19a
Valentin Mourot <valentin.mourot@edifice.io>
2025-11-18 18:47:39 +0100

    fix: PEDAGO-3479 fix status bar on pdf viewer

--- src/framework/components/status-bar.tsx
+++ src/framework/components/status-bar.tsx
@@ -11,30 +11,35 @@ const statusBarConfig = {
   dark: {
     backgroundColor: theme.palette.grey.black,
     barStyle: 'light-content' as const,
+    hidden: false,
     translucent: false,
   },
   light: {
     backgroundColor: theme.ui.background.page,
     barStyle: 'dark-content' as const,
+    hidden: false,
     translucent: false,
   },
   primary: {
     backgroundColor: theme.palette.primary.regular,
     barStyle: 'light-content' as const,
+    hidden: false,
     translucent: false,
   },
   white: {
     backgroundColor: theme.palette.grey.white,
     barStyle: 'dark-content' as const,
+    hidden: false,
     translucent: false,
   },
 };
 
 export const StatusBar = ({ type }: StatusBarProps) => {
-  const { backgroundColor, barStyle, translucent } = statusBarConfig[type];
+  const { backgroundColor, barStyle, hidden, translucent } = statusBarConfig[type];
 
   return (
     <RNStatusBar
+      hidden={hidden}
       translucent={translucent}
       backgroundColor={Platform.OS === 'android' ? backgroundColor : undefined}
       barStyle={barStyle}
