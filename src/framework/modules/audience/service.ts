c4a336d15db0f3408f425690042d8f68db6be4e7
francois <francois.touchard@edifice.io>
2025-11-24 17:32:36 +0100

    chore(audience): ENABLING-408 module fetch update

--- src/framework/modules/audience/service.ts
+++ src/framework/modules/audience/service.ts
@@ -8,30 +8,32 @@ import {
 } from './types';
 
 import { AuthActiveAccount } from '~/framework/modules/auth/model';
-import { fetchJSONWithCache, signedFetch, signedFetchRelative } from '~/infra/fetchWithCache';
+import { sessionFetch } from '~/framework/util/transport';
 
 export const audienceService = {
   reaction: {
     delete: async (session: AuthActiveAccount, referer: AudienceReferer) => {
       const api = `/audience/reactions/${referer.module}/${referer.resourceType}/${referer.resourceId}`;
-      return signedFetch(`${session.platform.url}${api}`, {
-        method: 'DELETE',
-      }) as Promise<Response>;
+
+      return sessionFetch(api, { method: 'DELETE' });
     },
     getDetails: async (referer: AudienceReferer, page: number, size: number) => {
       const api = `/audience/reactions/${referer.module}/${referer.resourceType}/${referer.resourceId}?page=${page}&size=${size}`;
-      const entcoreAudienceReactions = await fetchJSONWithCache(api);
-      return entcoreAudienceReactions as AudienceReactions;
+      const entcoreAudienceReactions = await sessionFetch.json<AudienceReactions>(api);
+
+      return entcoreAudienceReactions;
     },
     getSummary: async (module: string, resourceType: string, resourceIds: string[]) => {
       const api = `/audience/reactions/${module}/${resourceType}?resourceIds=${resourceIds.join(',')}`;
-      const audienceSummaryReactions = await fetchJSONWithCache(api);
-      return audienceSummaryReactions as AudienceSummaryReactions;
+      const audienceSummaryReactions = await sessionFetch.json<AudienceSummaryReactions>(api);
+
+      return audienceSummaryReactions;
     },
     getValidReactionTypes: async () => {
       const api = `/audience/conf/public`;
-      const validReactionTypes = await fetchJSONWithCache(api);
-      return validReactionTypes as AudienceValidReactionTypes;
+      const validReactionTypes = await sessionFetch.json<AudienceValidReactionTypes>(api);
+
+      return validReactionTypes;
     },
     post: async (session: AuthActiveAccount, referer: AudienceReferer, reaction: string) => {
       const api = `/audience/reactions/${referer.module}/${referer.resourceType}`;
@@ -39,10 +41,8 @@ export const audienceService = {
         reactionType: reaction,
         resourceId: referer.resourceId,
       });
-      return signedFetch(`${session.platform.url}${api}`, {
-        body,
-        method: 'POST',
-      }) as Promise<any>;
+
+      return sessionFetch(api, { body, method: 'POST' });
     },
     update: async (session: AuthActiveAccount, referer: AudienceReferer, reaction: string) => {
       const api = `/audience/reactions/${referer.module}/${referer.resourceType}`;
@@ -50,26 +50,26 @@ export const audienceService = {
         reactionType: reaction,
         resourceId: referer.resourceId,
       });
-      return signedFetch(`${session.platform.url}${api}`, {
-        body,
-        method: 'PUT',
-      }) as Promise<any>;
+
+      return sessionFetch(api, { body, method: 'PUT' });
     },
   },
   view: {
     getDetails: async (referer: AudienceReferer) => {
       const api = `/audience/views/details/${referer.module}/${referer.resourceType}/${referer.resourceId}`;
-      const audienceViews = await fetchJSONWithCache(api);
-      return audienceViews as AudienceViews;
+      const audienceViews = await sessionFetch.json<AudienceViews>(api);
+
+      return audienceViews;
     },
     getSummary: async (module: string, resourceType: string, resourceIds: string[]) => {
       const api = `/audience/views/count/${module}/${resourceType}?resourceIds=${resourceIds.join(',')}`;
-      const audienceSummaryViews = await fetchJSONWithCache(api);
-      return audienceSummaryViews as AudienceSummaryViews;
+      const audienceSummaryViews = await sessionFetch.json<AudienceSummaryViews>(api);
+
+      return audienceSummaryViews;
     },
     post: async (module: string, resourceType: string, resourceId: string) => {
       try {
-        return await signedFetchRelative(`/audience/views/${module}/${resourceType}/${resourceId}`, {
+        return await sessionFetch(`/audience/views/${module}/${resourceType}/${resourceId}`, {
           method: 'POST',
         });
       } catch (e) {
