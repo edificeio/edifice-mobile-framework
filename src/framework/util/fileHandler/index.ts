93685d35f99b3ebd41e686732c70462260620c84
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2025-11-25 12:02:19 +0100

    feat(fileHandler):COCO-4945, fix typo, "gallery".* instead of "galery".*

--- src/framework/util/fileHandler/index.ts
+++ src/framework/util/fileHandler/index.ts
@@ -158,7 +158,7 @@ export class LocalFile implements LocalFile.CustomUploadFileItem {
   }
   static async pickFromGallery(callback, multiple: boolean, synchrone?: boolean, callbackOnce?: boolean) {
     try {
-      await assertPermissions('galery.read');
+      await assertPermissions('gallery.read');
       console.debug('[Gallery] Opening picker...');
 
       const response = await launchImageLibrary({
