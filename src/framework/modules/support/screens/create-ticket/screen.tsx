ed916a9f3c29e0486518d39e79ccc915b3ed1b78
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2025-12-03 16:09:43 +0100

    feat(fileHandler-support): COCO-4885, migrate to new picker Apis

--- src/framework/modules/support/screens/create-ticket/screen.tsx
+++ src/framework/modules/support/screens/create-ticket/screen.tsx
@@ -16,7 +16,9 @@ import { IGlobalState } from '~/app/store';
 import theme from '~/app/theme';
 import PrimaryButton from '~/framework/components/buttons/primary';
 import { EmptyScreen } from '~/framework/components/empty-screens';
-import { cameraAction, documentAction, galleryAction } from '~/framework/components/menus/actions';
+import { cameraActionFm } from '~/framework/components/menus/actions/cameraAction';
+import { documentActionFm } from '~/framework/components/menus/actions/documentAction';
+import { galleryActionFm } from '~/framework/components/menus/actions/galleryAction';
 import BottomMenu from '~/framework/components/menus/bottom';
 import { KeyboardPageView, PageView } from '~/framework/components/page';
 import { Svg } from '~/framework/components/picture';
@@ -25,11 +27,12 @@ import Toast from '~/framework/components/toast';
 import usePreventBack from '~/framework/hooks/prevent-back';
 import { getSession } from '~/framework/modules/auth/reducer';
 import { postSupportTicketAction, uploadSupportTicketAttachmentsAction } from '~/framework/modules/support/actions';
+import moduleConfig from '~/framework/modules/support/module-config';
 import { SupportNavigationParams, supportRouteNames } from '~/framework/modules/support/navigation';
 import { getSupportWorkflowInformation } from '~/framework/modules/support/rights';
 import { Attachment } from '~/framework/modules/zimbra/components/Attachment';
 import { navBarOptions } from '~/framework/navigation/navBar';
-import { LocalFile, SyncedFileWithId } from '~/framework/util/fileHandler';
+import { LocalFile, SyncedFileWithId } from '~/framework/util/fileHandler/models';
 import { tryActionLegacy } from '~/framework/util/redux/actions';
 
 export const computeNavBar = ({
@@ -54,10 +57,8 @@ const SupportCreateTicketScreen = (props: ISupportCreateTicketScreenProps) => {
   const [attachments, setAttachments] = React.useState<LocalFile[]>([]);
   const [isSending, setSending] = React.useState(false);
 
-  const addAttachment = fileOrFiles => {
-    const files = Array.isArray(fileOrFiles) ? fileOrFiles : [fileOrFiles];
-    const newAttachments = files.map(f => new LocalFile(f, { _needIOSReleaseSecureAccess: false }));
-    setAttachments(prev => [...prev, ...newAttachments]);
+  const addAttachment = (fileOrFiles: LocalFile[]) => {
+    setAttachments(prev => [...prev, ...fileOrFiles]);
   };
 
   const removeAttachment = (filepath: string) => {
@@ -93,6 +94,7 @@ const SupportCreateTicketScreen = (props: ISupportCreateTicketScreenProps) => {
     const mandatoryText = ' *';
     const filesAdded = attachments.length > 0;
     const isActionDisabled = subject === '' || subject.length > 255 || description === '';
+    const attachOpts = (file: LocalFile | LocalFile[]) => addAttachment(file as LocalFile[]);
 
     return hasTicketCreationRights ? (
       <ScrollView contentContainerStyle={styles.container}>
@@ -147,9 +149,9 @@ const SupportCreateTicketScreen = (props: ISupportCreateTicketScreenProps) => {
               <BottomMenu
                 title={I18n.get('support-createticket-addfiles')}
                 actions={[
-                  cameraAction({ callback: addAttachment }),
-                  galleryAction({ callback: addAttachment, multiple: true }),
-                  documentAction({ callback: addAttachment }),
+                  cameraActionFm(moduleConfig.name, 'attachments', { callback: attachOpts }),
+                  galleryActionFm(moduleConfig.name, 'attachments', { callback: attachOpts }),
+                  documentActionFm(moduleConfig.name, 'attachments', { callback: attachOpts }),
                 ]}>
                 <View style={[styles.textIconContainer, filesAdded && styles.textIconContainerSmallerMargin]}>
                   <SmallActionText style={styles.actionText}>{I18n.get('support-createticket-addfiles')}</SmallActionText>
