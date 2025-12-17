fdd5ee3ac61286698bf8ce29b5645c952467777b
francoisTouchardEdifice <francois.touchard@edifice.io>
2025-12-17 16:31:22 +0100

    chore(splashads): ENABLING-423 module fetch update (#245)

--- src/framework/util/media/source.ts
+++ src/framework/util/media/source.ts
@@ -1,12 +1,17 @@
 import { ImageRequireSource, ImageSourcePropType, ImageURISource } from 'react-native';
 
+import { ReactVideoSourceProperties } from 'react-native-video';
+import { WebViewSourceUri } from 'react-native-webview/lib/WebViewTypes';
+
 import { Media } from './types';
 
 export function isRequireSource(input: ImageSourcePropType): input is ImageRequireSource {
   return typeof input === 'number';
 }
 
-export const toURISource = (src: Media['src']): typeof src extends undefined ? undefined : Pick<ImageURISource, 'uri'> =>
+export const toURISource = <SourceType extends ImageURISource | ReactVideoSourceProperties | WebViewSourceUri>(
+  src: Media['src'],
+): typeof src extends undefined ? undefined : Pick<SourceType, 'uri'> =>
   src instanceof URL ? { uri: src.href } : typeof src === 'string' ? { uri: src } : src;
 
 export const toImageURISourceArray = (srcs: Media['src'][]) => srcs.map(toURISource);
