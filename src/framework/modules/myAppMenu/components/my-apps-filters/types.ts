2981bbc5984f3543ee564a1c76bbc889f7b9733e
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2026-03-17 16:16:31 +0100

    feat(myapps): COCO-5403, display all apps on favorites tab

--- src/framework/modules/myAppMenu/components/my-apps-filters/types.ts
+++ src/framework/modules/myAppMenu/components/my-apps-filters/types.ts
@@ -1,5 +1,7 @@
 import { MyAppsFilter } from '~/framework/modules/myapps/types';
 
+export type { MyAppsFilterItem, MyAppsFilterItemFilter, MyAppsFilterItemSeparator } from './filter-config';
+
 export interface MyAppsFiltersProps {
   selectedFilter: MyAppsFilter;
   onFilterChange: (filter: MyAppsFilter) => void;
