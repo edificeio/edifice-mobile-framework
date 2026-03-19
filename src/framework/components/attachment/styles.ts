9c8f9fd164ad0cab1c96fa16b4ef0efc215dd3c0
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2026-03-19 09:18:20 +0100

    chore: ENABLING-649, remove Zimbra module

---
+++ src/framework/components/attachment/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  iconContainer: {
+    alignItems: 'center',
+    height: 42,
+    justifyContent: 'center',
+    width: 48,
+  },
+  mainContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
--- src/framework/modules/zimbra/model/draft-type.ts
+++
@@ -1,7 +0,0 @@
-export enum DraftType {
-  NEW,
-  DRAFT,
-  REPLY,
-  REPLY_ALL,
-  FORWARD,
-}
