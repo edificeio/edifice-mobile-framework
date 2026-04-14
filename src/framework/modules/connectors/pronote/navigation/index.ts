b69c4e79d076449e7f42be8f9945c1d399d322c5
Moustapha HENNAWI <moustapha.hennawi@edifice.io>
2026-04-14 22:00:35 +0200

    fix(pronote): INTEG-1366 add screen struct choice for pronote connector befor redirect

--- src/framework/modules/connectors/pronote/navigation/index.ts
+++ src/framework/modules/connectors/pronote/navigation/index.ts
@@ -2,10 +2,11 @@ import { ParamListBase } from '@react-navigation/native';
 
 import type { ConnectorRedirectScreenNavParams } from '~/framework/modules/connectors/common/redirect-screen/types';
 import moduleConfig from '~/framework/modules/connectors/pronote/module-config';
+import type { ConnectorSelectorScreenNavParams } from '~/framework/modules/connectors/pronote/screens/connector-selector';
 
 export const pronoteRouteNames = {
   home: `${moduleConfig.routeName}` as 'home',
 };
 export interface PronoteNavigationParams extends ParamListBase {
-  home: ConnectorRedirectScreenNavParams;
+  home: ConnectorRedirectScreenNavParams | ConnectorSelectorScreenNavParams;
 }
