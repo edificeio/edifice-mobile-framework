48a60bf394781683f9e1b4e8cfaf51b7c98d6b03
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2026-04-21 18:07:18 +0200

    fix(timline): COCO-5686,  mapp widget-name to backend data

--- src/framework/modules/timeline/utils/widget-title.ts
+++
@@ -1,9 +0,0 @@
-import { I18n } from '~/app/i18n';
-import { IEntcoreWidget } from '~/framework/util/moduleTool';
-
-export const getWidgetTitle = (widget: IEntcoreWidget): string => {
-  // extract widget name without "-widget" suffix
-  const widgetName = widget.name.replace(/-widget$/, '');
-  const i18nKey = `widget-${widgetName}-title`;
-  return I18n.get(i18nKey);
-};
