e9a189f97e720ba49bab63a0dc0b2f33ae36ad48
Valentin Mourot <valentin.mourot@edifice.io>
2026-05-04 11:07:42 +0200

    chore: remove scaffolders

--- scaffolder/module/index.ts
+++
@@ -1,8 +0,0 @@
-import { NavigableModule } from '~/framework/util/moduleTool';
-
-import config from './module-config';
-import getRoot from './navigation/navigator';
-import { reducer } from './store';
-
-module.exports = new NavigableModule({ config, getRoot, reducer });
-
