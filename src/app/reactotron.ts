cb78f2bfe33d5776d17586cc676d50d8442984da
Valentin Mourot <valentin.mourot@edifice.io>
2026-05-19 17:28:09 +0200

    chore: ENABLING-767 rm async-storage + minor upgrades

--- src/app/reactotron.ts
+++ src/app/reactotron.ts
@@ -1,7 +1,6 @@
 //
 // See https://docs.infinite.red/reactotron/quick-start/react-native for more details
 //
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import type { ReactotronReactNative } from 'reactotron-react-native';
 import Reactotron from 'reactotron-react-native';
 import mmkvPlugin from 'reactotron-react-native-mmkv';
@@ -10,8 +9,7 @@ import { reactotronRedux } from 'reactotron-redux';
 import { mmkvInstance as storage } from '~/framework/util/storage/mmkv';
 
 export default Reactotron.configure({})
-  .setAsyncStorageHandler(AsyncStorage)
   .use(reactotronRedux())
   .use(mmkvPlugin<ReactotronReactNative>({ storage }))
-  .useReactNative({ asyncStorage: true })
+  .useReactNative()
   .connect();
--- src/framework/util/storage/mmkv.tsx
+++ src/framework/util/storage/mmkv.tsx
@@ -1,48 +1,12 @@
 import { Platform } from 'react-native';
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import DeviveInfo from 'react-native-device-info';
 import { createMMKV } from 'react-native-mmkv';
 
+import { getOverrideName } from '~/framework/util/string';
+
 import { StorageHandler } from './handler';
 import type { IStorageBackend } from './types';
-
-import { getOverrideName } from '~/framework/util/string';
-import { Trackers } from '~/framework/util/tracker';
-
-const MIGRATION_KEYS_IGNORE: RegExp[] = [/^@phrase_/];
-
-/**
- * Migrate from AsyncStorage
- * - Find all existing AsyncStorage keys
- * - Launch migration script if some keys remain
- * - Get each key's value, add it to MMKV and remove it from AsyncStorage
- */
-const migrateFromAsyncStorage = async (storage: IStorageBackend) => {
-  const keys = await AsyncStorage.getAllKeys();
-  const hasRemainingKeys = keys.length > 0;
-  if (hasRemainingKeys) {
-    for (const key of keys) {
-      try {
-        if (MIGRATION_KEYS_IGNORE.find(pattern => pattern.test(key))) {
-          continue;
-        }
-        const value = await AsyncStorage.getItem(key);
-        if (value !== null) {
-          storage.set(key, value);
-          AsyncStorage.removeItem(key);
-        }
-      } catch (error) {
-        Trackers.trackDebugEvent('Storage', 'MIGRATION ERROR', (error as Error | null)?.message || 'migrateFromAsyncStorage');
-        console.error(
-          `[Storage] migrateFromAsyncStorage: failed to migrate items ${
-            error instanceof Error ? `: ${(error as Error).message}` : ''
-          }`,
-        );
-      }
-    }
-  }
-};
 
 export const mmkvInstance = createMMKV({
   encryptionKey: Platform.OS === 'ios' ? DeviveInfo.getBundleId() : undefined,
@@ -50,5 +14,5 @@ export const mmkvInstance = createMMKV({
 }) satisfies IStorageBackend;
 
 export const mmkvHandler = new StorageHandler(mmkvInstance, 'mmkv').setAppInit(async function () {
-  await migrateFromAsyncStorage(this);
+  // Nothing to do here for the moment.
 });
