351b6f7c4257330d0490eebbbeeeeede3e6ad5ef
Valentin Mourot <valentin.mourot@edifice.io>
2026-06-05 11:56:52 +0200

    fix(mails): add-folder bottomsheet missized

--- src/framework/modules/mails/components/input-bottom-sheet/styles.ts
+++ src/framework/modules/mails/components/input-bottom-sheet/styles.ts
@@ -1,9 +1,5 @@
 import { StyleSheet } from 'react-native';
 
-import { getScaleWidth } from '~/framework/components/constants';
-
 export default StyleSheet.create({
-  scrollViewBottomSheet: {
-    paddingBottom: getScaleWidth(60),
-  },
+  scrollViewBottomSheet: {},
 });
