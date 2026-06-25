c6d6ab0e231f6645c2b374d4694c8f1d3a70a88d
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2026-06-25 15:21:06 +0200

    fix(myapps): IMPULS-5918, add testID(resource-id) for myapps onboarding modal

--- src/framework/modules/myapps/components/my-apps-onboarding-modal/my-apps-onboarding-slide.tsx
+++ src/framework/modules/myapps/components/my-apps-onboarding-modal/my-apps-onboarding-slide.tsx
@@ -3,11 +3,11 @@ import { View } from 'react-native';
 
 import LottieView from 'lottie-react-native';
 
-import { styles } from './styles';
-import { MAOSProps } from './types';
-
 import { Svg } from '~/framework/components/picture';
 import { BodyBoldText, BodyText, HeadingXSText } from '~/framework/components/text';
+
+import { styles } from './styles';
+import { MAOSProps } from './types';
 
 export const MyAppsOnboardingSlide: React.FC<MAOSProps> = ({ description, illustration, isActive, subtitle, title }) => {
   const animationRef = React.useRef<LottieView>(null);
@@ -23,11 +23,13 @@ export const MyAppsOnboardingSlide: React.FC<MAOSProps> = ({ description, illust
   return (
     <View style={styles.containerSlide}>
       <View style={styles.titleWrapper}>
-        <HeadingXSText style={styles.title}>{title}</HeadingXSText>
+        <HeadingXSText testID="myapps-onboarding-modal-title" style={styles.title}>
+          {title}
+        </HeadingXSText>
       </View>
 
       <View style={styles.illustrationWrapper}>
-        {illustration.type === 'svg' && <Svg cached name={illustration.name} width="100%" height="100%" />}
+        {illustration.type === 'svg' && <Svg name={illustration.name} width="100%" height="100%" />}
         {illustration.type === 'animated' && (
           <LottieView
             ref={animationRef}
