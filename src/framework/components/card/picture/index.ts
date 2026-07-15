57ea034122405b527d7f13ea8f49163533ddb7f0
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2026-07-15 12:41:57 +0200

    chore(auth): IMPULS-6079, handle sonar's feedback

--- src/framework/components/card/picture/index.ts
+++ src/framework/components/card/picture/index.ts
@@ -1,4 +1,2 @@
+export { TouchableSelectorPictureCard as default } from '~/framework/components/card/picture/picture-card';
 export type { OverviewCardProps } from '~/framework/components/card/picture/types';
-import { TouchableSelectorPictureCard } from '~/framework/components/card/picture/picture-card';
-
-export default TouchableSelectorPictureCard;
