859312adb0062657cf3d8b43f12ec089784fea02
thegdz <guillaume.droulez@mac.com>
2026-07-20 18:59:23 +0200

    chore(build): add extra infos for firebase app distribution

--- cli/prepare-build.js
+++ cli/prepare-build.js
@@ -146,6 +146,7 @@ try {
       .replaceAll("\\\"", "")
       .replaceAll("\"", "");
     lastContent.last = moment().format('YYYY-MM-DD HH:mm:ss');
+    lastContent.order = fullBuildType;
     lastContent.track = versionNumber;
     lastContent.version = fullVersion;
     console.info('=> Release Notes :');
