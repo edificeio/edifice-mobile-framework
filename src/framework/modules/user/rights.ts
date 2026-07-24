fe79a835dad8e4da5b7f8dc920585c736b2d9c24
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2026-07-24 17:21:52 +0200

    feat(user): IMPULS-6044 add redirection button in my account

--- src/framework/modules/user/rights.ts
+++ src/framework/modules/user/rights.ts
@@ -1,7 +1,12 @@
-import { AuthLoggedAccount } from '~/framework/modules/auth/model';
+import { AuthActiveAccount } from '~/framework/modules/auth/model';
 
 export const showMottoMood = 'org.entcore.directory.controllers.UserBookController|userBookMottoMood';
+export const switchThemeRight = 'org.entcore.directory.controllers.UserBookController|userBookSwitchTheme';
 
-export const getShowMottoMoodRight = (session: AuthLoggedAccount): boolean => {
+export const getShowMottoMoodRight = (session: AuthActiveAccount): boolean => {
   return session.rights.authorizedActions.some(a => a.name === showMottoMood);
+};
+
+export const getSwitchThemeRight = (session: AuthActiveAccount): boolean => {
+  return session.rights.authorizedActions.some(a => a.name === switchThemeRight);
 };
