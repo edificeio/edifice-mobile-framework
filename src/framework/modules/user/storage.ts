125148123623b87718aec5c68009548ba3268236
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2026-07-28 16:20:56 +0200

    feat(theme): IMPULS-6044, persist the theme index and restore it per user

--- src/framework/modules/user/storage.ts
+++ src/framework/modules/user/storage.ts
@@ -1,7 +1,13 @@
-import moduleConfig from './module-config';
-
 import { Storage } from '~/framework/util/storage';
+
+import moduleConfig from './module-config';
 
 export interface UserStorageData {}
 
+export interface UserPreferencesData {
+  theme: number;
+}
+
 export const storage = Storage.slice<UserStorageData>().withModule(moduleConfig);
+
+export const preferences = Storage.preferences<UserPreferencesData>(moduleConfig, () => {});
