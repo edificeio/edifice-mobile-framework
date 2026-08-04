d3180255130e35fa232601500243bb26a573b427
Ibrahima Guissé <ibrahima.guisse@edifice.io>
2026-08-04 16:02:53 +0200

    feat(home): IMPULS-6160, create profile button since shared between home screens

--- src/framework/components/navigation/index.ts
+++ src/framework/components/navigation/index.ts
@@ -1,5 +1,6 @@
 import NavBarAction from './navbar-action';
 import NavBarActionsGroup from './navbar-actions-group';
 import NavBarLine from './navbar-line';
+import NavBarProfileButton from './navbar-profile-button';
 
-export { NavBarAction, NavBarActionsGroup, NavBarLine };
+export { NavBarAction, NavBarActionsGroup, NavBarLine, NavBarProfileButton };
--- src/framework/modules/home/screens/styles.ts
+++ src/framework/modules/home/screens/styles.ts
@@ -1,3 +1,18 @@
 import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({ avatarHeaderButton: { marginLeft: 0 } });
+import theme from '~/app/theme';
+import { UI_SIZES } from '~/framework/components/constants';
+
+export const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: theme.palette.grey.white.toString(),
+  },
+  navBarTitle: { alignItems: 'center' },
+  page: {
+    backgroundColor: theme.ui.background.page.toString(),
+    flex: 1,
+  },
+  scene: {
+    padding: UI_SIZES.spacing.medium,
+  },
+});
